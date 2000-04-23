bda7691dfad83be9e0cf601f4ef9990213758618
Eric Warmenhoven <warmenhoven@yahoo.com>
2000-04-23 00:11:04 +0000

    [gaim-migrate @ 158] Added notes for how to do save-state in the autoreconnect plugin. If no one implements it before I get back home, I'll do it myself; feel free to try it on your own though.
    --HG--
    extra : convert_revision : c92caa2455bd8d1526c78c99a438bd47f8b335b5

--- plugins/autorecon.c
+++ plugins/autorecon.c
@@ -1,3 +1,23 @@
+/* TODO: save state
+ * I'm not at my computer right now, so I'm not going to bother
+ * writing it, but if someone wants to before I get back (hint,
+ * hint), go for it. Here's how to do it.
+ *
+ * First, add a global "state", which is either 'away' or not.
+ *
+ * In gaim_plugin_init, set state, and add two more signal
+ * handlers: event_away and event_back, and if you can't figure
+ * out what you're supposed to do for them, you shouldn't be
+ * editing this plugin.
+ *
+ * In the reconnect function, if "state" was away, then reset
+ * the away message. You may have to remember the away message
+ * on your own; I haven't checked yet to see if there's a global
+ * that remembers it that isn't erased on signoff/signon.
+ *
+ * Anyway, that should be it.
+ */
+
 #define GAIM_PLUGINS
 #include "gaim.h"
 #include <gtk/gtk.h>
