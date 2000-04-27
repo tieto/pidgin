36c31ae5df87be2be4ffd6ab229820aefd304ac0
Eric Warmenhoven <warmenhoven@yahoo.com>
2000-04-27 12:14:16 +0000

    [gaim-migrate @ 201] Wrote a plugin for IcedSoal to put an asterisk in the titlebar of conversations where you haven't replied to the person yet (e.g. you weren't the last person to say something).
    --HG--
    extra : convert_revision : 8ebc93f0e5a7ddfc83f60b32b1c462cb3f77c367

---
+++ plugins/notify.c
@@ -0,0 +1,51 @@
+#define GAIM_PLUGINS
+#include "gaim.h"
+
+#include <gtk/gtk.h>
+
+void *handle;
+
+void received_im(char **who, char **what, void *m) {
+	char buf[256];
+	struct conversation *cnv = find_conversation(*who);
+	GtkWindow *win;
+
+	if (cnv == NULL)
+		cnv = new_conversation(*who);
+
+	win = (GtkWindow *)cnv->window;
+
+	g_snprintf(buf, sizeof(buf), "%s", win->title);
+	if (!strstr(buf, "(*) ")) {
+		g_snprintf(buf, sizeof(buf), "(*) %s", win->title);
+		gtk_window_set_title(win, buf);
+	}
+}
+
+void sent_im(char *who, char **what, void *m) {
+	char buf[256];
+	struct conversation *c = find_conversation(who);
+	GtkWindow *win = (GtkWindow *)c->window;
+
+	g_snprintf(buf, sizeof(buf), "%s", win->title);
+	if (strstr(buf, "(*) ")) {
+		g_snprintf(buf, sizeof(buf), "%s", &win->title[4]);
+		gtk_window_set_title(win, buf);
+	}
+}
+
+void gaim_plugin_init(void *hndl) {
+	handle = hndl;
+
+	gaim_signal_connect(handle, event_im_recv, received_im, NULL);
+	gaim_signal_connect(handle, event_im_send, sent_im, NULL);
+}
+
+char *name() {
+	return "Visual Notification";
+}
+
+char *description() {
+	return "Puts an asterisk in the title bar of all conversations"
+		" where you have not responded to a message yet.";
+}
