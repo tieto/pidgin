598284eaf31faa432b1e1e5544c446212985d8c0
Rob Flynn <gaim@robflynn.com>
2000-06-10 07:20:43 +0000

    [gaim-migrate @ 388] Commits
    --HG--
    extra : convert_revision : 67609beb369b835bc15420c48fc6ebb10f984021

--- src/prefs.c
+++ src/prefs.c
@@ -771,7 +771,7 @@ void build_prefs()
 #endif
 	gaim_button(_("Show logon/logoffs in conversation windows"), &display_options, OPT_DISP_SHOW_LOGON, appbox );
 	gaim_button(_("Use devil icons"), &display_options, OPT_DISP_DEVIL_PIXMAPS, appbox );
-	gaim_button(_("Show graphical smileys"), &display_options, OPT_DISP_SHOW_SMILEY, appbox );
+	gaim_button(_("Show graphical smileys (experimental)"), &display_options, OPT_DISP_SHOW_SMILEY, appbox );
 	
 	
 	gaim_button(_("Bold Text"), &font_options, OPT_FONT_BOLD, fontbox);
