c1a9ccc54f894cab89184e63171c87fa8951ec00
Rob Flynn <gaim@robflynn.com>
2000-07-10 00:51:53 +0000

    [gaim-migrate @ 488] Yo
    --HG--
    extra : convert_revision : f0573e79645b5fc7ccfac89ea3055283f954306c

--- src/dialogs.c
+++ src/dialogs.c
@@ -2157,7 +2157,7 @@ void show_find_email()
         struct findbyemail *b = g_new0(struct findbyemail, 1);
         b->window = gtk_window_new(GTK_WINDOW_DIALOG);
 	gtk_widget_set_usize(b->window, 240, 110);
-	gtk_widget_set_policy(GTK_WINDOW(b->window), FALSE, FALSE, TRUE);
+	gtk_window_set_policy(GTK_WINDOW(b->window), FALSE, FALSE, TRUE);
 	gtk_widget_show(b->window);
         dialogwindows = g_list_prepend(dialogwindows, b->window); 
 
