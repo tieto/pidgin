c65f6883e71cd11f0ca2b4e9fc0aeb9418351b6b
Rob Flynn <gaim@robflynn.com>
2000-10-13 18:55:04 +0000

    [gaim-migrate @ 997] I am cool.
    --HG--
    extra : convert_revision : 8aa0e156a030689929eb66542b7527142b68edb8

---
+++ plugins/irc.c
@@ -0,0 +1,86 @@
+/*
+ * gaim - IRC Protocol Plugin
+ *
+ * Copyright (C) 2000, Rob Flynn <rob@tgflinux.com>
+ * Copyright (C) 1998-1999, Mark Spencer <markster@marko.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "../config.h"
+#endif
+
+
+#include <netdb.h>
+#include <gtk/gtk.h>
+#include <unistd.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include "multi.h"
+#include "prpl.h"
+#include "gaim.h"
+#include "aim.h"
+#include "gnome_applet_mgr.h"
+
+#include "pixmaps/cancel.xpm"
+#include "pixmaps/ok.xpm"
+
+struct prpl *irc_init() {
+	struct prpl *ret = g_new0(struct prpl, 1);
+
+	ret->protocol = NULL;
+	ret->name = NULL;
+	ret->login = NULL;
+	ret->close = NULL;
+	ret->send_im = NULL;
+	ret->set_info = NULL;
+	ret->get_info = NULL;
+	ret->set_away = NULL;
+	ret->get_away_msg = NULL;
+	ret->set_dir = NULL;
+	ret->get_dir = NULL;
+	ret->dir_search = NULL;
+	ret->set_idle = NULL;
+	ret->change_passwd = NULL;
+	ret->add_buddy = NULL;
+	ret->add_buddies = NULL;
+	ret->remove_buddy = NULL;
+	ret->add_permit = NULL;
+	ret->add_deny = NULL;
+	ret->warn = NULL;
+	ret->accept_chat = NULL;
+	ret->join_chat = NULL;
+	ret->chat_invite = NULL;
+	ret->chat_leave = NULL;
+	ret->chat_whisper = NULL;
+	ret->chat_send = NULL;
+	ret->keepalive = NULL;
+	
+	return ret;
+}
+
+int gaim_plugin_init(void *handle) {
+	protocols = g_slist_append(protocols, irc_init());
+	return 0;
+}
