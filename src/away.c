c18a3d59734441d26d2a622a5a35896b3592e27f
falling <none@none>
2000-12-19 04:23:36 +0000

    [gaim-migrate @ 1316] patch from falling
    committer: Eric Warmenhoven <warmenhoven@yahoo.com>
    
    --HG--
    extra : convert_revision : e0bbc03eeb9cfcf3ae92ae245f25d7bfb340e77d

--- src/away.c
+++ src/away.c
@@ -181,6 +181,7 @@ void rem_away_mess(GtkWidget *w, struct away_message *a)
 		else
 			default_away = NULL;
 	}
+	away_messages = g_slist_remove(away_messages, a);
 	g_free(a);
 	do_away_menu();
 	save_prefs();
