bc098cbd9b1d3955d9cb882b321ffd40217185e4
Eric Warmenhoven <warmenhoven@yahoo.com>
2001-02-25 20:09:31 +0000

    [gaim-migrate @ 1513] hi
    --HG--
    extra : convert_revision : a440a40e29f48bd3ed1a64e209766e6a185ae93a

--- plugins/yay/yay.c
+++ plugins/yay/yay.c
@@ -293,6 +293,7 @@ static void yahoo_login(struct aim_user *user) {
 	account_online(gc);
 	serv_finish_login(gc);
 
+	yd->active_id = g_strdup(gc->username);
 	if (bud_list_cache_exists(gc))
 		do_import(NULL, gc);
 
@@ -306,8 +307,6 @@ static void yahoo_login(struct aim_user *user) {
 				add_buddy(gc, bud->group, bud->id, bud->id);
 		}
 	}
-
-	yd->active_id = g_strdup(gc->username);
 
 	gc->inpa = gdk_input_add(ctxt->sockfd, GDK_INPUT_READ | GDK_INPUT_EXCEPTION,
 				yahoo_callback, gc);
