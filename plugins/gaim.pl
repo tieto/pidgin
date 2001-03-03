39fb75e304a2a349d9f34932cf2070c0498a0854
Eric Warmenhoven <warmenhoven@yahoo.com>
2001-03-03 06:19:19 +0000

    [gaim-migrate @ 1536] updated for 0.11.0
    --HG--
    extra : convert_revision : bb04211c35978ce15ecc10ec3472983d51fc052f

--- plugins/gaim.pl
+++ plugins/gaim.pl
@@ -1,31 +1,26 @@
-AIM::register("gaim test", "0.0.1", "goodbye", "");
+GAIM::register("gaim test", "0.0.1", "goodbye", "");
 
-$ver = AIM::get_info(0);
-$nam = AIM::get_info(1);
-$pro = AIM::get_info(2);
+$ver = GAIM::get_info(0);
+$nam = GAIM::get_info(1);
+$pro = GAIM::get_info(2, $nam);
 
-AIM::print("Perl Says", "Gaim $ver, $nam using $pro");
+GAIM::print("Perl Says", "Gaim $ver, $nam using $pro");
 
-# i should probably do something with these to actually test them, huh
-@bud = AIM::buddy_list();
-@onl = AIM::online_list();
-@den = AIM::deny_list();
+GAIM::command("idle", 60000);
 
-AIM::command("idle", "60000") if ($pro ne "Offline");
-
-AIM::add_event_handler("event_buddy_signon", "say_hello");
-AIM::add_timeout_handler(600, "notify");
+GAIM::add_event_handler("event_buddy_signon", "say_hello");
+GAIM::add_timeout_handler(600, "notify");
 
 sub echo_reply {
 	$args = @_;
 	$args =~ s/\"//g;
-	AIM::print_to_conv($args, "Hello");
+	GAIM::print_to_conv($args, "Hello");
 }
 
 sub notify {
-	AIM::print("10 minutes", "gaim test has been loaded for 10 minutes");
+	GAIM::print("10 minutes", "gaim test has been loaded for 10 minutes");
 }
 
 sub goodbye {
-	AIM::print("You Bastard!", "You killed Kenny!");
+	GAIM::print("You Bastard!", "You killed Kenny!");
 }
