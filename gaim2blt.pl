b3249d41ba14ea30de565e7e37d1607e764d613d
Eric Warmenhoven <warmenhoven@yahoo.com>
2001-04-05 02:58:29 +0000

    [gaim-migrate @ 1696] good fixes.
    --HG--
    extra : convert_revision : 5f5caaf1e8bb9889bdd19476a01e58d3756d528c

--- gaim2blt.pl
+++ gaim2blt.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl 
 
 ##  gaim buddy list to aim blt format converter
-
+my $top = 1;
 if ($ARGV[0] && $ARGV[1]) {
 	open (FILE,$ARGV[0]);
-	print "Config {\n version 1\n}\nUser {\n screename $ARGV[1]\n}\nBuddy {\n list {\n  ";
+	print "Config {\n version 1\n}\nUser {\n screename $ARGV[1]\n}\nBuddy {\n list {\n";
 	foreach $line (<FILE>) {
 		chomp ($line);
 		$line =~ s/^ +//;			## strip any preceding spaces
@@ -18,13 +18,15 @@ if ($ARGV[0] && $ARGV[1]) {
 		@screenname = split(/:/,$buddy);	## split off the aliases
 		
 		if ($line =~ /^g/) {
-			print "  }\n  $screenname[0] {\n";
+			print "  }\n" unless $top;
+			print "  $screenname[0] {\n";
+			$top = 0;
 		} elsif ($line =~ /^b/) {
 			print "   $screenname[0]\n";
 		}
 	$buddy = undef;
 	}
-	print "  }\n";
+	print "  }\n }\n}\n";
 } else {
 	print "\n\n\n\tExample:\n\n\t\tgaim2blt.pl gaim.buddy.file YourScreenName\n\n\n\n";
 }
