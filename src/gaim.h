1e5f3b059bc821eede23d36d0ed70315cb3016f4
Rob Flynn <gaim@robflynn.com>
2001-04-27 23:53:19 +0000

    [gaim-migrate @ 1777] Removing some crap we dont need anymore.
    --HG--
    extra : convert_revision : 9f6a9eed6a6ae59c777eb20ba00537fb1374645f

--- src/gaim.h
+++ src/gaim.h
@@ -80,9 +80,6 @@
 #define PLUGIN_DIR ".gaim/plugins/"
 
 #define WEBSITE "http://gaim.sourceforge.net/"
-#define REG_EMAIL_ADDR "gaiminfo@blueridge.net"
-#define REG_SRVR "blueridge.net"
-#define REG_PORT 25
 
 #define FACE_ANGEL 0
 #define FACE_BIGSMILE 1
