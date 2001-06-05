9ef61ea63c841e11a0ffa37df6153cfd77833362
Eric Warmenhoven <warmenhoven@yahoo.com>
2001-06-05 17:47:39 +0000

    [gaim-migrate @ 1974] fun stuff
    --HG--
    extra : convert_revision : 96e0d06279e732f81b67d0fda8df22df285f52ee

--- src/gtkimhtml.c
+++ src/gtkimhtml.c
@@ -1342,7 +1342,7 @@ gtk_imhtml_adjustment_changed (GtkAdjustment *adjustment,
 	window = GTK_LAYOUT (imhtml)->bin_window;
 	gdk_window_get_size (window, &width, &height);
 	gdk_window_clear_area (window, 0, 0, width, BORDER_SIZE + 10);
-	gdk_window_clear_area (window, 0, height - BORDER_SIZE, width, BORDER_SIZE);
+	gdk_window_clear_area (window, 0, height - MIN_HEIGHT, width, MIN_HEIGHT);
 
 	gtk_imhtml_draw_exposed (imhtml);
 }
