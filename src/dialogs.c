24711d9e93dc6c23e756e76d8be472de0d7bbeaf
Eric Warmenhoven <warmenhoven@yahoo.com>
2001-09-20 08:33:42 +0000

    [gaim-migrate @ 2331] i need to rethink destroy_all_dialogs
    --HG--
    extra : convert_revision : a6b0e82586bb5d41fed4cd080df9b2f7a556cb68

--- src/dialogs.c
+++ src/dialogs.c
@@ -1721,7 +1721,6 @@ void g_show_info_text(char *info, ...)
 	va_start(ap, info);
 
         b->window = gtk_window_new(GTK_WINDOW_DIALOG);
-        dialogwindows = g_list_prepend(dialogwindows, b->window);
 	gtk_window_set_title(GTK_WINDOW(b->window), "Gaim");
         gtk_container_border_width(GTK_CONTAINER(b->window), 5);
         bbox = gtk_vbox_new(FALSE, 5);
