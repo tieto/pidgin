26d8e61ce8bf5f5a3287bc9aa4361c9771d601cf
Eric Warmenhoven <warmenhoven@yahoo.com>
2001-11-14 06:20:56 +0000

    [gaim-migrate @ 2728] this is what the old one did
    --HG--
    extra : convert_revision : 7e8fe8b7948adeac2831a3a7777d155945a906bc

--- src/protocols/yahoo/yahoo.c
+++ src/protocols/yahoo/yahoo.c
@@ -483,7 +483,18 @@ static void yahoo_process_message(struct gaim_connection *gc, struct yahoo_packe
 	}
 
 	if (pkt->status == 1) {
+		char *m;
+		int i, j;
 		strip_linefeed(msg);
+		m = msg;
+		for (i = 0, j = 0; m[i]; i++) {
+			if (m[i] == 033) {
+				while (m[i] != 'm')
+					i++;
+				continue;
+			}
+			msg[j++] = m[i];
+		}
 		serv_got_im(gc, from, msg, 0, tm);
 	} else if (pkt->status == 2) {
 		do_error_dialog(_("Your message did not get sent."), _("Gaim - Error"));
