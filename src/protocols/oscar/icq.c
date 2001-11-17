841895fa74c080271a62075fce0784c0efa19853
Eric Warmenhoven <warmenhoven@yahoo.com>
2001-11-17 02:53:55 +0000

    [gaim-migrate @ 2745] fine
    --HG--
    extra : convert_revision : 7f4992a72be8d898a67c6e80d5f320adfd59e1a3

--- src/protocols/oscar/icq.c
+++ src/protocols/oscar/icq.c
@@ -122,7 +122,7 @@ static int icqresponse(aim_session_t *sess, aim_module_t *mod, aim_frame_t *rx, 
 
 	if (!(tl = aim_readtlvchain(bs)) || !(datatlv = aim_gettlv(tl, 0x0001, 1))) {
 		if (tl)
-			aim_freetlvchain(tl);
+			aim_freetlvchain(&tl);
 		faimdprintf(sess, 0, "corrupt ICQ response\n");
 		return 0;
 	}
@@ -165,7 +165,7 @@ static int icqresponse(aim_session_t *sess, aim_module_t *mod, aim_frame_t *rx, 
 			ret = userfunc(sess, rx);
 	}
 
-	aim_freetlvchain(tl);
+	aim_freetlvchain(&tl);
 
 	return ret;
 }
