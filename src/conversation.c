fa0ba4c7c8f62124d66b6630689958b7af54c8cd
Eric Warmenhoven <warmenhoven@yahoo.com>
2001-12-11 10:35:46 +0000

    [gaim-migrate @ 2887] t h i s
    i
    s
    
    n
    o
    t
    
    a
    
    t
    e
    s
    t
    .
    
    
    
    t
    h
    i
    s
    
    i
    s
    
    n
    o
    t
    
    a
    
    t
    e
    s
    t
    .
    
    
    
    t
    h
    i
    s
    
    i
    s
    
    n
    o
    t
    
    a
    
    t
    e
    s
    t
    .
    
    --HG--
    extra : convert_revision : 764aefc04c17f5868e1889b788566f78602c4ade

--- src/conversation.c
+++ src/conversation.c
@@ -59,6 +59,7 @@
 #include "pixmaps/crazy4.xpm"
 #include "pixmaps/mrt.xpm"
 #include "pixmaps/download.xpm"
+#include "pixmaps/farted.xpm"
 
 static gchar *ispell_cmd[] = { "ispell", "-a", NULL };
 
@@ -105,6 +106,7 @@ void gaim_setup_imhtml(GtkWidget *imhtml)
 	gtk_imhtml_associate_smiley(GTK_IMHTML(imhtml), ":-o)))", mrt_xpm);
 	gtk_imhtml_associate_smiley(GTK_IMHTML(imhtml), ":-O)))", mrt_xpm);
 	gtk_imhtml_associate_smiley(GTK_IMHTML(imhtml), "8-|)", download_xpm);
+	gtk_imhtml_associate_smiley(GTK_IMHTML(imhtml), ":-])", farted_xpm);
 }
 
 void quiet_set(GtkWidget *tb, int state)
