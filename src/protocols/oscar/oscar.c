2f3606abb92416ccb33c346487b7e98f020c6041
Rob Flynn <gaim@robflynn.com>
2002-03-07 00:46:10 +0000

    [gaim-migrate @ 3029] Is this retarded? Someone tell me.
    --HG--
    extra : convert_revision : b6235276d5b0407f710acd7889fcb4e5add0bf46

--- src/protocols/oscar/oscar.c
+++ src/protocols/oscar/oscar.c
@@ -2412,7 +2412,7 @@ static void oscar_keepalive(struct gaim_connection *gc) {
 }
 
 static char *oscar_name() {
-	return "Oscar";
+	return "Oscar / ICQ";
 }
 
 static int oscar_send_typing(struct gaim_connection *gc, char *name, int typing) {
