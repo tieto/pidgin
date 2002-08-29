987073a486b0aabc52d8184ad1917e483f099891
Sean Egan <seanegan@pidgin.im>
2002-08-29 03:22:21 +0000

    [gaim-migrate @ 3517] Some people are complaining that reporting version 6 is making them fail to log in.  So let's use version 9.
    --HG--
    extra : convert_revision : 890091f40e88ea3528871e4d89ca7cea7c68d55b

--- src/protocols/yahoo/yahoo.c
+++ src/protocols/yahoo/yahoo.c
@@ -87,6 +87,8 @@ extern char *yahoo_crypt(char *, char *);
 #define USEROPT_PAGERPORT 4
 #define YAHOO_PAGER_PORT 5050
 
+#define YAHOO_PROTO_VER 0x0900
+
 enum yahoo_service { /* these are easier to see in hex */
 	YAHOO_SERVICE_LOGON = 1,
 	YAHOO_SERVICE_LOGOFF,
@@ -346,7 +348,7 @@ static int yahoo_send_packet(struct yahoo_data *yd, struct yahoo_packet *pkt)
 	data = g_malloc0(len + 1);
 
 	memcpy(data + pos, "YMSG", 4); pos += 4;
-	pos += yahoo_put16(data + pos, 0x0600);
+	pos += yahoo_put16(data + pos, YAHOO_PROTO_VER);
 	pos += yahoo_put16(data + pos, 0x0000);
 	pos += yahoo_put16(data + pos, pktlen);
 	pos += yahoo_put16(data + pos, pkt->service);
