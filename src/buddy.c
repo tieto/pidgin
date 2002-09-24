fe77d3c6b1b7c11cf3b63f760b585c77cf75d8c5
Luke Schierer <lschiere@pidgin.im>
2002-09-24 00:46:39 +0000

    [gaim-migrate @ 3619] now the blist won't steal focus from a conversation
    --HG--
    extra : convert_revision : f281b6b37825fb8a3421222d63c53f4567f3a1d3

--- src/buddy.c
+++ src/buddy.c
@@ -2352,7 +2352,7 @@ void set_buddy(struct gaim_connection *gc, struct buddy *b)
 			if (bs->sound != 2)
 				play_sound(SND_BUDDY_ARRIVE);
 			if (blist_options & OPT_BLIST_POPUP)
-				unhide_buddy_list();
+				gdk_window_show(blist->window);
 			pm = gdk_pixmap_create_from_xpm_d(blist->window, &bm,
 							  NULL, (char **)login_icon_xpm);
 			gtk_widget_hide(bs->pix);
@@ -2411,7 +2411,7 @@ void set_buddy(struct gaim_connection *gc, struct buddy *b)
 		if (bs->sound != 1)
 			play_sound(SND_BUDDY_LEAVE);
 		if (blist_options & OPT_BLIST_POPUP)
-			unhide_buddy_list();
+			gdk_window_show(blist->window);
 		bs->connlist = g_slist_remove(bs->connlist, gc);
 		update_num_group(gs);
 		if (bs->log_timer > 0)
