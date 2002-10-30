67191e22c7c39470b9e59b830be4d40515761397
Rob Flynn <gaim@robflynn.com>
2002-10-30 00:44:49 +0000

    [gaim-migrate @ 3973] This pisses me off less.
    --HG--
    extra : convert_revision : 992b6ad5b05757c3da5ea458a9444a4bf9f19389

--- src/dialogs.c
+++ src/dialogs.c
@@ -706,6 +706,7 @@ void show_im_dialog()
 		gtk_container_add(GTK_CONTAINER(hbox), vbox);
 		
 		label = gtk_label_new(_("Please enter the screenname of the person you would like to IM.\n"));
+		gtk_widget_set_usize(GTK_WIDGET(label), 350, -1);
 		gtk_label_set_line_wrap(GTK_LABEL(label), TRUE);
 		gtk_misc_set_alignment(GTK_MISC(label), 0, 0);
 		gtk_box_pack_start(GTK_BOX(vbox), label, FALSE, FALSE, 0);
