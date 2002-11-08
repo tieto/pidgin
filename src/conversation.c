c08c0ededee7aea6234bab6aca812bd0628b4efe
Rob Flynn <gaim@robflynn.com>
2002-11-08 06:28:02 +0000

    [gaim-migrate @ 4097] okay
    --HG--
    extra : convert_revision : 5324e66149059af47095e0aecd9f66ca09cf6537

--- src/conversation.c
+++ src/conversation.c
@@ -2841,6 +2841,7 @@ void show_conv(struct conversation *c)
 	sw = gtk_scrolled_window_new(NULL, NULL);
 	c->sw = sw;
 	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sw), GTK_POLICY_NEVER, GTK_POLICY_ALWAYS);
+	gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(sw), GTK_SHADOW_IN);
 	gtk_box_pack_start(GTK_BOX(vbox), sw, TRUE, TRUE, 0);
 	gtk_widget_set_usize(sw, conv_size.width, conv_size.height);
 	gtk_widget_show(sw);
