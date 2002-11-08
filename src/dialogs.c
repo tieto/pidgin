73aa2b0c0aafd5d5110f879f3a34f031c36b2c11
Rob Flynn <gaim@robflynn.com>
2002-11-08 06:30:46 +0000

    [gaim-migrate @ 4098] forgot this one
    --HG--
    extra : convert_revision : 21a0eebf03de507da42543eea1e3e9bea7e3328a

--- src/dialogs.c
+++ src/dialogs.c
@@ -2360,6 +2360,7 @@ void g_show_info_text(struct gaim_connection *gc, char *who, int away, char *inf
 
 		sw = gtk_scrolled_window_new(NULL, NULL);
 		gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sw), GTK_POLICY_NEVER, GTK_POLICY_ALWAYS);
+		gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(sw), GTK_SHADOW_IN);
 		gtk_box_pack_start(GTK_BOX(bbox), sw, TRUE, TRUE, 0);
 
 		text = gtk_imhtml_new(NULL, NULL);
