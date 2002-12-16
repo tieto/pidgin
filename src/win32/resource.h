d3cfc765e8b06aa38e8044833ee4bdbff05c25e1
Herman Bloggs <herman@bluedigits.com>
2002-12-16 22:23:53 +0000

    [gaim-migrate @ 4301] Changed icon defines
    --HG--
    extra : convert_revision : 5c1a8628c83db8290279d6252c907d43f505e41f

--- src/win32/resource.h
+++ src/win32/resource.h
@@ -1,3 +1,3 @@
-#define IDI_ICON2                       104
-#define IDI_ICON3                       105
-#define IDI_ICON4                       106
+#define GAIM_ICON                       104
+#define GAIM_OFFLINE_ICON               105
+#define GAIM_AWAY_ICON                  106
