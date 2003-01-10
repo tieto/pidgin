a4dd334460f5cdf4d2d201ef49a0b725426f2f05
Robert McQueen <robot101@debian.org>
2003-01-10 22:41:04 +0000

    [gaim-migrate @ 4531] (17:39:44) Robot101: this patch to back out a change that made the OPT_BLIST_POPUP steal focus in an evil way (17:39:45) Robot101: http://people.debian.org/~robot101/blist-avoid-focus-stealing3.diff (17:39:56) Robot101: (entirely my fault)
    committer: Luke Schierer <lschiere@pidgin.im>
    
    --HG--
    extra : convert_revision : a369c433c71b5642238f71985d8e5e7b10ad1f03

--- src/buddy.c
+++ src/buddy.c
@@ -2374,7 +2374,7 @@ void set_buddy(struct gaim_connection *gc, struct buddy *b)
 			if (bs->sound != 2)
 				play_sound(SND_BUDDY_ARRIVE);
 			if (blist_options & OPT_BLIST_POPUP)
-				gtk_window_present(GTK_WINDOW(blist));
+				gdk_window_show(blist->window);
 			pm = gdk_pixmap_create_from_xpm_d(blist->window, &bm,
 							  NULL, (char **)login_icon_xpm);
 			gtk_widget_hide(bs->pix);
@@ -2435,7 +2435,7 @@ void set_buddy(struct gaim_connection *gc, struct buddy *b)
 		if (bs->sound != 1)
 			play_sound(SND_BUDDY_LEAVE);
 		if (blist_options & OPT_BLIST_POPUP)
-			gtk_window_present(GTK_WINDOW(blist));
+			gdk_window_show(blist->window);
 		bs->connlist = g_slist_remove(bs->connlist, gc);
 		update_num_group(gs);
 		if (bs->log_timer > 0)
