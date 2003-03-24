ff610288955093a7858d0f0b41e59314d90ed33b
Herman Bloggs <herman@bluedigits.com>
2003-03-24 18:25:40 +0000

    [gaim-migrate @ 5209] Windows needs the buddy window to be realized
    --HG--
    extra : convert_revision : f7d421125818c97919d82b1f7ac2615932ddd1f7

--- src/buddy.c
+++ src/buddy.c
@@ -1052,6 +1052,7 @@ static void gaim_gtk_blist_show(struct gaim_buddy_list *list)
 	gtk_window_set_gravity(GTK_WINDOW(gtkblist->window), GDK_GRAVITY_NORTH_WEST);
 	gtk_window_set_role(GTK_WINDOW(gtkblist->window), "buddy_list");
 	gtk_window_set_title(GTK_WINDOW(gtkblist->window), _("Buddy List"));
+	gtk_widget_realize(gtkblist->window);
 
 	gtkblist->vbox = gtk_vbox_new(FALSE, 6);
 	gtk_container_add(GTK_CONTAINER(gtkblist->window), gtkblist->vbox);
