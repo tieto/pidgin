3d452e596490dfe400c7e052a9480d581c8b5e2c
Herman Bloggs <herman@bluedigits.com>
2003-04-05 01:10:13 +0000

    [gaim-migrate @ 5340] Initial import
    --HG--
    extra : convert_revision : a480f0b9c275d1a6fb75d409a707acb0b67889ce

---
+++ src/win32/untar.h
@@ -0,0 +1,22 @@
+/*
+ *  untar.h
+ *
+ *  Author: Herman Bloggs <hermanator12002@yahoo.com>
+ *  Date: April, 2003
+ *  Description: untar.c header
+ */
+#ifndef _UNTAR_H_
+#define _UNTAR_H_
+
+typedef enum _untar_opt {
+	UNTAR_LISTING =      (1 << 0),
+	UNTAR_QUIET =        (1 << 1),
+	UNTAR_VERBOSE =      (1 << 2),
+	UNTAR_FORCE =        (1 << 3),
+	UNTAR_ABSPATH =      (1 << 4),
+	UNTAR_CONVERT =      (1 << 5)
+} untar_opt;
+
+int untar(const char *filename, const char *destdir, untar_opt options);
+
+#endif
