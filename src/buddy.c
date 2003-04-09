3c3f2d6d38c8fdfdbed4326f0330ef0a4c589e14
Joe Clarke <none@none>
2003-04-09 23:28:47 +0000

    [gaim-migrate @ 5451] Joe Clarke (jmarcus) writes: "The accelerator keys (e.g. ctl+q, ctl+i, etc.) do not work in GAIM 0.60. I think that's because there is no accelerator group attached to the main GAIM window. The attached patch corrects the problem."
    committer: Luke Schierer <lschiere@pidgin.im>
    
    --HG--
    extra : convert_revision : cdc4a6fdd5ea8d0bcb1b66019ee69c734039c7e3

--- src/buddy.c
+++ src/buddy.c
@@ -1112,6 +1112,7 @@ static void gaim_gtk_blist_show(struct gaim_buddy_list *list)
 	GtkWidget *sw;
 	GtkWidget *button;
 	GtkSizeGroup *sg;
+	GtkAccelGroup *accel_group;
 	GtkTreeSelection *selection;
 	GtkTargetEntry gte[] = {{"GAIM_BLIST_NODE", GTK_TARGET_SAME_APP, DRAG_ROW},
 				{"application/x-im-contact", 0, DRAG_BUDDY}};
@@ -1136,7 +1137,10 @@ static void gaim_gtk_blist_show(struct gaim_buddy_list *list)
 	gtk_widget_add_events(gtkblist->window, GDK_VISIBILITY_NOTIFY_MASK);
 
 	/******************************* Menu bar *************************************/
-	ift = gtk_item_factory_new(GTK_TYPE_MENU_BAR, "<GaimMain>", NULL);
+	accel_group = gtk_accel_group_new();
+	gtk_window_add_accel_group(GTK_WINDOW (gtkblist->window), accel_group);
+	g_object_unref(accel_group);
+	ift = gtk_item_factory_new(GTK_TYPE_MENU_BAR, "<GaimMain>", accel_group);
 	gtk_item_factory_set_translate_func (ift,
 					     item_factory_translate_func,
 					     NULL, NULL);
