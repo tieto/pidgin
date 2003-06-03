cb4d8ea5b87fd12784ce79c4831b6ebc4509f2a9
Sean Egan <seanegan@pidgin.im>
2003-06-03 21:05:12 +0000

    [gaim-migrate @ 6144] You must now have at least autoconf 2.50 to compile Gaim.
    --HG--
    extra : convert_revision : 84863ed037f72187c3f29c84ee5505c391c61a5f

--- autogen.sh
+++ autogen.sh
@@ -30,16 +30,6 @@ SETUP_GETTEXT=./setup-gettext
 	exit;
 }
 
-# Thanks decklin
-if test -f configure.ac ; then
-	if autoconf --version | grep '2\.[01]' > /dev/null 2>&1 ; then
-		mv configure.ac configure.2.1x;
-		echo "configure.ac has been moved to configure.2.1x to retain compatibility with autoconf 2.1x"
-		echo "Future versions of Gaim will not support autoconf versions older than 2.50"
-
-	fi
-fi
-
 echo "Generating configuration files for Gaim, please wait...."
 echo;
 
