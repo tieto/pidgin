6eaef50fd8dd79bbb24f1854ee926b4ed31c5901
Herman Bloggs <herman@bluedigits.com>
2003-07-10 23:42:50 +0000

    [gaim-migrate @ 6541] New docklet icons
    --HG--
    extra : convert_revision : 39c723343a2d91d4de4598d93f086e93d23c21b0

--- src/win32/resource.h
+++ src/win32/resource.h
@@ -4,3 +4,5 @@
 #define GAIM_TRAY_ICON                  107
 #define GAIM_OFFLINE_TRAY_ICON          108
 #define GAIM_AWAY_TRAY_ICON             109
+#define GAIM_PEND_TRAY_ICON             110
+#define GAIM_AWAYPEND_TRAY_ICON         111
