b571b97d5021e75a10aaf62dea25efa470675f3b
Christian Hammond <chipx86@chipx86.com>
2003-07-11 22:13:57 +0000

    [gaim-migrate @ 6553] I broke the Pause button a few days ago. Now it's fixed.
    --HG--
    extra : convert_revision : a6d8f050347f005297c8adc114bb7de62e883904

--- src/gtkdebug.c
+++ src/gtkdebug.c
@@ -125,6 +125,7 @@ debug_window_new(void)
 	GtkWidget *toolbar;
 	GtkWidget *sw;
 	GtkWidget *button;
+	GtkWidget *image;
 	int width, height;
 
 	win = g_new0(DebugWindow, 1);
@@ -175,8 +176,11 @@ debug_window_new(void)
 		gtk_toolbar_insert_space(GTK_TOOLBAR(toolbar), -1);
 
 		/* Pause */
-		gtk_toolbar_insert_stock(GTK_TOOLBAR(toolbar), GAIM_STOCK_PAUSE,
-								 NULL, NULL, G_CALLBACK(pause_cb), win, -1);
+		image = gtk_image_new_from_stock(GAIM_STOCK_PAUSE, GTK_ICON_SIZE_MENU);
+		button = gtk_toolbar_append_element(GTK_TOOLBAR(toolbar),
+											GTK_TOOLBAR_CHILD_TOGGLEBUTTON,
+											NULL, _("Pause"), NULL, NULL,
+											image, G_CALLBACK(pause_cb), win);
 
 		/* Timestamps */
 		button = gtk_toolbar_append_element(GTK_TOOLBAR(toolbar),
