8ca7ba0a009335e0c644e9401a504c928e852952
Mark Doliner <markdoliner@pidgin.im>
2003-07-13 18:57:34 +0000

    [gaim-migrate @ 6573] Change "Please enter your password" to "Enter password for [account name]"
    --HG--
    extra : convert_revision : 4ae222cda6999210edaea92632cb6e85677ff183

--- src/connection.c
+++ src/connection.c
@@ -118,12 +118,14 @@ gaim_connection_connect(GaimConnection *gc)
 		!(prpl_info->options & OPT_PROTO_PASSWORD_OPTIONAL) &&
 		gaim_account_get_password(account) == NULL) {
 
+		gchar *primary;
 		gaim_debug(GAIM_DEBUG_INFO, "connection", "Requesting password\n");
-
-		gaim_request_input(gc, NULL, _("Please enter your password"), NULL, 
-						   NULL, FALSE, TRUE,
+		primary = g_strdup_printf(_("Enter password for %s"),
+								 gaim_account_get_username(account));
+		gaim_request_input(gc, NULL, primary, NULL, NULL, FALSE, TRUE,
 						   _("OK"), G_CALLBACK(request_pass_ok_cb),
 						   _("Cancel"), NULL, account);
+		g_free(primary);
 		gaim_connection_destroy(gc);
 
 		return;
