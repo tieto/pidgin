ea14fefc8f66285d509cc0edf6ee6d69bd6b120c
Rob Flynn <gaim@robflynn.com>
2003-08-05 03:21:40 +0000

    [gaim-migrate @ 6875] I like cheese.
    --HG--
    extra : convert_revision : e43b03c610d9820461bc32b38719ac987dca950d

--- src/prpl.h
+++ src/prpl.h
@@ -61,6 +61,7 @@ typedef enum
 	GAIM_PROTO_MOO,         /**< MOO protocol              */
 	GAIM_PROTO_ELVIN,       /**< Elvin Protocol            */
 	GAIM_PROTO_TREPIA,      /**< Trepia IM protocol        */
+	GAIM_PROTO_BATTLENET,   /**< Battle.NET protocol       */
 	GAIM_PROTO_UNTAKEN      /**< Untaken protocol number   */
 
 } GaimProtocol;
