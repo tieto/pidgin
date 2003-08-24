bb2ca2bccc469e51e7ba3c42412b06a1b9a8b446
Christian Hammond <chipx86@chipx86.com>
2003-08-24 02:43:42 +0000

    [gaim-migrate @ 7116] Updated test.pl
    --HG--
    extra : convert_revision : cf383a0a7bda93f65e50a337faa0e82f8d27c3a1

--- plugins/test.pl
+++ plugins/test.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl -w
 
 use Gaim;
-use vars qw(%PLUGIN_INFO);
 
 %PLUGIN_INFO = (
 	perl_api_version => 2,
@@ -13,19 +12,45 @@ use vars qw(%PLUGIN_INFO);
 	url              => 'http://gaim.sf.net/',
 
 	load             => "plugin_load",
-	unload           => "plugin_unload",
+	unload           => "plugin_unload"
 );
+
+sub account_away_cb {
+	Gaim::debug_info("perl test plugin", "In account_away_cb\n");
+
+	my ($account, $state, $message, $data) = @_;
+
+	Gaim::debug_info("perl test plugin", "Account " .
+	                 $account->get_username() . " went away.\n");
+	Gaim::debug_info("perl test plugin", $data . "\n");
+}
 
 sub plugin_init {
 	return %PLUGIN_INFO;
 }
 
 sub plugin_load {
+	Gaim::debug_info("perl test plugin", "plugin_load\n");
 	my $plugin = shift;
 
+	Gaim::debug_info("perl test plugin", "Listing accounts.\n");
 	foreach $account (Gaim::accounts()) {
-		Gaim::debug("perl test plugin", $account->get_username() . "\n");
+		Gaim::debug_info("perl test plugin", $account->get_username() . "\n");
 	}
+
+	Gaim::debug_info("perl test plugin", "Listing buddy list.\n");
+	foreach $group (Gaim::BuddyList::groups()) {
+		Gaim::debug_info("perl test plugin",
+		                 $group->get_name() . ":\n");
+
+		foreach $buddy ($group->buddies()) {
+			Gaim::debug_info("perl test plugin",
+			                 "  " . $buddy->get_name() . "\n");
+		}
+	}
+
+	Gaim::signal_connect(Gaim::Accounts::handle, "account-away",
+	                     $plugin, \&account_away_cb, "test");
 }
 
 sub plugin_unload {
