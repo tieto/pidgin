bafc3fbbeed173fa5af15c752d56bc980bf7032f
Christian Hammond <chipx86@chipx86.com>
2003-09-19 00:39:27 +0000

    [gaim-migrate @ 7438] Disable MSNSLP.
    --HG--
    extra : convert_revision : d84ee4a7cea990ac46e8199ce4ea4f7ea12f0ac6

--- src/protocols/msn/switchboard.c
+++ src/protocols/msn/switchboard.c
@@ -91,7 +91,7 @@ ans_cmd(MsnServConn *servconn, const char *command, const char **params,
 
 	//send_clientcaps(swboard);
 
-	if (session->protocol_ver >= 9)
+	if (0 && session->protocol_ver >= 9)
 	{
 		MsnUser *local_user, *remote_user;
 
