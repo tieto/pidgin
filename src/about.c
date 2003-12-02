aee8529677dd7efec8c6e1f977ca34a621835cb0
Luke Schierer <lschiere@pidgin.im>
2003-12-02 02:12:35 +0000

    [gaim-migrate @ 8335] because documenting ssl for us counts, i give you.... nosnilmot
    --HG--
    extra : convert_revision : 9592aa461466f7cb923f6d26e04089efb342eca2

--- src/about.c
+++ src/about.c
@@ -128,6 +128,7 @@ void show_about(GtkWidget *w, void *data)
 			"  Robert 'Robot101' McQueen<BR>"
 			"  Tim 'marv' Ringenbach<br>"
 			"  Kevin 'SimGuy' Stange<br>"
+			"  Stu 'nosnilmot' Tomlinson<br>"
 			"<BR>", NULL);
 		gtk_imhtml_append_text(GTK_IMHTML(text), str, GTK_IMHTML_NO_SCROLL);
 		g_free(str);
