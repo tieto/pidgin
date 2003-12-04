f48c9d01836ca7316d1730d1b7ce09b061d94cc4
Sean Egan <seanegan@pidgin.im>
2003-12-04 05:31:54 +0000

    [gaim-migrate @ 8382] I swear, these ellipses keep sneaking in here.
    --HG--
    extra : convert_revision : 1a6af2aabd21eb082db274abdf501a588a966432

--- src/gtkblist.c
+++ src/gtkblist.c
@@ -1698,7 +1698,7 @@ static GtkItemFactoryEntry blist_menu[] =
 	{ N_("/Tools/P_rotocol Actions"), NULL, NULL, 0, "<Branch>" },
 	{ "/Tools/sep1", NULL, NULL, 0, "<Separator>" },
 	{ N_("/Tools/A_ccounts"), "<CTL>A", gaim_gtk_accounts_window_show, 0, "<StockItem>", GAIM_STOCK_ACCOUNTS },
-	{ N_("/Tools/_File Transfers..."), NULL, gaim_show_xfer_dialog, 0, "<StockItem>", GAIM_STOCK_FILE_TRANSFER },
+	{ N_("/Tools/_File Transfers"), NULL, gaim_show_xfer_dialog, 0, "<StockItem>", GAIM_STOCK_FILE_TRANSFER },
 	{ N_("/Tools/Preferences"), "<CTL>P", gaim_gtk_prefs_show, 0, "<StockItem>", GTK_STOCK_PREFERENCES },
 	{ N_("/Tools/Pr_ivacy"), NULL, gaim_gtk_privacy_dialog_show, 0, "<StockItem>", GAIM_STOCK_PRIVACY },
 	{ "/Tools/sep2", NULL, NULL, 0, "<Separator>" },
