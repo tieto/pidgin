a726dcf5f06392adbcf299ce322cc51c5ed496b7
Nathan Walp <nwalp@pidgin.im>
2004-01-06 08:07:39 +0000

    [gaim-migrate @ 8702] we don't really need this either
    --HG--
    extra : convert_revision : 1272f357b593ef89a9dc4fb955a0a0a6f4e7865f

--- src/gtkblist.c
+++ src/gtkblist.c
@@ -1711,8 +1711,8 @@ static GtkItemFactoryEntry blist_menu[] =
 	{ N_("/Tools/_File Transfers"), NULL, gaim_show_xfer_dialog, 0, "<StockItem>", GAIM_STOCK_FILE_TRANSFER },
 	{ N_("/Tools/Pr_eferences"), "<CTL>P", gaim_gtk_prefs_show, 0, "<StockItem>", GTK_STOCK_PREFERENCES },
 	{ N_("/Tools/Pr_ivacy"), NULL, gaim_gtk_privacy_dialog_show, 0, NULL },
-	{ "/Tools/sep2", NULL, NULL, 0, "<Separator>" },
 #if 0
+	{ "/Tools/sep2", NULL, NULL, 0, "<Separator>" },
 	{ N_("/Tools/View System _Log"), NULL, gtk_blist_show_systemlog_cb, 0, NULL },
 #endif
 
