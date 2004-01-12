3b769b3dfc8b98b01e20683c738383877bc2d1bc
Nathan Walp <nwalp@pidgin.im>
2004-01-12 05:47:57 +0000

    [gaim-migrate @ 8781] re-fix static prpls now that perl is un-broken.  or something like that.
    --HG--
    extra : convert_revision : 868e228bc24ef6ba9dba83259360d67d972d6f28

--- src/plugin.h
+++ src/plugin.h
@@ -117,8 +117,7 @@ struct _GaimPlugin
 /**
  * Handles the initialization of modules.
  */
-//if !defined(GAIM_PLUGINS) || defined(STATIC)
-#if !defined(GAIM_PLUGINS)
+#if !defined(GAIM_PLUGINS) || defined(GAIM_STATIC_PRPL)
 # define GAIM_INIT_PLUGIN(pluginname, initfunc, plugininfo) \
 	gboolean gaim_init_##pluginname##_plugin(void) { \
 		GaimPlugin *plugin = gaim_plugin_new(TRUE, NULL); \
@@ -126,8 +125,7 @@ struct _GaimPlugin
 		initfunc((plugin)); \
 		return gaim_plugin_register(plugin); \
 	}
-//else /* GAIM_PLUGINS  && !STATIC */
-#else /* GAIM_PLUGINS */
+#else /* GAIM_PLUGINS  && !GAIM_STATIC_PRPL */
 # define GAIM_INIT_PLUGIN(pluginname, initfunc, plugininfo) \
 	G_MODULE_EXPORT gboolean gaim_init_plugin(GaimPlugin *plugin) { \
 		plugin->info = &(plugininfo); \
