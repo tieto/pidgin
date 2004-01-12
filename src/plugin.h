4013df59399785524d674b92270cddc350201b79
Mark Doliner <markdoliner@pidgin.im>
2004-01-12 05:42:09 +0000

    [gaim-migrate @ 8780] Well this fixes perl plugins for the time being.  If someone wants to fix it for real feel frizee.
    I couldn't find where STATIC is defined.
    
    --HG--
    extra : convert_revision : c6f978bdee7fd2eed226396dfcd3946494a35a00

--- src/plugin.h
+++ src/plugin.h
@@ -117,7 +117,8 @@ struct _GaimPlugin
 /**
  * Handles the initialization of modules.
  */
-#if !defined(GAIM_PLUGINS) || defined(STATIC)
+//if !defined(GAIM_PLUGINS) || defined(STATIC)
+#if !defined(GAIM_PLUGINS)
 # define GAIM_INIT_PLUGIN(pluginname, initfunc, plugininfo) \
 	gboolean gaim_init_##pluginname##_plugin(void) { \
 		GaimPlugin *plugin = gaim_plugin_new(TRUE, NULL); \
@@ -125,7 +126,8 @@ struct _GaimPlugin
 		initfunc((plugin)); \
 		return gaim_plugin_register(plugin); \
 	}
-#else /* GAIM_PLUGINS  && !STATIC */
+//else /* GAIM_PLUGINS  && !STATIC */
+#else /* GAIM_PLUGINS */
 # define GAIM_INIT_PLUGIN(pluginname, initfunc, plugininfo) \
 	G_MODULE_EXPORT gboolean gaim_init_plugin(GaimPlugin *plugin) { \
 		plugin->info = &(plugininfo); \
