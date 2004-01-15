458f7cb12da67b8e191e6a866db8c57c99d37916
Mark Doliner <markdoliner@pidgin.im>
2004-01-15 17:08:38 +0000

    [gaim-migrate @ 8813] Remove the horizontal scroll bar on text input boxes in conversation windows.  Apparently it's possible to do some weird and wild stuff and get gtk to freak out and not be able to make up its mind about whether to actually add the scroll bars or not.
    So don't type really long words and you'll be alright.
    
    In closing, I think I might go get some Quizznos.
    
    --HG--
    extra : convert_revision : 821d0993c86be431f55e4cf3453caa6dd85ce531

--- src/gtkconv.c
+++ src/gtkconv.c
@@ -3707,7 +3707,7 @@ setup_chat_pane(GaimConversation *conv)
 	/* Setup the entry widget. */
 	sw = gtk_scrolled_window_new(NULL, NULL);
 	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sw),
-								   GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+								   GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
 	gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(sw),
 										GTK_SHADOW_IN);
 	gtk_box_pack_start(GTK_BOX(vbox), sw, TRUE, TRUE, 0);
@@ -3807,7 +3807,7 @@ setup_im_pane(GaimConversation *conv)
 	/* Setup the entry widget. */
 	sw = gtk_scrolled_window_new(NULL, NULL);
 	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sw),
-								   GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+								   GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
 	gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(sw),
 										GTK_SHADOW_IN);
 	gtk_box_pack_start(GTK_BOX(vbox2), sw, TRUE, TRUE, 0);
