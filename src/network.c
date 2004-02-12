5db5525626477948d6162266f60df6d9327b2cd7
Herman Bloggs <herman@bluedigits.com>
2004-02-12 19:55:05 +0000

    [gaim-migrate @ 8968] Use internal.h
    --HG--
    extra : convert_revision : 981716faa0bb8dc26d0ba2718d609fb794978df6

--- src/network.c
+++ src/network.c
@@ -23,15 +23,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <errno.h>
-#include <unistd.h>
-#include <string.h>
-#include <netdb.h>
-#include <fcntl.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <sys/stat.h>
-#include <arpa/inet.h>
+#include "internal.h"
 
 #include "debug.h"
 #include "account.h"
