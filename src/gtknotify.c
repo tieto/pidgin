ac5fc08b1f764d0b68c9b7b6aa57dcb78a13f548
Nathan Walp <nwalp@pidgin.im>
2004-02-26 17:28:48 +0000

    [gaim-migrate @ 9069] i think this might crash less
    --HG--
    extra : convert_revision : a0ea280a2be2160c72058be5e50e6e618dfccae1

--- src/gtknotify.c
+++ src/gtknotify.c
@@ -301,7 +301,7 @@ gaim_gtk_notify_formatted(const char *title, const char *primary,
 	char *linked_text;
 
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(GTK_WINDOW(title), title);
+	gtk_window_set_title(GTK_WINDOW(window), title);
 	gtk_window_set_type_hint(GTK_WINDOW(window), GDK_WINDOW_TYPE_HINT_DIALOG);
 	gtk_container_set_border_width(GTK_CONTAINER(window), 12);
 
