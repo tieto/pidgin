9ca54402539b6a051a07ed2404cc06770f1ef3d1
Christian Hammond <chipx86@chipx86.com>
2004-03-19 20:17:31 +0000

    [gaim-migrate @ 9208] Copyright date update.
    --HG--
    extra : convert_revision : 5225c2d48c43819dc29ef43b579f040d954a92c4

--- src/protocols/msn/msg.h
+++ src/protocols/msn/msg.h
@@ -3,7 +3,7 @@
  *
  * gaim
  *
- * Copyright (C) 2003 Christian Hammond <chipx86@gnupdate.org>
+ * Copyright (C) 2003-2004 Christian Hammond <chipx86@gnupdate.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
