72ae7bf199fbdbe57090595598cc0d9a49217cef
Christian Hammond <chipx86@chipx86.com>
2004-04-22 02:28:27 +0000

    [gaim-migrate @ 9505] Compile. I guess TCL wasn't being built here.
    --HG--
    extra : convert_revision : 54d3816bb12e0a6dbc87d647288fe0439fb1a3b0

--- plugins/tcl/tcl.c
+++ plugins/tcl/tcl.c
@@ -330,7 +330,7 @@ static gboolean tcl_unload(GaimPlugin *plugin)
 
 static GaimPluginLoaderInfo tcl_loader_info =
 {
-	GAIM_PLUGIN_LOADER_VERSION,
+	GAIM_PLUGIN_API_VERSION,
 	NULL,
 	tcl_probe_plugin,
 	tcl_load_plugin,
