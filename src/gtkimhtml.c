c68ec7ebe2ccd74afd6862e1d10868db11210f40
Tim Ringenbach <marv@pidgin.im>
2004-05-09 01:39:42 +0000

    [gaim-migrate @ 9679] (21:39:11) marv: LSchiere2: this strips the BOM from the inserted pasted text, while keeping it around during the conversation, thus making marcus's patch more better
    committer: Luke Schierer <lschiere@pidgin.im>
    
    --HG--
    extra : convert_revision : 2cc44009caac8220902adab47427b94bc9338436

--- src/gtkimhtml.c
+++ src/gtkimhtml.c
@@ -763,6 +763,7 @@ static void paste_received_cb (GtkClipboard *clipboard, GtkSelectionData *select
 	if (selection_data->length >= 2 &&
 		(*(guint16 *)text == 0xfeff || *(guint16 *)text == 0xfffe)) {
 		/* This is UCS-2 */
+		char *tmp;
 		char *utf8 = g_convert(text, selection_data->length, "UTF-8", "UCS-2", NULL, NULL, NULL);
 		g_free(text);
 		text = utf8;
@@ -770,6 +771,8 @@ static void paste_received_cb (GtkClipboard *clipboard, GtkSelectionData *select
 			gaim_debug_warning("gtkimhtml", "g_convert from UCS-2 failed in paste_received_cb\n");
 			return;
 		}
+		tmp = g_utf8_next_char(text);
+		memmove(text, tmp, strlen(tmp) + 1);
 	}
 
 	if (!(*text) || !g_utf8_validate(text, -1, NULL)) {
