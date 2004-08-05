a05f4a695565e94f38278b8af25d26940357d0b7
Mark Doliner <markdoliner@pidgin.im>
2004-08-05 21:40:37 +0000

    [gaim-migrate @ 10535] I'm hoping this fixes bug 981034? Here's looking at you, kid.
    --HG--
    extra : convert_revision : a048c3924e0b01b88e7b3b9b21bec9293d308148

--- src/gtkconv.c
+++ src/gtkconv.c
@@ -5761,7 +5761,7 @@ gaim_gtkconv_update_buddy_icon(GaimConversation *conv)
 
 	button_type = gaim_prefs_get_int("/gaim/gtk/conversations/button_type");
 	gtk_widget_size_request(gtkconv->bbox, &requisition);
-	if(button_type == GAIM_BUTTON_NONE || requisition.height < scale_height) {
+	if (button_type == GAIM_BUTTON_NONE || requisition.height * 1.5 < scale_height) {
 		gtk_box_pack_start(GTK_BOX(gtkconv->lower_hbox), vbox, FALSE, FALSE, 0);
 /*		gtk_box_reorder_child(GTK_BOX(gtkconv->lower_hbox), vbox, 0); */
 	} else {
