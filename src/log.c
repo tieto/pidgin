ec43396c6a74c94f4c355cf36e6226fec0b63683
Stu Tomlinson <nosnilmot@pidgin.im>
2004-11-12 21:33:54 +0000

    [gaim-migrate @ 11287] It sneaked in here too
    --HG--
    extra : convert_revision : 0613ee1ca57558ecd9c7536724370066ddb27acd

--- src/log.c
+++ src/log.c
@@ -74,11 +74,12 @@ void gaim_log_free(GaimLog *log)
 void gaim_log_write(GaimLog *log, GaimMessageFlags type,
 		    const char *from, time_t time, const char *message)
 {
+	struct _gaim_logsize_user *lu;
+
 	g_return_if_fail(log);
 	g_return_if_fail(log->logger);
 	g_return_if_fail(log->logger->write);
 
-	struct _gaim_logsize_user *lu;
 	(log->logger->write)(log, type, from, time, message);
 
 	lu = g_new(struct _gaim_logsize_user, 1);
