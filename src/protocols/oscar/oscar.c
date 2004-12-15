a96e3e0f7735f38985d1c511068de0a589195c23
Sean Egan <seanegan@pidgin.im>
2004-12-15 01:56:10 +0000

    [gaim-migrate @ 11601] Of course, though, I meant this.
    --HG--
    extra : convert_revision : 4bb8f4dbb05d95b1c255c70d9f77eb51d5f8eed6

--- src/protocols/oscar/oscar.c
+++ src/protocols/oscar/oscar.c
@@ -378,13 +378,12 @@ oscar_encoding_to_utf8(const char *encoding, const char *text, int textlen)
 	if ((encoding == NULL) || encoding[0] == '\0') {
 		gaim_debug_info("oscar", "Empty encoding, assuming UTF-8\n");
 
-	} else if (!strcmp(encoding, "iso-8859-1")
-                   || !strcmp(encoding, "ISO-8859-1-Windows-3.1-Latin-1")) {
+	} else if (!strcmp(encoding, "iso-8859-1")) {
+		utf8 = g_convert(text, textlen, "UTF-8", "iso-8859-1", NULL, NULL, NULL);
+	} else if (!strcmp(encoding, "ISO-8859-1-Windows-3.1-Latin-1")) {
 		utf8 = g_convert(text, textlen, "UTF-8", "Windows-1252", NULL, NULL, NULL);
-
 	} else if (!strcmp(encoding, "unicode-2-0")) {
 		utf8 = g_convert(text, textlen, "UTF-8", "UCS-2BE", NULL, NULL, NULL);
-
 	} else if (strcmp(encoding, "us-ascii") && strcmp(encoding, "utf-8")) {
 		gaim_debug_warning("oscar", "Unrecognized character encoding \"%s\", "
 						   "attempting to convert to UTF-8 anyway\n", encoding);
