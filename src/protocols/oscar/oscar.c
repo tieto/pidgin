df3bc66744ab901b5746775e4ec8b5671d0d3a9f
Mark Doliner <markdoliner@pidgin.im>
2005-01-04 02:35:29 +0000

    [gaim-migrate @ 11757] Chow down.
    --HG--
    extra : convert_revision : 367209aa3aa01adf4b2c970f68d815d8490b5a07

--- src/protocols/oscar/oscar.c
+++ src/protocols/oscar/oscar.c
@@ -3299,7 +3299,7 @@ static int incomingim_chan1(aim_session_t *sess, aim_conn_t *conn, aim_userinfo_
 	gchar *tmp;
 	aim_mpmsg_section_t *curpart;
 
-	gaim_debug_misc("oscar", "Recived IM from %s with %d parts\n",
+	gaim_debug_misc("oscar", "Received IM from %s with %d parts\n",
 					userinfo->sn, args->mpmsg.numparts);
 
 	bi = g_hash_table_lookup(od->buddyinfo, gaim_normalize(account, userinfo->sn));
