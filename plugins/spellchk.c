8fae32e96aa33c02bc4ffe7c934f0cf485cc1171
Mark Doliner <markdoliner@pidgin.im>
2005-04-16 16:05:40 +0000

    [gaim-migrate @ 12494] La di da di
    --HG--
    extra : convert_revision : f5f2901b7efb3becde9664c36009341cd12f7cd7

--- plugins/spellchk.c
+++ plugins/spellchk.c
@@ -1,7 +1,35 @@
 /*
+ * Gaim - Replace certain misspelled words with their correct form.
+ *
+ * Signification changes were made by Benjamin Kahn ("xkahn") and
+ * Richard Laager ("rlaager") in April 2005--you may want to contact
+ * them if you have questions.
+ *
+ * Gaim is the legal property of its developers, whose names are too numerous
+ * to list here.  Please refer to the COPYRIGHT file distributed with this
+ * source distribution.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+/*
  * A lot of this code (especially the config code) was taken directly
  * or nearly directly from xchat, version 1.4.2 by Peter Zelezny and others.
  */
+
 #include "internal.h"
 #include "gtkgaim.h"
 
