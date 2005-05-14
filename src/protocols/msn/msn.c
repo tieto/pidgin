169759f3912692d3c66157acd3d6e9e884bcd4f9
Stu Tomlinson <nosnilmot@pidgin.im>
2005-05-14 14:40:57 +0000

    [gaim-migrate @ 12671] Prevent the autoreconnect plugin trying to reconnect if there's no SSL support.
    --HG--
    extra : convert_revision : be89c188def9c5125f7213d33f517ebd6c6d3eea

--- src/protocols/msn/msn.c
+++ src/protocols/msn/msn.c
@@ -660,6 +660,7 @@ msn_login(GaimAccount *account, GaimStatus *status)
 
 	if (!gaim_ssl_is_supported())
 	{
+		gc->wants_to_die = TRUE;
 		gaim_connection_error(gc,
 			_("SSL support is needed for MSN. Please install a supported "
 			  "SSL library. See http://gaim.sf.net/faq-ssl.php for more "
