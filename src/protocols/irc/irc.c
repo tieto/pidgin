c30247134e4024be11b5dec091eebea053e6fcd8
Sadrul Habib Chowdhury <sadrul@pidgin.im>
2005-07-09 20:27:57 +0000

    [gaim-migrate @ 13089] sf patch #1233607, from Sadrul H C "The other protocols allows to disconnect by setting the status to "offline" (_set_status). This patch makes IRC do that as well."
    committer: Mark Doliner <markdoliner@pidgin.im>
    
    --HG--
    extra : convert_revision : e49323bab2dbe0d19886bc09170888bdccf67fd6

--- src/protocols/irc/irc.c
+++ src/protocols/irc/irc.c
@@ -453,6 +453,8 @@ static void irc_set_status(GaimAccount *account, GaimStatus *status)
 
 	if (strcmp(status_id, "offline") && !gc) {
 		gaim_account_connect(account);
+	} else if (!strcmp(status_id, "offline") && gc) {
+		gaim_account_disconnect(account);
 	} else if (!strcmp(status_id, "away")) {
 		args[0] = gaim_status_get_attr_string(status, "message");
 		irc_cmd_away(irc, "away", NULL, args);
