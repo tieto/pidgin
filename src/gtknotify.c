ca1220339a1e26cb2f2e8a6ca225ebdbe1214564
Mark Doliner <markdoliner@pidgin.im>
2005-08-18 04:34:49 +0000

    [gaim-migrate @ 13493] Rename ignore formatting to show_incoming_formatting here, too
    --HG--
    extra : convert_revision : 157a9c0df578e1f7dc8d680b9356b86836aca1e9

--- src/gtknotify.c
+++ src/gtknotify.c
@@ -407,7 +407,7 @@ gaim_gtk_notify_formatted(const char *title, const char *primary,
 					 G_CALLBACK(formatted_input_cb), NULL);
 
 	/* Add the text to the gtkimhtml */
-	if (gaim_prefs_get_bool("/gaim/gtk/conversations/ignore_formatting"))
+	if (!gaim_prefs_get_bool("/gaim/gtk/conversations/show_incoming_formatting"))
 		options |= GTK_IMHTML_NO_COLOURS | GTK_IMHTML_NO_FONTS | GTK_IMHTML_NO_SIZES;
 
 	options |= GTK_IMHTML_NO_COMMENTS;
