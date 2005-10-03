029a69a667e4c21de7c77483784d8f0aec24db68
Daniel Atallah <datallah@pidgin.im>
2005-10-03 02:59:17 +0000

    [gaim-migrate @ 13865] Fix a msn ft crash that I introduced.
    --HG--
    extra : convert_revision : c1cae2bafcf3217aeb923b394207749ead801658

--- src/protocols/msn/slp.c
+++ src/protocols/msn/slp.c
@@ -156,7 +156,6 @@ msn_xfer_completed_cb(MsnSlpCall *slpcall, const guchar *body,
 					  gsize size)
 {
 	gaim_xfer_set_completed(slpcall->xfer, TRUE);
-	msn_slp_call_close(slpcall);
 }
 
 /**************************************************************************
--- src/protocols/msn/slplink.c
+++ src/protocols/msn/slplink.c
@@ -278,13 +278,15 @@ msg_ack(MsnMessage *msg, void *data)
 	else
 	{
 		/* The whole message has been sent */
-
 		if (slpmsg->flags == 0x20 || slpmsg->flags == 0x1000030)
 		{
-			if ((slpmsg->slpcall != NULL) &&
-				(slpmsg->slpcall->cb != NULL))
+			if (slpmsg->slpcall != NULL)
 			{
-				slpmsg->slpcall->cb(slpmsg->slpcall, NULL, 0);
+				if (slpmsg->slpcall->cb)
+					slpmsg->slpcall->cb(slpmsg->slpcall,
+						NULL, 0);
+
+				msn_slp_call_destroy(slpmsg->slpcall);
 			}
 		}
 	}
