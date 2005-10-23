0746bb28b24e40b56a2a8956855bc079fe89ce7b
Richard Laager <rlaager@pidgin.im>
2005-10-23 23:52:17 +0000

    [gaim-migrate @ 14068] Hi, I'm dumb. I'm putting the tabbing back as it apparently wasn't wrong before and is now. But nosnilmot's right, there may be a better solution to this, in general.
    --HG--
    extra : convert_revision : b469129a83fbb3c836fa1d25cda1c71f492bab8b

--- src/gtkplugin.c
+++ src/gtkplugin.c
@@ -193,7 +193,7 @@ static void prefs_plugin_sel (GtkTreeSelection *sel, GtkTreeModel *model)
 #ifndef _WIN32
 		   _("%s%s"
 		     "<span weight=\"bold\">Written by:</span>\t%s\n"
-		     "<span weight=\"bold\">Web site:</span>\t%s\n"
+		     "<span weight=\"bold\">Web site:</span>\t\t%s\n"
 		     "<span weight=\"bold\">File name:</span>\t%s"),
 #else
 		   _("<span size=\"larger\">%s %s</span>\n\n"
