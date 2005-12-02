99cd3c8c965bf84ce2f347004517d6e6bb7dd4df
Björn Voigt <bjoern@cs.tu-berlin.de>
2005-12-02 03:13:51 +0000

    [gaim-migrate @ 14594] Patch from Björn to translate the plugin name in plugin configuration windows
    committer: Mark Doliner <markdoliner@pidgin.im>
    
    --HG--
    extra : convert_revision : 465c193e5cec70fbd8299ad57f18b402fa0a9bb5

--- src/gtkplugin.c
+++ src/gtkplugin.c
@@ -329,7 +329,7 @@ static void plugin_dialog_response_cb(GtkWidget *d, int response, GtkTreeSelecti
 		g_signal_connect(G_OBJECT(dialog), "response", G_CALLBACK(pref_dialog_response_cb), plug);
 		gtk_container_add(GTK_CONTAINER(GTK_DIALOG(dialog)->vbox), box);
 		gtk_window_set_role(GTK_WINDOW(dialog), "plugin_config");
-		gtk_window_set_title(GTK_WINDOW(dialog), gaim_plugin_get_name(plug));
+		gtk_window_set_title(GTK_WINDOW(dialog), _(gaim_plugin_get_name(plug)));
 		gtk_widget_show_all(dialog);
 		g_value_unset(&val);
 		break;
