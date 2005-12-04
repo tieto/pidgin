a26f78374c437df13471903ae08c50b2826fdd50
Stu Tomlinson <nosnilmot@pidgin.im>
2005-12-04 18:12:19 +0000

    [gaim-migrate @ 14624] Fix the "Gtk: gtk_scrolled_window_add(): cannot add non scrollable widget use gtk_scrolled_window_add_with_viewport() instead" debug message. Amazingly, the fix is to do what the message suggested!
    --HG--
    extra : convert_revision : 838a0ed1b7edc240ebab78dabee1b28e25a93f6e

--- src/gtkstatusbox.c
+++ src/gtkstatusbox.c
@@ -615,7 +615,7 @@ gtk_gaim_status_box_init (GtkGaimStatusBox *status_box)
 	status_box->sw = gtk_scrolled_window_new(NULL, NULL);
 	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(status_box->sw), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
 	gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(status_box->sw), GTK_SHADOW_IN);
-	gtk_container_add(GTK_CONTAINER(status_box->sw), vbox);
+	gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW(status_box->sw), vbox);
 	gtk_box_pack_start(GTK_BOX(vbox), status_box->toolbar, FALSE, FALSE, 0);
 	gtk_box_pack_start(GTK_BOX(vbox), status_box->hsep, FALSE, FALSE, 0);
 	gtk_box_pack_start(GTK_BOX(vbox), status_box->imhtml, TRUE, TRUE, 0);
