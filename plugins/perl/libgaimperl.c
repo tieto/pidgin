001ea03a15eb430630f6452006bedcebb852e627
Richard Laager <rlaager@pidgin.im>
2005-12-08 21:07:24 +0000

    [gaim-migrate @ 14725] Prototype this ugly hack.
    --HG--
    extra : convert_revision : 215634d276c0bab330fb28731bf514e43931045a

--- plugins/perl/libgaimperl.c
+++ plugins/perl/libgaimperl.c
@@ -1,7 +1,9 @@
 #include <gmodule.h>
-void __attribute__ ((constructor)) my_init(void) {
-	/* Very evil hack...puts perl.so's symbols in the global table 		*/
-	/* but does not create a circular dependancy because g_module_open	*/
-	/* will only open the library once.					*/
+void __attribute__ ((constructor)) my_init(void);
+
+void __attribute__ ((constructor)) my_init() {
+	/* Very evil hack...puts perl.so's symbols in the global table
+	 * but does not create a circular dependancy because g_module_open
+	 * will only open the library once. */
 	g_module_open("perl.so", 0);
 }
