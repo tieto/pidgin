787785e12101ce29ca5be41b94c7ee3fdb94423f
Mark Doliner <markdoliner@pidgin.im>
2005-12-12 04:50:55 +0000

    [gaim-migrate @ 14762] Peter and Ethan: Pretty please with sugar on top test this as soon as possible.  I'm hoping it'll fix the ICQ auth request/SPIM problem. The problem is much more prominent with you two than with I.
    --HG--
    extra : convert_revision : 26b54e44b1d2b53ea8adfe793ee79d5a40da1177

--- src/protocols/oscar/service.c
+++ src/protocols/oscar/service.c
@@ -807,7 +807,11 @@ faim_export int aim_setextstatus(aim_session_t *sess, fu32_t status)
 	if (!sess || !(conn = aim_conn_findbygroup(sess, AIM_CB_FAM_MSG)))
 		return -EINVAL;
 
-	data = AIM_ICQ_STATE_DIRECTREQUIREAUTH | AIM_ICQ_STATE_HIDEIP | AIM_ICQ_STATE_WEBAWARE | status;
+	/*
+	 * 'AIM_ICQ_STATE_DIRECTREQUIREAUTH' was removed because setting it might
+	 * be the cause of people reciving many unsolicited authorization requests.
+	 */
+	data = AIM_ICQ_STATE_HIDEIP | AIM_ICQ_STATE_WEBAWARE | status;
 
 	if (!(fr = aim_tx_new(sess, conn, AIM_FRAMETYPE_FLAP, 0x02, 10 + 8)))
 		return -ENOMEM;
