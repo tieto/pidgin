b7592a474dae0e527eef8dbb29375d4bc6b59afb
Richard Laager <rlaager@pidgin.im>
2006-01-12 02:19:37 +0000

    [gaim-migrate @ 15190] Turns out g_unichar_to_utf8 doesn't do NUL-termination for us.
    --HG--
    extra : convert_revision : f96fec39a0268c4bb18e4598ecdd4cc930af0573

--- plugins/spellchk.c
+++ plugins/spellchk.c
@@ -125,9 +125,12 @@ make_word_proper(const gchar *word)
 {
 	char buf[7];
 	gchar *lower = g_utf8_strdown(word, -1);
+	gint bytes;
 	gchar *ret;
 
-	g_unichar_to_utf8(g_unichar_toupper(g_utf8_get_char(word)), buf);
+	bytes = g_unichar_to_utf8(g_unichar_toupper(g_utf8_get_char(word)), buf);
+	buf[MIN(bytes, sizeof(buf) - 1)] = '\0';
+
 	ret = g_strconcat(buf, g_utf8_offset_to_pointer(lower, 1), NULL);
 	g_free(lower);
 
