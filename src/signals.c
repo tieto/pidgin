15c4db01b19c3fcc1d5a075228204656b384e60e
Richard Laager <rlaager@pidgin.im>
2006-01-28 01:18:41 +0000

    [gaim-migrate @ 15417] Sadrul pointed this out. I can't test this either, but I think it works. :)
    --HG--
    extra : convert_revision : c3bfc836241d153421e27f5740555df320332abe

--- src/signals.c
+++ src/signals.c
@@ -540,8 +540,10 @@ gaim_signal_emit_vargs_return_1(void *instance, const char *signal,
 	}
 
 #ifdef HAVE_DBUS
+	G_VA_COPY(tmp, args)
 	gaim_dbus_signal_emit_gaim(signal, signal_data->num_values, 
-				   signal_data->values, args);
+				   signal_data->values, tmp);
+	va_end(tmp);
 #endif	/* HAVE_DBUS */
 
 	for (l = signal_data->handlers; l != NULL; l = l_next)
