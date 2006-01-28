b165f8b59ba14f0e7e6153315c5df5bd82982f99
Ethan Blanton <elb@pidgin.im>
2006-01-28 01:35:58 +0000

    [gaim-migrate @ 15419] Handle a bad nick change properly, as opposed to a bad nick at signon.
    --HG--
    extra : convert_revision : ea50d1681c7c5fcbdd16bcb21ad230ae5bfc49f9

--- src/protocols/irc/msgs.c
+++ src/protocols/irc/msgs.c
@@ -806,8 +806,16 @@ void irc_msg_nick(struct irc_conn *irc, const char *name, const char *from, char
 
 void irc_msg_badnick(struct irc_conn *irc, const char *name, const char *from, char **args)
 {
-	gaim_connection_error(gaim_account_get_connection(irc->account),
-			      _("Your selected account name was rejected by the server.  It probably contains invalid characters."));
+	GaimConnection *gc = gaim_account_get_connection(irc->account);
+	if (gaim_connection_get_state(gc) == GAIM_CONNECTED) {
+		gaim_notify_error(gc, _("Invalid nickname"),
+				  _("Invalid nickname"),
+				  _("Your selected nickname was rejected by the server.  It probably contains invalid characters."));
+
+	} else {
+		gaim_connection_error(gaim_account_get_connection(irc->account),
+				      _("Your selected account name was rejected by the server.  It probably contains invalid characters."));
+	}
 }
 
 void irc_msg_nickused(struct irc_conn *irc, const char *name, const char *from, char **args)
