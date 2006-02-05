c415ce8b01dac96a00002ee81adf360343486049
Mark Doliner <markdoliner@pidgin.im>
2006-02-05 22:23:45 +0000

    [gaim-migrate @ 15488] I believe this is equivalent.  And it should be more portable.
    --HG--
    extra : convert_revision : 62a6e39a9269856fa024c413a38f358ff2b75b1a

--- src/dnssrv.c
+++ src/dnssrv.c
@@ -95,7 +95,7 @@ static void resolve(int in, int out) {
 	guchar *end;
 	guchar *cp;
 	gchar name[256];
-	int type, dlen, pref, weight, port;
+	guint16 type, dlen, pref, weight, port;
 	gchar query[256];
 
 	if(read(in, query, 256) <= 0) {
@@ -124,17 +124,24 @@ static void resolve(int in, int out) {
 
 		cp += size;
 
-		NS_GET16(type,cp);
-		cp += 6; /* skip ttl and class since we already know it */
+		type = GUINT16_FROM_BE(*cp);
+		cp += 2;
 
-		NS_GET16(dlen,cp);
+		/* skip ttl and class since we already know it */
+		cp += 6;
+
+		dlen = GUINT16_FROM_BE(*cp);
+		cp += 2;
 
 		if (type == T_SRV) {
-			NS_GET16(pref,cp);
+			pref = GUINT16_FROM_BE(*cp);
+			cp += 2;
 
-			NS_GET16(weight, cp);
+			weight = GUINT16_FROM_BE(*cp);
+			cp += 2;
 
-			NS_GET16(port, cp);
+			port = GUINT16_FROM_BE(*cp);
+			cp += 2;
 
 			size = dn_expand( (unsigned char*)&answer, end, cp, name, 256);
 			if(size < 0 )
