1fb91973cae14872c4fdd1c1b561397552571289
Sadrul Habib Chowdhury <sadrul@pidgin.im>
2006-02-22 02:08:45 +0000

    [gaim-migrate @ 15679] SF Patch #1434650 from Sadrul
    "This is a minor documentation fix for
     gaim_gethostbyname_async"
    
    committer: Richard Laager <rlaager@pidgin.im>
    
    --HG--
    extra : convert_revision : 85604da226456c979d692c3feeafa781a4d57539

--- src/proxy.h
+++ src/proxy.h
@@ -237,7 +237,8 @@ typedef void (*dns_callback_t)(GSList *hosts, gpointer data,
  * @param port A portnumber which is stored in the struct sockaddr
  * @param callback Callback to call after resolving
  * @param data Extra data for the callback function
- * @return a GSList containing the size of followed by the struct sockaddr for any returned IP
+ *
+ * @return Zero indicates the connection is pending. Any other value indicates failure.
  */
 int gaim_gethostbyname_async(const char *hostname, int port, dns_callback_t callback, gpointer data);
 
