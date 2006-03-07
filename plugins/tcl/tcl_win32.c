8979368ba8122a31aaaefb4165cf8f80c3a0776b
Daniel Atallah <datallah@pidgin.im>
2006-03-07 05:09:51 +0000

    [gaim-migrate @ 15808] Whitespace and warning fixes
    --HG--
    extra : convert_revision : adcc63b051b4ff11bd9a6c1efa15c3a3777085fb

--- plugins/tcl/tcl_win32.c
+++ plugins/tcl/tcl_win32.c
@@ -4,7 +4,7 @@
  * gaim
  *
  * Copyright (C) 2003 Herman Bloggs <hermanator12002@yahoo.com>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -33,34 +33,29 @@ typedef void        (CALLBACK* LPFNTKINIT)(Tcl_Interp*);
 LPFNTCLCREATEINTERP wtcl_CreateInterp = NULL;
 LPFNTKINIT wtk_Init = NULL;
 
-BOOL tcl_win32_init() {
-        if(!(wtcl_CreateInterp=(LPFNTCLCREATEINTERP)wgaim_find_and_loadproc("tcl84.dll", "Tcl_CreateInterp"))) {
-                gaim_debug(GAIM_DEBUG_INFO, "tcl", "tcl_win32_init error loading Tcl_CreateInterp\n");
-                return FALSE;
-        }
+static BOOL tcl_win32_init() {
+	if(!(wtcl_CreateInterp = (LPFNTCLCREATEINTERP) wgaim_find_and_loadproc("tcl84.dll", "Tcl_CreateInterp"))) {
+		gaim_debug(GAIM_DEBUG_INFO, "tcl", "tcl_win32_init error loading Tcl_CreateInterp\n");
+		return FALSE;
+	}
 
-        if(!(wtk_Init=(LPFNTKINIT)wgaim_find_and_loadproc("tk84.dll", "Tk_Init"))) {
-                gaim_debug(GAIM_DEBUG_INFO, "tcl", "tcl_win32_init error loading Tk_Init\n");
-                return FALSE;
-        }
-        return TRUE;
+	if(!(wtk_Init = (LPFNTKINIT) wgaim_find_and_loadproc("tk84.dll", "Tk_Init"))) {
+		gaim_debug(GAIM_DEBUG_INFO, "tcl", "tcl_win32_init error loading Tk_Init\n");
+		return FALSE;
+	}
+	return TRUE;
 }
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
-        switch( fdwReason ) { 
-        case DLL_PROCESS_ATTACH:
-                return tcl_win32_init();
+	switch (fdwReason) {
+		case DLL_PROCESS_ATTACH:
+			return tcl_win32_init();
 
-        case DLL_THREAD_ATTACH:
-                break;
-
-        case DLL_THREAD_DETACH:
-                break;
-
-        case DLL_PROCESS_DETACH:
-                break;
-    }
-    return TRUE;
+		case DLL_THREAD_ATTACH:
+		case DLL_THREAD_DETACH:
+		case DLL_PROCESS_DETACH:
+			break;
+	}
+	return TRUE;
 }
-
 
