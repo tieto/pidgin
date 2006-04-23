9c3b742c6b25c7e3a56d3ca6a318ab560eecbd34
Mark Doliner <markdoliner@pidgin.im>
2006-04-23 03:44:58 +0000

    [gaim-migrate @ 16085] Add an "example: stunserver.org" to the Network preferences page.
    Align some gtkentries on the Network preferences page.
    
    Improve the vertical alignment of the spinners and their labels
    in the preferences window.
    
    --HG--
    extra : convert_revision : 81c9f0abb467f3bf2420fc2da83904dc8821867e

--- src/gtkprefs.c
+++ src/gtkprefs.c
@@ -123,7 +123,7 @@ gaim_gtk_prefs_labeled_spin_button(GtkWidget *box, const gchar *title,
 
 	if (sg) {
 		gtk_size_group_add_widget(sg, label);
-		gtk_misc_set_alignment(GTK_MISC(label), 0, 0);
+		gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
 	}
 
 	gaim_set_accessible_label (spin, label);
@@ -148,7 +148,7 @@ gaim_gtk_prefs_labeled_entry(GtkWidget *page, const gchar *title,
 	value = gaim_prefs_get_string(key);
 
 	hbox = gtk_hbox_new(FALSE, 5);
-	gtk_box_pack_start(GTK_BOX(page), hbox, FALSE, FALSE, 5);
+	gtk_box_pack_start(GTK_BOX(page), hbox, FALSE, FALSE, 0);
 	gtk_widget_show(hbox);
 
 	label = gtk_label_new_with_mnemonic(title);
@@ -167,7 +167,7 @@ gaim_gtk_prefs_labeled_entry(GtkWidget *page, const gchar *title,
 
 	if(sg) {
 		gtk_size_group_add_widget(sg, label);
-		gtk_misc_set_alignment(GTK_MISC(label), 0, 0);
+		gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
 	}
 
 	gaim_set_accessible_label(entry, label);
@@ -971,21 +971,36 @@ network_page()
 	gtk_container_set_border_width (GTK_CONTAINER (ret), GAIM_HIG_BORDER);
 
 	vbox = gaim_gtk_make_frame (ret, _("IP Address"));
+	sg = gtk_size_group_new(GTK_SIZE_GROUP_HORIZONTAL);
+	gaim_gtk_prefs_labeled_entry(vbox,_("ST_UN server:"),
+			"/core/network/stun_server", sg);
 
-	gaim_gtk_prefs_labeled_entry(vbox,_("ST_UN server:"), "/core/network/stun_server", NULL);
+	hbox = gtk_hbox_new(FALSE, GAIM_HIG_BOX_SPACE);
+	gtk_container_add(GTK_CONTAINER(vbox), hbox);
+
+	label = gtk_label_new(NULL);
+	gtk_container_add(GTK_CONTAINER(hbox), label);
+	gtk_size_group_add_widget(sg, label);
+
+	label = gtk_label_new(NULL);
+	gtk_label_set_markup(GTK_LABEL(label),
+			_("<span style=\"italic\">Example: stunserver.org</span>"));
+	gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
+	gtk_container_add(GTK_CONTAINER(hbox), label);
 
 	auto_ip_checkbox = gaim_gtk_prefs_checkbox(_("_Autodetect IP address"),
 			"/core/network/auto_ip", vbox);
 
-	table = gtk_table_new(2, 1, FALSE);
+	table = gtk_table_new(2, 2, FALSE);
 	gtk_container_set_border_width(GTK_CONTAINER(table), 0);
 	gtk_table_set_col_spacings(GTK_TABLE(table), 5);
 	gtk_table_set_row_spacings(GTK_TABLE(table), 10);
 	gtk_container_add(GTK_CONTAINER(vbox), table);
 
 	label = gtk_label_new_with_mnemonic(_("Public _IP:"));
-	gtk_misc_set_alignment(GTK_MISC(label), 1.0, 0.5);
+	gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
 	gtk_table_attach(GTK_TABLE(table), label, 0, 1, 0, 1, GTK_FILL, 0, 0, 0);
+	gtk_size_group_add_widget(sg, label);
 
 	entry = gtk_entry_new();
 	gtk_label_set_mnemonic_widget(GTK_LABEL(label), entry);
@@ -1047,7 +1062,7 @@ network_page()
 
 		gaim_prefs_connect_callback(prefs, "/core/proxy/type",
 					    proxy_changed_cb, prefs_proxy_frame);
-	
+
 		table = gtk_table_new(4, 2, FALSE);
 		gtk_container_set_border_width(GTK_CONTAINER(table), 0);
 		gtk_table_set_col_spacings(GTK_TABLE(table), 5);
