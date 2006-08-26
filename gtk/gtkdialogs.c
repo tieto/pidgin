6ea2d42d120c455287c5d26db45de2e4292bed22
Richard Laager <rlaager@pidgin.im>
2006-08-26 06:26:25 +0000

    [gaim-migrate @ 17037] Add QQ to the list of protocols in the About box.
    --HG--
    extra : convert_revision : 5146e192cc70066594c2e115a5d958a1af010c5e

--- gtk/gtkdialogs.c
+++ gtk/gtkdialogs.c
@@ -320,8 +320,8 @@ void gaim_gtkdialogs_about()
 	g_string_append(str,
 		_("Gaim is a modular messaging client capable of using "
 		  "AIM, MSN, Yahoo!, Jabber, ICQ, IRC, SILC, "
-		  "Novell GroupWise, Lotus Sametime, Zephyr, and Gadu-Gadu "
-		  "all at once.  It is written using GTK+.<BR><BR>"
+		  "Novell GroupWise, Lotus Sametime, Zephyr, Gadu-Gadu, "
+		  "and QQ all at once.  It is written using GTK+.<BR><BR>"
 		  "You may modify and redistribute the program under "
 		  "the terms of the GPL (version 2 or later).  A copy of the GPL is "
 		  "contained in the 'COPYING' file distributed with Gaim.  "
