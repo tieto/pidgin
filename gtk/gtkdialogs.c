d0a0617a3e21cce0c25136f97ff3fb8ecf153cbe
Luke Schierer <lschiere@pidgin.im>
2006-09-11 16:51:42 +0000

    [gaim-migrate @ 17250] a few translation updates
    --HG--
    extra : convert_revision : 8a07b6218eb3c889472fcc28e5c0cd9f90de1eda

--- gtk/gtkdialogs.c
+++ gtk/gtkdialogs.c
@@ -138,6 +138,8 @@ static struct translator current_translators[] = {
 	{N_("French"),              "fr", "Éric Boumaour", "zongo_fr@users.sourceforge.net"},
 	{N_("Galician"),            "gl", "Ignacio Casal Quinteiro", "nacho.resa@gmail.com"},
 	{N_("Gujarati"),            "gu", "Ankit Patel", "ankit_patel@users.sf.net"},
+	{N_("Gujarati"),            "gu", "Kartik Mistry", "kartik.mistry@gmail.com"},
+	{N_("Gujarati"),            "gu", "Gujarati Language Team", "indianoss-gujarati@lists.sourceforge.net"
 	{N_("Hebrew"),              "he", "Shalom Craimer", "scraimer@gmail.com"},
 	{N_("Hindi"),               "hi", "Ravishankar Shrivastava", "raviratlami@yahoo.com"},
 	{N_("Hungarian"),           "hu", "Zoltan Sutto", "sutto.zoltan@rutinsoft.hu"},
