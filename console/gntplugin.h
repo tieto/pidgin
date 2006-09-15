e25a511957d1b8605d0cd82b7a7f68ce007851bc
Sadrul Habib Chowdhury <sadrul@pidgin.im>
2006-09-15 01:21:53 +0000

    [gaim-migrate @ 17281] Doxygen for console/
    --HG--
    extra : convert_revision : 2abed821cdf78d79dd1bfd0e51acb913a713c867

--- console/gntplugin.h
+++ console/gntplugin.h
@@ -1,3 +1,27 @@
+/**
+ * @file gntplugin.h GNT Plugins API
+ * @ingroup gntui
+ *
+ * gaim
+ *
+ * Gaim is the legal property of its developers, whose names are too numerous
+ * to list here.  Please refer to the COPYRIGHT file distributed with this
+ * source distribution.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 #ifndef _GNT_PLUGIN_H
 #define _GNT_PLUGIN_H
 
@@ -7,20 +31,39 @@
 
 #include <string.h>
 
+/**********************************************************************
+ * @name GNT Plugins API
+ **********************************************************************/
+/*@{*/
+
 typedef GntWidget* (*GGPluginFrame) ();
 
 /* Guess where these came from */
 #define GAIM_GNT_PLUGIN_TYPE "gnt"
 
+/**
+ * Decide whether a plugin is a GNT-plugin.
+ */
 #define GAIM_IS_GNT_PLUGIN(plugin) \
 	((plugin)->info != NULL && (plugin)->info->ui_info != NULL && \
 	 !strcmp((plugin)->info->ui_requirement, GAIM_GNT_PLUGIN_TYPE))
 
+/**
+ * Get the ui-info from GNT-plugins.
+ */
 #define GAIM_GNT_PLUGIN_UI_INFO(plugin) \
 	(GGPluginFrame)((plugin)->info->ui_info)
 
+/**
+ * Show a list of plugins.
+ */
 void gg_plugins_show_all(void);
 
+/**
+ * Save the list of loaded plugins.
+ */
 void gg_plugins_save_loaded(void);
+
+/*@}*/
 
 #endif
--- console/gntrequest.h
+++ console/gntrequest.h
@@ -1,12 +1,54 @@
+/**
+ * @file gntrequest.h GNT Request API
+ * @ingroup gntui
+ *
+ * gaim
+ *
+ * Gaim is the legal property of its developers, whose names are too numerous
+ * to list here.  Please refer to the COPYRIGHT file distributed with this
+ * source distribution.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 #ifndef _GNT_REQUEST_H
 #define _GNT_REQUEST_H
 
 #include "request.h"
 
+/**********************************************************************
+ * @name GNT Request API
+ **********************************************************************/
+/*@{*/
+
+/**
+ * Get the ui-functions.
+ *
+ * @return The GaimRequestUiOps structure populated with the appropriate functions.
+ */
 GaimRequestUiOps *gg_request_get_ui_ops(void);
 
+/**
+ * Perform necessary initializations.
+ */
 void gg_request_init(void);
 
+/**
+ * Perform necessary uninitializations.
+ */
 void gg_request_uninit(void);
+
+/*@}*/
 
 #endif
