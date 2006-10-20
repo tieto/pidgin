840c9df515c4f04527248edf36afd521abfe902a
Luke Schierer <lschiere@pidgin.im>
2006-10-20 15:04:26 +0000

    [gaim-migrate @ 17553] Noticed by Eric Covener, someone messed up :-P
    --HG--
    extra : convert_revision : ba5939df0f175a4f94169932c662621c4ca48707

--- gtk/gtkgaim.h
+++ gtk/gtkgaim.h
@@ -22,7 +22,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-// #warning ***gtkgaim***
+/* #warning ***gtkgaim*** */
 #ifndef _GAIM_GTKGAIM_H_
 #define _GAIM_GTKGAIM_H_
 
