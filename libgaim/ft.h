d2b84c50518020301627f7c3d8a4b793f6301658
Daniel Atallah <datallah@pidgin.im>
2006-11-09 05:45:55 +0000

    [gaim-migrate @ 17710] Add reference to file transfer signals documentation.
    --HG--
    extra : convert_revision : 023a33faca85a50ac7f731ebe7152a69c0c294fc

--- libgaim/ft.h
+++ libgaim/ft.h
@@ -21,6 +21,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @see @ref xfer-signals
  */
 #ifndef _GAIM_FT_H_
 #define _GAIM_FT_H_
