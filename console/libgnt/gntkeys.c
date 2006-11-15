19b6fea9524b555439398ebfdb5fd780696c82e4
Sadrul Habib Chowdhury <sadrul@pidgin.im>
2006-11-15 20:11:01 +0000

    [gaim-migrate @ 17754] *cough*
    --HG--
    extra : convert_revision : 376002bf730fdee2cbffb36faf2bddd957b18ef5

--- console/libgnt/gntkeys.c
+++ console/libgnt/gntkeys.c
@@ -14,7 +14,7 @@ void gnt_keys_refine(char *text)
 	}
 
 	if (*text == 27 && *(text + 1) == '[' && *(text + 3) == '\0' &&
-			(*(text + 2) >= 'A' || *(text + 2) <= 'D')) {
+			(*(text + 2) >= 'A' && *(text + 2) <= 'D')) {
 		/* Apparently this is necessary for urxvt and screen and xterm */
 		if (strcmp(term, "screen") == 0 || strcmp(term, "rxvt-unicode") == 0 ||
 				strcmp(term, "xterm") == 0)
