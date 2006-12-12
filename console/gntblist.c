b622a62a15920519971fe41d86ddc1bb86ea2a77
Sadrul Habib Chowdhury <sadrul@pidgin.im>
2006-12-12 01:06:42 +0000

    [gaim-migrate @ 17966] Fix the tooltip.
    --HG--
    extra : convert_revision : 5080992a724f3ae911840376fbb91519ee58efd0

--- console/gntblist.c
+++ console/gntblist.c
@@ -1183,7 +1183,7 @@ tooltip_for_buddy(GaimBuddy *buddy, GString *str)
 		}
 	}
 	
-	tmp = gaim_notify_user_info_get_text_with_newline(user_info, "\n");
+	tmp = gaim_notify_user_info_get_text_with_newline(user_info, "<BR>");
 	gaim_notify_user_info_destroy(user_info);
 
 	strip = gaim_markup_strip_html(tmp);
