e36b0d1b788a37a00bbb28696994f373d4150bd7
Evan Schoenberg <evands@pidgin.im>
2006-12-26 02:01:15 +0000

    [gaim-migrate @ 18052] Things are wrong if we get presence for a Jabber buddy not on our list... I've added a debug warning to point out when that happens.
    --HG--
    extra : convert_revision : 92f9730f8b9ebcb8af9184bbab865e3bd068e1ed

--- libgaim/protocols/jabber/presence.c
+++ libgaim/protocols/jabber/presence.c
@@ -507,6 +507,8 @@ void jabber_presence_parse(JabberStream *js, xmlnode *packet)
 		buddy_name = g_strdup_printf("%s%s%s", jid->node ? jid->node : "",
 				jid->node ? "@" : "", jid->domain);
 		if((b = gaim_find_buddy(js->gc->account, buddy_name)) == NULL) {
+			gaim_debug_warning("jabber", "Got presence for unknown buddy %s on account %s (%x)",
+				buddy_name, gaim_account_get_username(js->gc->account), js->gc->account);
 			jabber_id_free(jid);
 			if(avatar_hash)
 				g_free(avatar_hash);
