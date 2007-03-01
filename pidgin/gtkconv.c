ed4220bcfde9058f9ffd1bbd14a3fea0dec917bc
Sean Egan <seanegan@pidgin.im>
2007-03-01 21:48:46 +0000

    Fix a small leak
    --HG--
    extra : convert_revision : 54e688cbafab9d76c49db6eb137bcbde87a32b74

--- pidgin/gtkconv.c
+++ pidgin/gtkconv.c
@@ -5879,8 +5879,10 @@ gray_stuff_out(PidginConversation *gtkconv)
 			l = pidgin_conv_get_tab_icons(conv);
 		}
 		gtk_window_set_icon_list(GTK_WINDOW(win->window), l);
-		if (window_icon != NULL)
+		if (window_icon != NULL) {
 			g_object_unref(G_OBJECT(window_icon));
+			g_list_free(l);
+		}
 	}
 }
 
