9f019b97ff998eac60b60c66ef01aba5108fd369
Sean Egan <seanegan@pidgin.im>
2007-04-25 00:45:17 +0000

    New logo.png
    --HG--
    extra : convert_revision : 88a78587504cdd42570461a2b391b54b5c3d4aab

--- pidgin/gtkdialogs.c
+++ pidgin/gtkdialogs.c
@@ -285,7 +285,7 @@ pidgin_logo_versionize(GdkPixbuf **original, GtkWidget *widget) {
 	context = gtk_widget_get_pango_context(widget);
 	layout = pango_layout_new(context);
 
-	markup = g_strdup_printf("<span foreground=\"#FFFFFF\" size=\"larger\">%s</span>", VERSION);
+	markup = g_strdup_printf("<span foreground=\"#5c3566\">%s</span>", VERSION);
 	pango_layout_set_font_description(layout, style->font_desc);
 	pango_layout_set_markup(layout, markup, strlen(markup));
 	g_free(markup);
