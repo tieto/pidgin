a3bc3269534535cbd12e70be07a820b86b84cd34
Sean Egan <seanegan@pidgin.im>
2007-05-03 18:29:14 +0000

    kill the Help > About star icon
    --HG--
    extra : convert_revision : fdfd91d14e23232a6658a7b0f2926923cb70c8c4

--- pidgin/gtkblist.c
+++ pidgin/gtkblist.c
@@ -2850,7 +2850,7 @@ static GtkItemFactoryEntry blist_menu[] =
 	{ N_("/_Help"), NULL, NULL, 0, "<Branch>", NULL },
 	{ N_("/Help/Online _Help"), "F1", gtk_blist_show_onlinehelp_cb, 0, "<StockItem>", GTK_STOCK_HELP },
 	{ N_("/Help/_Debug Window"), NULL, toggle_debug, 0, "<Item>", NULL },
-	{ N_("/Help/_About"), NULL, pidgin_dialogs_about, 0,  "<StockItem>", PIDGIN_STOCK_ABOUT },
+	{ N_("/Help/_About"), NULL, pidgin_dialogs_about, 0,  "<Item>", NULL },
 };
 
 /*********************************************************
