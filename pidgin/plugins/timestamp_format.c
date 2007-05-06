e10cb2229db50ca56830750b584795a6e46da4ee
Richard Laager <rlaager@pidgin.im>
2007-05-06 00:43:29 +0000

    Traditional Pidgin doesn't make any sense.  It was only ever the case for Gaim.  Since we're not speaking that name any more, I just removed it.
    --HG--
    extra : convert_revision : 76c65bb2616c97163a344c65154af0c150198189

--- pidgin/plugins/timestamp_format.c
+++ pidgin/plugins/timestamp_format.c
@@ -23,7 +23,7 @@ get_plugin_pref_frame(PurplePlugin *plugin)
 	ppref = purple_plugin_pref_new_with_label(_("Timestamp Format Options"));
 	purple_plugin_pref_frame_add(frame, ppref);
 
-	tmp = g_strdup_printf(_("_Force (traditional %s) 24-hour time format"), PIDGIN_NAME);
+	tmp = g_strdup_printf(_("_Force 24-hour time format"));
 	ppref = purple_plugin_pref_new_with_name_and_label(
 			"/plugins/gtk/timestamp_format/force_24hr",
 			tmp);
