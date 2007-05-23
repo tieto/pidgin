2f82b143f071e0672d78ece3322ed5d733a34f4e
Daniel Atallah <datallah@pidgin.im>
2007-05-23 03:59:19 +0000

    Fix file transfers aborting and mistakenly being marked as cancelled when they are actually complete. Fixes #814
    --HG--
    extra : convert_revision : 4e303e0d82031bfb84148de1b35be7efabbb36f7

--- libpurple/ft.c
+++ libpurple/ft.c
@@ -891,7 +891,7 @@ transfer_cb(gpointer data, gint source, PurpleInputCondition condition)
 		r = purple_xfer_read(xfer, &buffer);
 		if (r > 0) {
 			fwrite(buffer, 1, r, xfer->dest_fp);
-		} else if(r <= 0) {
+		} else if(r < 0) {
 			purple_xfer_cancel_remote(xfer);
 			return;
 		}
