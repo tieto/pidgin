022fd9cae21ae0b533fd3ed6173e7cd9cd218bca
Sean Egan <seanegan@pidgin.im>
2007-05-28 18:25:43 +0000

    fixes #1316
    --HG--
    extra : convert_revision : 22370540554736ee87a874712ddef1047bcb2998

--- pidgin/gtkprefs.c
+++ pidgin/gtkprefs.c
@@ -1693,6 +1693,7 @@ sound_page()
 			vbox->parent->parent, TRUE, TRUE, 0, GTK_PACK_START);
 
 	sw = gtk_scrolled_window_new(NULL,NULL);
+	gtk_widget_set_size_request(sw, -1, 100);
 	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sw), GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
 	gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW(sw), GTK_SHADOW_IN);
 
