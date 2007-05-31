ae487d7be7621ae45eab094c7c5d69c0ea8d4f7c
William Ehlhardt <williamehlhardt@gmail.com>
2007-05-31 00:37:50 +0000

    disapproval of revision 'b81591d629b9074fcdb05a9257aed5b13c1cfbed'
    --HG--
    branch : soc.2007.certmgr
    extra : convert_revision : 4538f6c33c0f8ed2d8e1f9124fb52bce0d676e63

--- libpurple/plugins/ssl/ssl-gnutls.c
+++ libpurple/plugins/ssl/ssl-gnutls.c
@@ -224,8 +224,6 @@ static void ssl_gnutls_authcheck_ask(PurpleSslConnection * gsc)
 
       gnutls_x509_crt_deinit(cert);
     } /* if (0 == ... */
-
-  purple_debug_info("gnutls","Requested user verification for certificate from %s\n", gsc->host);
 }
 
 static void ssl_gnutls_handshake_cb(gpointer data, gint source,
@@ -330,7 +328,6 @@ static void ssl_gnutls_handshake_cb(gpointer data, gint source,
 		} /* End keydata spitting */
 
 		/* Ask for cert verification */
-		ssl_gnutls_authcheck_ask(gsc);
 	}
 
 }
