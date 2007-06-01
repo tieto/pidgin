155355e10dd40e82f44add53d1461dcc12df3207
Sean Egan <seanegan@pidgin.im>
2007-06-01 04:21:55 +0000

    Fix default custom font
    --HG--
    extra : convert_revision : 4d5db7915aefec35d5303682ec33a210c4a5035e

--- pidgin/gtkconv.c
+++ pidgin/gtkconv.c
@@ -6952,7 +6952,7 @@ pidgin_conversations_init(void)
 	purple_prefs_add_int(PIDGIN_PREFS_ROOT "/conversations/scrollback_lines", 4000);
 
 	purple_prefs_add_bool(PIDGIN_PREFS_ROOT "/conversations/use_theme_font", TRUE);
-	purple_prefs_add_string(PIDGIN_PREFS_ROOT "/conversations/custom_font", "Sans 28");
+	purple_prefs_add_string(PIDGIN_PREFS_ROOT "/conversations/custom_font", "");
 
 	/* Conversations -> Chat */
 	purple_prefs_add_none(PIDGIN_PREFS_ROOT "/conversations/chat");
