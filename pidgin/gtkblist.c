57f70e4222fe1623ca7225ae9935788384baadcf
Gabriel Schulhof <nix@go-nix.ca>
2007-06-04 21:10:44 +0000

    About's item type has to be "<StockItem>"
    --HG--
    extra : convert_revision : d8a42ed758757ffa01f3dbf8dd6ec2a33d4b3891

--- pidgin/gtkblist.c
+++ pidgin/gtkblist.c
@@ -2852,7 +2852,7 @@ static GtkItemFactoryEntry blist_menu[] =
 	{ N_("/Buddies/Add _Group..."), NULL, purple_blist_request_add_group, 0, "<StockItem>", GTK_STOCK_ADD },
 	{ "/Buddies/sep3", NULL, NULL, 0, "<Separator>", NULL },
 #if GTK_CHECK_VERSION(2,6,0)
-	{ N_("/Buddies/_About Pidgin"), NULL, pidgin_dialogs_about, 0,  "<Item>", GTK_STOCK_ABOUT },
+	{ N_("/Buddies/_About Pidgin"), NULL, pidgin_dialogs_about, 0,  "<StockItem>", GTK_STOCK_ABOUT },
 #else /* GTK < 2.6.0 */
 	{ N_("/Buddies/_About Pidgin"), NULL, pidgin_dialogs_about, 0,  "<Item>", NULL },
 #endif /* GTK >= 2.6.0 */
