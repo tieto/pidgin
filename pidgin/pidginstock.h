772e9fa441886edb58851493c386da941ecd5c96
Sean Egan <seanegan@pidgin.im>
2007-06-13 00:44:27 +0000

    Use the correct icon for new message
    --HG--
    extra : convert_revision : ebff6c3b8f2304c88a5769e8c4c25a763f3a5c1f

--- pidgin/pidginstock.h
+++ pidgin/pidginstock.h
@@ -136,6 +136,9 @@
 #define PIDGIN_STOCK_TRAY_OFFLINE         "pidgin-tray-offline"
 #define PIDGIN_STOCK_TRAY_CONNECT         "pidgin-tray-connect"
 #define PIDGIN_STOCK_TRAY_PENDING         "pidgin-tray-pending"
+/* Uncomment me after 2.0.2! 
+ * #define PIDGIN_STOCK_TRAY_EMAIL		  "pidgin-tray-email"
+ */
 
 
 /*@}*/
