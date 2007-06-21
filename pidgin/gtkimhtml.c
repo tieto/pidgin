37f0351c2bb52256c8a9c44cd96db3db0640e1bf
Stu Tomlinson <nosnilmot@pidgin.im>
2007-06-21 11:37:19 +0000

    Kill a compile warning, as we now include internal.h in gtkimhtml.c, we don't need to define _ here too.
    --HG--
    extra : convert_revision : d0dba9615657d0f81a8f82c3eaf6f66862eb7607

--- pidgin/gtkimhtml.c
+++ pidgin/gtkimhtml.c
@@ -54,19 +54,6 @@
 #include <windows.h>
 #endif
 
-#ifdef ENABLE_NLS
-#  include <libintl.h>
-#  define _(x) gettext(x)
-#  ifdef gettext_noop
-#    define N_(String) gettext_noop (String)
-#  else
-#    define N_(String) (String)
-#  endif
-#else
-#  define N_(String) (String)
-#  define _(x) (x)
-#endif
-
 #include <pango/pango-font.h>
 
 /* GTK+ < 2.4.x hack, see pidgin.h for details. */
