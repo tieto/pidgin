54f36a8b4a6a34eb7267aaf12597287ffdc6653a
Sean Egan <seanegan@pidgin.im>
2007-06-27 18:11:30 +0000

    When an XMPP buddy signs off, show the status of the currently highest priority buddy, rather than the status of the signed off buddy. Fixes #1126
    --HG--
    extra : convert_revision : 475a8ca28dabbffcdd32005dfbc33c853b1eb7de

--- libpurple/protocols/jabber/presence.c
+++ libpurple/protocols/jabber/presence.c
@@ -558,6 +558,7 @@ void jabber_presence_parse(JabberStream *js, xmlnode *packet)
 			jabber_buddy_remove_resource(jb, jid->resource);
 			if((conv = jabber_find_unnormalized_conv(from, js->gc->account)))
 				purple_conversation_set_name(conv, buddy_name);
+	printf("Removed resource\n");
 
 		} else {
 			jbr = jabber_buddy_track_resource(jb, jid->resource, priority,
@@ -565,7 +566,7 @@ void jabber_presence_parse(JabberStream *js, xmlnode *packet)
 		}
 
 		if((found_jbr = jabber_buddy_find_resource(jb, NULL))) {
-			purple_prpl_got_user_status(js->gc->account, buddy_name, jabber_buddy_state_get_status_id(state), "priority", found_jbr->priority, found_jbr->status ? "message" : NULL, found_jbr->status, NULL);
+			purple_prpl_got_user_status(js->gc->account, buddy_name, jabber_buddy_state_get_status_id(found_jbr->state), "priority", found_jbr->priority, found_jbr->status ? "message" : NULL, found_jbr->status, NULL);
 		} else {
 			purple_prpl_got_user_status(js->gc->account, buddy_name, "offline", status ? "message" : NULL, status, NULL);
 		}
