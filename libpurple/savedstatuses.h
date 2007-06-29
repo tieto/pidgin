a3f0f96a387d16ed92dcb7dfd04ac1e1c4a6e020
Sadrul Habib Chowdhury <sadrul@pidgin.im>
2007-06-29 15:30:22 +0000

    Add links to the signal documents in the API documents.
    --HG--
    extra : convert_revision : 85bfdd599b8244d85271918b48154ff6285139c4

--- libpurple/savedstatuses.h
+++ libpurple/savedstatuses.h
@@ -21,6 +21,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @see @ref savedstatus-signals
  */
 #ifndef _PURPLE_SAVEDSTATUSES_H_
 #define _PURPLE_SAVEDSTATUSES_H_
