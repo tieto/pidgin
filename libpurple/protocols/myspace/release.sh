067e37cab93a4129980c37b17ad2d609209472d7
Jeff Connelly <jeff2@soc.pidgin.im>
2007-07-02 04:22:42 +0000

    Update for 0.9.
    --HG--
    branch : soc.2007.msimprpl
    extra : convert_revision : d1e5d5258c5e0fb595e0f497e2e023805035dd61

--- libpurple/protocols/myspace/release.sh
+++ libpurple/protocols/myspace/release.sh
@@ -4,7 +4,7 @@
 
 # Package a new msimprpl for release. Must be run with bash.
 
-VERSION=0.8
+VERSION=0.9
 make
 cd ..
 # Include 'myspace' directory in archive, so it can easily be unextracted
@@ -20,5 +20,5 @@ make -f Makefile.mingw
 mv ~/pidgin/config.h- ~/pidgin/config.h
 cp ~/pidgin/win32-install-dir/plugins/libmyspace.dll .
 # Zip is more common with Win32 users. Just include a few files in this archive.
-zip ../msimprpl-$VERSION-win32.zip libmyspace.dll README LICENSE
+zip ../msimprpl-$VERSION-win32.zip libmyspace.dll README LICENSE CHANGES
 ls -l ../msimprpl-$VERSION*
