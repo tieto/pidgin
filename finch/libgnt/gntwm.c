5f3cc55284c1327c3c480321cc7a7c18fce9d11a
Eric Polino <aluink@pidgin.im>
2007-07-11 20:03:11 +0000

    Cleaned up a few missing NULL checks in GntWM Put a few comments about a warning and other potential NULL checks missing.
    --HG--
    branch : soc.2007.finchfeat
    extra : convert_revision : db793e506dda38bd8d837986f60cfcbe027860e4

--- finch/libgnt/gntwm.c
+++ finch/libgnt/gntwm.c
@@ -735,6 +735,7 @@ dump_screen(GntBindable *bindable, GList *null)
 			print = ch;
 #ifndef NO_WIDECHAR
 			if (wch.chars[0] > 255) {
+				/* XXX This lines throws a warning, can we quiet it? */
 				snprintf(unicode, sizeof(unicode), "&#x%x;", wch.chars[0]);
 				print = unicode;
 			}
@@ -803,6 +804,9 @@ shift_left(GntBindable *bindable, GList *null)
 	if (wm->_list.window)
 		return TRUE;
 
+	if(!wm->cws->ordered)
+		return FALSE;
+
 	shift_window(wm, wm->cws->ordered->data, -1);
 	return TRUE;
 }
@@ -811,8 +815,12 @@ static gboolean
 shift_right(GntBindable *bindable, GList *null)
 {
 	GntWM *wm = GNT_WM(bindable);
+	
 	if (wm->_list.window)
 		return TRUE;
+
+	if(!wm->cws->ordered)
+		return FALSE;
 
 	shift_window(wm, wm->cws->ordered->data, 1);
 	return TRUE;
@@ -1146,7 +1154,13 @@ static gboolean
 help_for_window(GntBindable *bindable, GList *null)
 {
 	GntWM *wm = GNT_WM(bindable);
-	GntWidget *widget = wm->cws->ordered->data;
+	GntWidget *widget;
+	
+	
+	if(!wm->cws->ordered)
+		return FALSE;
+	
+	widget = wm->cws->ordered->data;
 
 	return help_for_bindable(wm,GNT_BINDABLE(widget));
 }
@@ -1947,7 +1961,8 @@ gnt_wm_give_focus(GntWM *wm, GntWidget *widget)
 
 	if (!node)
 		return;
-	
+
+	/* XXX Should there be a check before access to 'data' to make sure 'ordered' isn't NULL? */
 	if (widget != wm->_list.window && !GNT_IS_MENU(widget) &&
 				wm->cws->ordered->data != widget) {
 		GntWidget *w = wm->cws->ordered->data;
@@ -2009,6 +2024,7 @@ void gnt_wm_raise_window(GntWM *wm, GntWidget *widget)
 	GntWS *ws = gnt_wm_widget_find_workspace(wm, widget);
 	if (wm->cws != ws)
 		gnt_wm_switch_workspace(wm, g_list_index(wm->workspaces, ws));
+	/* XXX Should there be a check before access to 'data' to make sure 'ordered' isn't NULL? */
 	if (widget != wm->cws->ordered->data) {
 		GntWidget *wid = wm->cws->ordered->data;
 		wm->cws->ordered = g_list_bring_to_front(wm->cws->ordered, widget);
