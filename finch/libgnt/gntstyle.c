de64f878cb1e42f0abc5bfb89f7e56f9ab8261d2
Sadrul Habib Chowdhury <sadrul@pidgin.im>
2007-07-12 23:57:56 +0000

    Allow one-line high buttons. Specify 'small-button = true' under 'general', or 'finch'. This gets rid of the border from the buttons, and does the highlighting a little differently.
    --HG--
    extra : convert_revision : 70232e190a2c640678a9ec7142bf9b1a9a2876aa

--- finch/libgnt/gntstyle.c
+++ finch/libgnt/gntstyle.c
@@ -48,13 +48,14 @@ const char *gnt_style_get(GntStyle style)
 char *gnt_style_get_from_name(const char *group, const char *key)
 {
 #if GLIB_CHECK_VERSION(2,6,0)
+	if ((group == NULL && (group = g_get_prgname()) == NULL) || *group == '\0')
+		group = "general";
 	return g_key_file_get_value(gkfile, group, key, NULL);
 #endif
 }
 
 gboolean gnt_style_get_bool(GntStyle style, gboolean def)
 {
-	int i;
 	const char * str;
 
 	if (bool_styles[style] != -1)
@@ -62,11 +63,20 @@ gboolean gnt_style_get_bool(GntStyle style, gboolean def)
 	
 	str = gnt_style_get(style);
 
+	bool_styles[style] = str ? gnt_style_parse_bool(str) : def;
+	return bool_styles[style];
+}
+
+gboolean gnt_style_parse_bool(const char *str)
+{
+	gboolean def = FALSE;
+	int i;
+
 	if (str)
 	{
-		if (strcmp(str, "false") == 0)
+		if (g_ascii_strcasecmp(str, "false") == 0)
 			def = FALSE;
-		else if (strcmp(str, "true") == 0)
+		else if (g_ascii_strcasecmp(str, "true") == 0)
 			def = TRUE;
 		else if (sscanf(str, "%d", &i) == 1)
 		{
@@ -76,9 +86,7 @@ gboolean gnt_style_get_bool(GntStyle style, gboolean def)
 				def = FALSE;
 		}
 	}
-
-	bool_styles[style] = def;
-	return bool_styles[style];
+	return def;
 }
 
 static void
