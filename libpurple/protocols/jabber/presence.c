7d603906743c5a5561ec33ebbaad276eff485cb2
Etan Reisner <deryni@pidgin.im>
2007-07-28 04:35:41 +0000

    Commit a patch by bibliochat to handle an XMPP MUC status code that I pushed for adding to the spec and then never updated pidgin to handle. This should let us correctly handle the case where a MUC room assigns us a nickname other than what we requested originally.
    --HG--
    extra : convert_revision : e59325356500c39d0e99251d92399d3c372e694a

--- libpurple/protocols/jabber/presence.c
+++ libpurple/protocols/jabber/presence.c
@@ -372,6 +372,12 @@ void jabber_presence_parse(JabberStream *js, xmlnode *packet)
 										_("_Configure Room"), G_CALLBACK(jabber_chat_request_room_configure),
 										_("_Accept Defaults"), G_CALLBACK(jabber_chat_create_instant_room));
 						}
+					} else if(code && !strcmp(code, "210")) {
+						/*  server rewrote room-nick */
+						if((chat = jabber_chat_find(js, jid->node, jid->domain))) {
+							g_free(chat->handle);
+							chat->handle = g_strdup(jid->resource);
+						}
 					}
 				}
 				if((z = xmlnode_get_child(y, "item"))) {
