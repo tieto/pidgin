b3e179022e5dc91bcd7af40f384b3bd23e2d679b
William Ehlhardt <williamehlhardt@gmail.com>
2007-08-03 07:37:57 +0000

    - ssl-nss x509_nss check_name
    --HG--
    branch : soc.2007.certmgr
    extra : convert_revision : c9c2f5e6abc6444db9e447cd1da57cd1ed17df34

--- libpurple/plugins/ssl/ssl-nss.c
+++ libpurple/plugins/ssl/ssl-nss.c
@@ -511,6 +511,26 @@ x509_common_name (PurpleCertificate *crt)
 static gboolean
 x509_check_name (PurpleCertificate *crt, const gchar *name)
 {
+	CERTCertificate *crt_dat;
+	SECStatus st;
+	
+	g_return_val_if_fail(crt, FALSE);
+	g_return_val_if_fail(crt->scheme == &x509_nss, FALSE);
+
+	crt_dat = X509_NSS_DATA(crt);
+	g_return_val_if_fail(crt_dat, FALSE);
+
+	st = CERT_VerifyCertName(crt_dat, name);
+
+	if (st == SECSuccess) {
+		return TRUE;
+	}
+	else if (st == SECFailure) {
+		return FALSE;
+	}
+	
+	/* If we get here...bad things! */
+	g_assert(FALSE);
 	return FALSE;
 }
 
