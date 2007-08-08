0925792eb82bb0d33f71b5ff4c3f3e05a3386d32
Jeff Connelly <jeff2@soc.pidgin.im>
2007-08-08 04:09:17 +0000

    Use a better directory layout for the Win32 archive, now you can just unzip into the Pidgin installation directory and everything will be setup. Suggested by BigRedBrent at http://planet-soc.com/node/377#comment-2168.
    --HG--
    branch : soc.2007.msimprpl
    extra : convert_revision : e2632db6f1f142fbbe34194220f65d31e3fdcec9

--- libpurple/protocols/myspace/release.sh
+++ libpurple/protocols/myspace/release.sh
@@ -4,7 +4,7 @@
 
 # Package a new msimprpl for release. Must be run with bash.
 
-VERSION=0.13
+VERSION=0.14
 make
 # Include 'myspace' directory in archive, so it can easily be unextracted
 # into ~/pidgin/libpurple/protocols at the correct location.
@@ -19,6 +19,26 @@ mv ~/pidgin/config.h ~/pidgin/config.h-
 make -f Makefile.mingw
 mv ~/pidgin/config.h- ~/pidgin/config.h
 cp ~/pidgin/win32-install-dir/plugins/libmyspace.dll .
-# Zip is more common with Win32 users. Just include a few files in this archive.
-zip ../msimprpl-$VERSION-win32.zip libmyspace.dll README LICENSE CHANGES
+# Zip is more common with Win32 users. Just include a few files in this archive,
+# but (importantly) preserve the install directory structure!
+mkdir -p win32-archive/plugins
+cp libmyspace.dll win32-archive/plugins
+mkdir -p win32-archive/pixmaps/pidgin/protocols/{48,22,16}
+cp ~/pidgin/pidgin/pixmaps/protocols/48/myspace.png \
+        win32-archive/pixmaps/pidgin/protocols/48/
+cp ~/pidgin/pidgin/pixmaps/protocols/22/myspace.png \
+        win32-archive/pixmaps/pidgin/protocols/22/
+cp ~/pidgin/pidgin/pixmaps/protocols/16/myspace.png \
+        win32-archive/pixmaps/pidgin/protocols/16/
+mkdir -p win32-archive/pixmaps/emotes/default
+cp ~/pidgin/pidgin/pixmaps/emotes/default/22/theme \
+        win32-archive/pixmaps/emotes/default/theme
+        # Use DOS line endings and .txt file extension for convenience
+u2d < README > win32-archive/msimprpl-README.txt
+u2d < LICENSE > win32-archive/msimprpl-LICENSE.txt
+u2d < CHANGES > win32-archive/msimprpl-CHANGES.txt
+cd win32-archive
+zip -r ../../msimprpl-$VERSION-win32.zip *
+cd ..
+rm -rf win32-archive
 ls -l ../msimprpl-$VERSION*
