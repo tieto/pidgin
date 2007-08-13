9b4df9cf3be7587e1128fc41cb6ecd9c8026fcf7
William Ehlhardt <williamehlhardt@gmail.com>
2007-08-13 19:38:49 +0000

    - Move ssl-gnutls x509 registration until after GnuTLS itself is inited   to guarantee that it is ready to use when registered
    --HG--
    branch : soc.2007.certmgr
    extra : convert_revision : 448544795ea34e653d4774673183ac893768d9f9

--- libpurple/plugins/ssl/ssl-gnutls.c
+++ libpurple/plugins/ssl/ssl-gnutls.c
@@ -925,16 +925,16 @@ static gboolean
 plugin_load(PurplePlugin *plugin)
 {
 #ifdef HAVE_GNUTLS
-	/* Register that we're providing an X.509 CertScheme */
-	/* @TODO : error checking */
-	purple_certificate_register_scheme( &x509_gnutls );
-	
 	if(!purple_ssl_get_ops()) {
 		purple_ssl_set_ops(&ssl_ops);
 	}
 
 	/* Init GNUTLS now so others can use it even if sslconn never does */
 	ssl_gnutls_init_gnutls();
+
+	/* Register that we're providing an X.509 CertScheme */
+	/* @TODO : error checking */
+	purple_certificate_register_scheme( &x509_gnutls );
 
 	return TRUE;
 #else
