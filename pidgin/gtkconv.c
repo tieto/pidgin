2c791841ca8ceec6132aa38ed06488bb3cdacab6
Sean Egan <seanegan@pidgin.im>
2007-08-17 21:28:57 +0000

    remove double-clicking tab behavior
    --HG--
    extra : convert_revision : fa334638256d18d53ef63711570eb6e5a6c7d4ba

--- pidgin/gtkconv.c
+++ pidgin/gtkconv.c
@@ -8783,8 +8783,6 @@ pidgin_conv_tab_pack(PidginWindow *win, PidginConversation *gtkconv)
 	gtk_event_box_set_visible_window(GTK_EVENT_BOX(ebox), FALSE);
 #endif
 	gtk_container_add(GTK_CONTAINER(ebox), gtkconv->tabby);
-	g_signal_connect(G_OBJECT(ebox), "button-press-event",
-					G_CALLBACK(alias_double_click_cb), gtkconv);
 
 	if (gtkconv->tab_label->parent == NULL) {
 		/* Pack if it's a new widget */
