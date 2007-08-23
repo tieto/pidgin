5529be0313623ce0ae515335362399139b45c78f
Jeff Connelly <jeff2@soc.pidgin.im>
2007-08-23 04:41:40 +0000

    Update for 0.15 for GSoC final release for Google. Documentation updates only (no code changes).
    --HG--
    branch : soc.2007.msimprpl
    extra : convert_revision : e18d6803875edd5bf1ef2dd5c95324cbda8261c5

--- libpurple/protocols/myspace/myspace.h
+++ libpurple/protocols/myspace/myspace.h
@@ -90,7 +90,7 @@
 #define MSIM_LANGUAGE_NAME_ENGLISH  "ENGLISH"
 
 /* msimprpl version string of this plugin */
-#define MSIM_PRPL_VERSION_STRING    "0.14"
+#define MSIM_PRPL_VERSION_STRING    "0.15"
 
 /* Default server */
 #define MSIM_SERVER                 "im.myspace.akadns.net"
