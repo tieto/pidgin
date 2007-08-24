3818541de135dca6f0da7a6b9e09d67234bcc93f
Jeff Connelly <jeff2@soc.pidgin.im>
2007-08-24 06:39:51 +0000

    Update msimprpl to 0.16.
    --HG--
    extra : convert_revision : 195aef2568dd6ceacd046d089326f3f87a0e761a

--- libpurple/protocols/myspace/release.sh
+++ libpurple/protocols/myspace/release.sh
@@ -4,7 +4,7 @@
 
 # Package a new msimprpl for release. Must be run with bash.
 
-VERSION=0.15
+VERSION=0.16
 make
 # Include 'myspace' directory in archive, so it can easily be unextracted
 # into ~/pidgin/libpurple/protocols at the correct location.
