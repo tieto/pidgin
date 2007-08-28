a71a067080353408b8eb5d528a55f6735aa6bb21
William Ehlhardt <williamehlhardt@gmail.com>
2007-08-28 03:31:38 +0000

    - Change assert on ssl init failure to a mere warning
    --HG--
    extra : convert_revision : db94e3dde2f5edb422e7d7c1ec30474e2000997c

--- libpurple/sslconn.c
+++ libpurple/sslconn.c
@@ -297,7 +297,10 @@ purple_ssl_init(void)
 	/* Although purple_ssl_is_supported will do the initialization on
 	   command, SSL plugins tend to register CertificateSchemes as well
 	   as providing SSL ops. */
-	g_assert(ssl_init());
+	if ( !ssl_init() ) {
+		purple_debug_error("sslconn",
+				   "SSL subsystem failed to init!\n");
+	}
 }
 
 void
