c821246b5255b53fd185d2328c127bf739925922
Ethan Blanton <elb@pidgin.im>
2007-09-03 23:22:54 +0000

    Whitespace fix
    --HG--
    extra : convert_revision : 0372d7dd82f502ec3e68166bf2b289e71267897a

--- libpurple/protocols/irc/irc.c
+++ libpurple/protocols/irc/irc.c
@@ -347,7 +347,7 @@ static gboolean do_login(PurpleConnection *gc) {
 	const char *username, *realname;
 	struct irc_conn *irc = gc->proto_data;
 	const char *pass = purple_connection_get_password(gc);
-        int ret;
+	int ret;
 
 	if (pass && *pass) {
 		buf = irc_format(irc, "vv", "PASS", pass);
@@ -362,10 +362,10 @@ static gboolean do_login(PurpleConnection *gc) {
 
 	ret = gethostname(hostname, sizeof(hostname));
 	hostname[sizeof(hostname) - 1] = '\0';
-        if (ret < 0 || hostname[0] == '\0') {
-        	purple_debug_warning("irc", "gethostname() failed -- is your hostname set?");
-        	strcpy(hostname, "localhost");
-        }
+	if (ret < 0 || hostname[0] == '\0') {
+		purple_debug_warning("irc", "gethostname() failed -- is your hostname set?");
+		strcpy(hostname, "localhost");
+	}
 	realname = purple_account_get_string(irc->account, "realname", "");
 	username = purple_account_get_string(irc->account, "username", "");
 
