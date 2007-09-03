3aa0147d2e517614735eead2dd8a99d186110420
Jeff Connelly <jeff2@soc.pidgin.im>
2007-09-03 06:39:42 +0000

    Update msimprpl to 0.17, for an independent release for Pidgin 2.1.1.
    --HG--
    extra : convert_revision : 45b0379b2d5811e577a4bd4e39445639df061128

--- libpurple/protocols/myspace/release.sh
+++ libpurple/protocols/myspace/release.sh
@@ -4,10 +4,10 @@
 
 # Package a new msimprpl for release. Must be run with bash.
 
-VERSION=0.16
+VERSION=0.17
 make
 # Include 'myspace' directory in archive, so it can easily be unextracted
-# into ~/pidgin/libpurple/protocols at the correct location.
+# into ~/pidgin-2.1.1/libpurple/protocols at the correct location.
 # (if this command fails, run it manually).
 # This convenient command requires bash.
 cd ../../..
@@ -15,26 +15,26 @@ tar -cf libpurple/protocols/msimprpl-$VERSION.tar libpurple/protocols/myspace/{C
 cd libpurple/protocols/myspace
 gzip ../msimprpl-$VERSION.tar
 
-mv ~/pidgin/config.h ~/pidgin/config.h-
+mv ~/pidgin-2.1.1/config.h ~/pidgin-2.1.1/config.h-
 make -f Makefile.mingw
-mv ~/pidgin/config.h- ~/pidgin/config.h
-cp ~/pidgin/win32-install-dir/plugins/libmyspace.dll .
+mv ~/pidgin-2.1.1/config.h- ~/pidgin-2.1.1/config.h
+cp ~/pidgin-2.1.1/win32-install-dir/plugins/libmyspace.dll .
 # Zip is more common with Win32 users. Just include a few files in this archive,
 # but (importantly) preserve the install directory structure!
 mkdir -p win32-archive/plugins
 cp libmyspace.dll win32-archive/plugins
 mkdir -p win32-archive/pixmaps/pidgin/protocols/{48,22,16}
-cp ~/pidgin/win32-install-dir/pixmaps/pidgin/protocols/48/myspace.png \
+cp ~/pidgin-2.1.1/win32-install-dir/pixmaps/pidgin/protocols/48/myspace.png \
                 win32-archive/pixmaps/pidgin/protocols/48/
-cp ~/pidgin/win32-install-dir/pixmaps/pidgin/protocols/22/myspace.png \
+cp ~/pidgin-2.1.1/win32-install-dir/pixmaps/pidgin/protocols/22/myspace.png \
                 win32-archive/pixmaps/pidgin/protocols/22/
-cp ~/pidgin/win32-install-dir/pixmaps/pidgin/protocols/16/myspace.png \
+cp ~/pidgin-2.1.1/win32-install-dir/pixmaps/pidgin/protocols/16/myspace.png \
                 win32-archive/pixmaps/pidgin/protocols/16/
 mkdir -p win32-archive/pixmaps/pidgin/emotes/default
-cp ~/pidgin/win32-install-dir/pixmaps/pidgin/emotes/default/theme \
+cp ~/pidgin-2.1.1/win32-install-dir/pixmaps/pidgin/emotes/default/theme \
         win32-archive/pixmaps/pidgin/emotes/default/theme
 # Emoticons in MySpaceIM but not Pidgin 2.1.0
-cp ~/pidgin/win32-install-dir/pixmaps/pidgin/emotes/default/{sinister,sidefrown,pirate,mohawk,messed,bulgy-eyes}.png \
+cp ~/pidgin-2.1.1/win32-install-dir/pixmaps/pidgin/emotes/default/{sinister,sidefrown,pirate,mohawk,messed,bulgy-eyes}.png \
 	win32-archive/pixmaps/pidgin/emotes/default/
 
 # Use DOS line endings and .txt file extension for convenience
