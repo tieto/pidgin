6fa414fbf9ccb6e3d6d4020ea92d79f6dfbd4a4d
Jeff Connelly <jeff2@soc.pidgin.im>
2007-09-03 05:16:36 +0000

    Treat status messages as the plaintext that they are.
    Specifically, strip markup on user-entered status text, and escape special
    characters on incoming status messages. Now status messages should be displayed
    correctly.
    
    Closes #2637.
    
    --HG--
    extra : convert_revision : 037dd557f9293f0488bb536ed85c24acddad6553

--- libpurple/protocols/myspace/myspace.c
+++ libpurple/protocols/myspace/myspace.c
@@ -1066,8 +1066,11 @@ msim_set_status(PurpleAccount *account, PurpleStatus *status)
 	statstring = purple_status_get_attr_string(status, "message");
 
 	if (!statstring) {
-		statstring = g_strdup("");
+		statstring = "";
 	}
+
+	/* Status strings are plain text. */
+	statstring = purple_markup_strip_html(statstring);
 
 	msim_set_status_code(session, status_code, g_strdup(statstring));
 }
@@ -1713,7 +1716,7 @@ msim_incoming_status(MsimSession *session, MsimMessage *msg)
 	PurpleBuddyList *blist;
 	MsimUser *user;
 	GList *list;
-	gchar *status_headline;
+	gchar *status_headline, *status_headline_escaped;
 	gint status_code, purple_status_code;
 	gchar *username;
 
@@ -1768,8 +1771,16 @@ msim_incoming_status(MsimSession *session, MsimMessage *msg)
 		purple_debug_info("msim", "msim_status: found buddy %s\n", username);
 	}
 
+	/* The status headline is plaintext, but libpurple treats it as HTML,
+	 * so escape any HTML characters to their entity equivalents. */
+	status_headline_escaped = g_markup_escape_text(status_headline, strlen(status_headline));
+	g_free(status_headline);
+
+	if (user->headline) 
+		g_free(user->headline);
+
 	/* don't copy; let the MsimUser own the headline, memory-wise */
-	user->headline = status_headline;
+	user->headline = status_headline_escaped;
   
 	/* Set user status */
 	switch (status_code) {
