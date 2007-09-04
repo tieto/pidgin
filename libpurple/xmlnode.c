5f8a81303cdcc1a82542aa4db8ae486124b4d1a2
Mark Doliner <markdoliner@pidgin.im>
2007-09-04 08:53:18 +0000

    Print an error when there was a problem parsing some XML
    --HG--
    extra : convert_revision : 69a2a80d4279609bad90306a373f7373fc794d04

--- libpurple/xmlnode.c
+++ libpurple/xmlnode.c
@@ -549,7 +549,16 @@ static void
 xmlnode_parser_error_libxml(void *user_data, const char *msg, ...)
 {
 	struct _xmlnode_parser_data *xpd = user_data;
+	char errmsg[2048];
+	va_list args;
+
 	xpd->error = TRUE;
+
+	va_start(args, msg);
+	vsnprintf(errmsg, sizeof(errmsg), msg, args);
+	va_end(args);
+
+	purple_debug_error("xmlnode", "Error parsing xml file: %s\n", errmsg);
 }
 
 static xmlSAXHandler xmlnode_parser_libxml = {
