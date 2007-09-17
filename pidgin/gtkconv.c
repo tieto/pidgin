dc3b15ea248f8f1ae32ba3f62e5bd52bf7b8f6f8
Sadrul Habib Chowdhury <sadrul@pidgin.im>
2007-09-17 11:39:45 +0000

    Fix #3131 ('Ctrl , and Ctrl . moving tabs incorrectly'). Thanks to ftmichael for reporting the bug.
    --HG--
    extra : convert_revision : a4f6f19956ec6706019fd07c85e20d4d6c8f84e7

--- pidgin/gtkconv.c
+++ pidgin/gtkconv.c
@@ -1913,6 +1913,7 @@ conv_keypress_common(PidginConversation *gtkconv, GdkEventKey *event)
 				gtk_notebook_reorder_child(GTK_NOTEBOOK(win->notebook),
 						gtk_notebook_get_nth_page(GTK_NOTEBOOK(win->notebook), curconv),
 						curconv - 1);
+				return TRUE;
 				break;
 
 			case GDK_period:
@@ -1923,6 +1924,7 @@ conv_keypress_common(PidginConversation *gtkconv, GdkEventKey *event)
 #else
 						(curconv + 1) % g_list_length(GTK_NOTEBOOK(win->notebook)->children));
 #endif
+				return TRUE;
 				break;
 
 		} /* End of switch */
