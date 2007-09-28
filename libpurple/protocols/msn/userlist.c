aa3dd5fe1d6f72f4e49daf82814eaafe9f8c064b
Richard Laager <rlaager@pidgin.im>
2007-09-28 16:22:16 +0000

    Make this compile by removing a poorly-plucked #if 0
    --HG--
    branch : release-2.2.1
    extra : convert_revision : 0fcefcd05e8ebfeb34a2d39e7fe4bc011cc13093

--- libpurple/protocols/msn/userlist.c
+++ libpurple/protocols/msn/userlist.c
@@ -121,7 +121,6 @@ user_is_there(MsnUser *user, int list_id, int group_id)
 	return TRUE;
 }
 
-#if 0
 static const char*
 get_store_name(MsnUser *user)
 {
@@ -174,7 +173,6 @@ msn_request_add_group(MsnUserList *userlist, const char *who,
 
 	msn_cmdproc_send_trans(cmdproc, trans);
 }
-#endif
 
 /**************************************************************************
  * Server functions
