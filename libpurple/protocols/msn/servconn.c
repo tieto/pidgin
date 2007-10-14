cb30a1b8351fd0a89186219b0c1bcac93e4fbdfd
Mark Doliner <markdoliner@pidgin.im>
2007-10-14 09:57:32 +0000

    Fix all our calls to fcntl(listenfd, F_SETFL, O_NONBLOCK); fcntl() with F_SETFL overwrites the old flags with the new ones, so you should call fcntl() with F_GETFL, then OR that value with O_NONBLOCK before setting the flags.  We've been doing this wrong for a long time and it hasn't seemed to hurt anything, but I thought it would be good to fix it.
    --HG--
    extra : convert_revision : 8548e491a5b470d5665cb1cf87a7b0caaa3c87a5

--- libpurple/protocols/msn/servconn.c
+++ libpurple/protocols/msn/servconn.c
@@ -480,6 +480,7 @@ static int
 create_listener(int port)
 {
 	int fd;
+	int flags;
 	const int on = 1;
 
 #if 0
@@ -555,7 +556,8 @@ create_listener(int port)
 		return -1;
 	}
 
-	fcntl(fd, F_SETFL, O_NONBLOCK);
+	flags = fcntl(fd, F_GETFL);
+	fcntl(fd, F_SETFL, flags | O_NONBLOCK);
 
 	return fd;
 }
