e683d00d6fe5c19fffdf73f193db3e591f92b247
John Bailey <rekkanoryo@rekkanoryo.org>
2007-11-09 08:52:38 +0000

    applied changes from 7c2d1d363d48b1f08080714d46146c060bae216f              through 574b8609b14d3927cc786ae89c00705ede971d37
    applied changes from 1add15394515c75824600c86f6df714bc7017374
                 through 2cecdc50a5092d64d885f3eccee1a6b2209e6651
    
    applied changes from aae224d70daefc8067e62613f21822754de7feea
                 through de413390d6cfd199238da03e9d83b20ac231c258
    
    --HG--
    branch : release-2.2.3
    extra : convert_revision : bcefe7f341e6c1cafc7689a19e9a73e2cf1d3bfb

--- libpurple/protocols/oscar/oscar.c
+++ libpurple/protocols/oscar/oscar.c
@@ -778,7 +778,7 @@ oscar_user_info_convert_and_add(PurpleAccount *account, PurpleNotifyUserInfo *us
 	gchar *utf8;
 	
 	if (value && value[0] && (utf8 = oscar_utf8_try_convert(account, value))) {
-		purple_notify_user_info_add_pair(user_info, name, value);
+		purple_notify_user_info_add_pair(user_info, name, utf8);
 		g_free(utf8);
 	}
 }
