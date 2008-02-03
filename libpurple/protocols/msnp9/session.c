d27a84e3421e60bd957f4c76873275c94e78f7a1
Stu Tomlinson <nosnilmot@pidgin.im>
2008-02-03 17:33:02 +0000

    Fix the bug where mid-session notification server transfers end up with the server thinking we're trying to be invisible, which then prevents sending IMs and other things pesky users think should be possible with an IM client
    --HG--
    extra : convert_revision : d36c1d52c48fe90a9321222eca0615dbf4d0ad77

--- libpurple/protocols/msnp9/session.c
+++ libpurple/protocols/msnp9/session.c
@@ -397,8 +397,13 @@ msn_session_finish_login(MsnSession *session)
 	PurpleStoredImage *img;
 	const char *passport;
 
-	if (session->logged_in)
+	if (session->logged_in) {
+		/* We are probably here because of a mid-session notification server XFR
+		 * We must send a CHG now, otherwise the servers default to invisible,
+		 * and prevent things happening, like sending IMs */
+		msn_change_status(session);
 		return;
+	}
 
 	account = session->account;
 	gc = purple_account_get_connection(account);
