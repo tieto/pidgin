5f07202d3c32079eb085b2c223730e56b8204e4e
Sadrul Habib Chowdhury <sadrul@pidgin.im>
2008-02-12 03:58:58 +0000

    Highlighted message color has a higher priority over an action message color.
    --HG--
    extra : convert_revision : ec3eb6c7eb0cf35470ad2903ec365b5a64f74a5a

--- finch/gntconv.c
+++ finch/gntconv.c
@@ -837,7 +837,11 @@ finch_write_common(PurpleConversation *conv, const char *who, const char *messag
 
 		if (purple_message_meify((char*)message, -1)) {
 			name = g_strdup_printf("*** %s", who);
-			msgflags = gnt_color_pair(color_message_action);
+			if (!(flags & PURPLE_MESSAGE_SEND) &&
+					(flags & PURPLE_MESSAGE_NICK))
+				msgflags = gnt_color_pair(color_message_highlight);
+			else
+				msgflags = gnt_color_pair(color_message_action);
 			me = TRUE;
 		} else {
 			name =  g_strdup_printf("%s", who);
