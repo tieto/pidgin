bc05ea2e07dca01af1d0a02f669d98fe0df0475e
Sadrul Habib Chowdhury <sadrul@pidgin.im>
2008-03-07 23:19:47 +0000

    The patch to msn to allow sending custom smileys. Doesn't send all the custom smileys correctly at the moment. References #1187.
    --HG--
    branch : custom_smiley
    extra : convert_revision : 75e04b225965ca0ce88f89f2d921acb4ecd6dd58

--- libpurple/protocols/msn/slp.c
+++ libpurple/protocols/msn/slp.c
@@ -31,6 +31,8 @@
 #include "user.h"
 #include "switchboard.h"
 
+#include "smiley.h"
+
 /* ms to delay between sending buddy icon requests to the server. */
 #define BUDDY_ICON_DELAY 20000
 /*debug SLP*/
@@ -261,6 +263,7 @@ got_sessionreq(MsnSlpCall *slpcall, const char *branch,
 		char *msnobj_data;
 		PurpleStoredImage *img;
 		int type;
+		char *path;
 
 		/* Send Ok */
 		content = g_strdup_printf("SessionID: %lu\r\n\r\n",
@@ -278,22 +281,23 @@ got_sessionreq(MsnSlpCall *slpcall, const char *branch,
 		type = msn_object_get_type(obj);
 		g_free(msnobj_data);
 
-		if (!(type == MSN_OBJECT_USERTILE))
+		if ((type != MSN_OBJECT_USERTILE) && (type != MSN_OBJECT_EMOTICON))
 		{
 			purple_debug_error("msn", "Wrong object?\n");
 			msn_object_destroy(obj);
 			g_return_if_reached();
 		}
 
-		img = msn_object_get_image(obj);
+		path = g_build_filename(purple_smileys_get_storing_dir(),
+				obj->location, NULL);
+		msn_object_destroy(obj);
+		img = purple_imgstore_new_from_file(path);
+		g_free(path);
 		if (img == NULL)
 		{
 			purple_debug_error("msn", "Wrong object.\n");
-			msn_object_destroy(obj);
 			g_return_if_reached();
 		}
-
-		msn_object_destroy(obj);
 
 		slpsession = msn_slplink_find_slp_session(slplink,
 												  slpcall->session_id);
@@ -319,6 +323,7 @@ got_sessionreq(MsnSlpCall *slpcall, const char *branch,
 #endif
 		msn_slpmsg_set_image(slpmsg, img);
 		msn_slplink_queue_slpmsg(slplink, slpmsg);
+		purple_imgstore_unref(img);
 	}
 	else if (!strcmp(euf_guid, "5D3E02AB-6190-11D3-BBBB-00C04F795683"))
 	{
