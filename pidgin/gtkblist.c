4f7934483bda0bb6592866b0baa5a1c86c544653
Stu Tomlinson <nosnilmot@pidgin.im>
2008-03-12 01:34:40 +0000

    Fix a memory leak
    --HG--
    extra : convert_revision : 5c34be2e2afa892d272763a3b1404664cc513e2d

--- pidgin/gtkblist.c
+++ pidgin/gtkblist.c
@@ -3132,7 +3132,6 @@ static char *pidgin_get_tooltip_text(PurpleBlistNode *node, gboolean full)
 		GList *cur;
 		struct proto_chat_entry *pce;
 		char *name, *value;
-		const char *chat_name;
 		PurpleConversation *conv;
 		PidginBlistNode *bnode = node->ui_data;
 
@@ -3150,13 +3149,15 @@ static char *pidgin_get_tooltip_text(PurpleBlistNode *node, gboolean full)
 		if (bnode && bnode->conv.conv) {
 			conv = bnode->conv.conv;
 		} else {
+			char *chat_name;
 			if (prpl_info && prpl_info->get_chat_name)
 				chat_name = prpl_info->get_chat_name(chat->components);
 			else
-				chat_name = purple_chat_get_name(chat);
+				chat_name = g_strdup(purple_chat_get_name(chat));
 
 			conv = purple_find_conversation_with_account(PURPLE_CONV_TYPE_CHAT, chat_name,
 					chat->account);
+			g_free(chat_name);
 		}
 		if (conv && prpl_info && (prpl_info->options & OPT_PROTO_CHAT_TOPIC) &&
 				!purple_conv_chat_has_left(PURPLE_CONV_CHAT(conv))) {
