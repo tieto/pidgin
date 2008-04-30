63cc7e68a0266642c3365641ef5ba4e78be2b92b
John Bailey <rekkanoryo@rekkanoryo.org>
2008-04-30 05:56:40 +0000

    This fixes the lack of the Local Alias field for IRC accounts since I moved the field to the User Options section earlier.
    --HG--
    extra : convert_revision : 48a880a9d212aec86b08bab7bec6296ac8c826ea

--- pidgin/gtkaccount.c
+++ pidgin/gtkaccount.c
@@ -654,6 +654,7 @@ add_user_options(AccountPrefsDialog *dialog, GtkWidget *parent)
 		set_dialog_icon(dialog, NULL, 0, NULL);
 	}
 
+#if 0
 	if (!dialog->prpl_info ||
 			(!(dialog->prpl_info->options & OPT_PROTO_MAIL_CHECK) &&
 			 (dialog->prpl_info->icon_spec.format ==  NULL))) {
@@ -661,6 +662,7 @@ add_user_options(AccountPrefsDialog *dialog, GtkWidget *parent)
 		/* Nothing to see :( aww. */
 		gtk_widget_hide(dialog->user_frame);
 	}
+#endif
 }
 
 static void
