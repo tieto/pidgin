1b2ed536bf7bbcb66828dc9f300dc496826478ff
Sulabh Mahajan <sulabh@pidgin.im>
2008-08-20 21:17:04 +0000

    Dont try a p2p connection to non yahoo buddies
    --HG--
    branch : soc.2008.yahoo
    extra : convert_revision : df0f1863e6996c97ef4e7a9b57c57876b6a61e43

--- libpurple/protocols/yahoo/yahoo.c
+++ libpurple/protocols/yahoo/yahoo.c
@@ -2642,6 +2642,10 @@ static void yahoo_send_p2p_pkt(PurpleConnection *gc, const char *who, int val_13
 	if( strcmp(purple_normalize(account, purple_account_get_username(account)), who) == 0)
 		return;
 
+	/* Dont send p2p packet to buddies of other protocols */
+	if(f->protocol)
+		return;
+
 	/* send packet to only those friends who arent p2p connected and to whom we havent already sent. Do not send if this condition doesn't hold good */ 
 	if( !( f && (yahoo_friend_get_p2p_status(f) == YAHOO_P2PSTATUS_NOT_CONNECTED) && (f->p2p_packet_sent == 0)) )
 		return;
