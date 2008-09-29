837d9c8f5cc3999e74fb14c9be9ecf4385e7fb16
Ethan Blanton <elb@pidgin.im>
2008-09-29 00:59:53 +0000

    Do not set the connect server for Google Talk accounts.  Setting the connect server causes problems for SSL certificate verification.  If this is a problem, revert.
    --HG--
    extra : convert_revision : 6cc280ceab18bb5790d00f56d9ceef85b21f2d15

--- pidgin/gtkaccount.c
+++ pidgin/gtkaccount.c
@@ -858,13 +858,6 @@ add_protocol_options(AccountPrefsDialog *dialog, GtkWidget *parent)
 						gtk_entry_set_invisible_char(GTK_ENTRY(entry), PIDGIN_INVISIBLE_CHAR);
 				}
 
-				/* Google Talk default domain hackery! */
-				menu = gtk_option_menu_get_menu(GTK_OPTION_MENU(dialog->protocol_menu));
-				item = gtk_menu_get_active(GTK_MENU(menu));
-				if (str_value == NULL && g_object_get_data(G_OBJECT(item), "fake") &&
-					!strcmp(_("Connect server"),  purple_account_option_get_text(option)))
-					str_value = "talk.google.com";
-
 				if (str_value != NULL)
 					gtk_entry_set_text(GTK_ENTRY(entry), str_value);
 
