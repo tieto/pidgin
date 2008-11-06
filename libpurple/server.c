84418726b56a0bd558d53eecdd290ac3b94fe16c
Michael Ruprecht <maiku@pidgin.im>
2008-11-06 08:26:14 +0000

    Remove unnecessary changes in server.c.
    --HG--
    branch : maiku.vv
    extra : convert_revision : fb14d1a502df2592c021ec065b5556daf0b73068

--- libpurple/server.c
+++ libpurple/server.c
@@ -37,9 +37,6 @@
 #include "server.h"
 #include "status.h"
 #include "util.h"
-#ifdef USE_VV
-#include "media.h"
-#endif
 
 #define SECS_BEFORE_RESENDING_AUTORESPONSE 600
 #define SEX_BEFORE_RESENDING_AUTORESPONSE "Only after you're married"
@@ -982,4 +979,3 @@ void serv_send_file(PurpleConnection *gc, const char *who, const char *file)
 		}
 	}
 }
-
