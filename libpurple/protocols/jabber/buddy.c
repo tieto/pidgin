58d6192740855e71ad76730e399ca70b363c600e
Marcus Lundblad <malu@pidgin.im>
2008-11-18 20:22:57 +0000

    Patch from darkrain42 Introduce a temporary variable to aid in readability
    --HG--
    branch : cpw.malu.xmpp.idle
    extra : convert_revision : 434c704edf18753d8e5b5148fdca3e3c83c3f018

--- libpurple/protocols/jabber/buddy.c
+++ libpurple/protocols/jabber/buddy.c
@@ -99,19 +99,20 @@ JabberBuddyResource *jabber_buddy_find_resource(JabberBuddy *jb,
 
 	for(l = jb->resources; l; l = l->next)
 	{
+		JabberBuddyResource *tmp = l->data;
 		if(!jbr && !resource) {
-			jbr = l->data;
+			jbr = tmp;
 		} else if(!resource) {
-			if(((JabberBuddyResource *)l->data)->priority > jbr->priority)
-				jbr = l->data;
-			else if(((JabberBuddyResource *)l->data)->priority == jbr->priority) {
+			if(tmp->priority > jbr->priority)
+				jbr = tmp;
+			else if(tmp->priority == jbr->priority) {
 				/* Determine if this resource is more available than the one we've currently chosen */
-				switch(((JabberBuddyResource *)l->data)->state) {
+				switch(tmp->state) {
 					case JABBER_BUDDY_STATE_ONLINE:
 					case JABBER_BUDDY_STATE_CHAT:
 						/* This resource is online/chatty. Prefer to one which isn't either. */
 						if ((jbr->state != JABBER_BUDDY_STATE_ONLINE) && (jbr->state != JABBER_BUDDY_STATE_CHAT))
-							jbr = l->data;
+							jbr = tmp;
 						break;
 					case JABBER_BUDDY_STATE_AWAY:
 					case JABBER_BUDDY_STATE_DND:
@@ -119,12 +120,12 @@ JabberBuddyResource *jabber_buddy_find_resource(JabberBuddy *jb,
 						/* This resource is away/dnd/unavailable. Prefer to one which is extended away or unknown. */
 						if ((jbr->state == JABBER_BUDDY_STATE_XA) || 
 							(jbr->state == JABBER_BUDDY_STATE_UNKNOWN) || (jbr->state == JABBER_BUDDY_STATE_ERROR))
-							jbr = l->data;
+							jbr = tmp;
 						break;
 					case JABBER_BUDDY_STATE_XA:
 						/* This resource is extended away. That's better than unknown. */
 						if ((jbr->state == JABBER_BUDDY_STATE_UNKNOWN) || (jbr->state == JABBER_BUDDY_STATE_ERROR))
-							jbr = l->data;
+							jbr = tmp;
 						break;
 					case JABBER_BUDDY_STATE_UNKNOWN:
 					case JABBER_BUDDY_STATE_ERROR:
@@ -132,9 +133,9 @@ JabberBuddyResource *jabber_buddy_find_resource(JabberBuddy *jb,
 						break;
 				}
 			}
-		} else if(((JabberBuddyResource *)l->data)->name) {
-			if(!strcmp(((JabberBuddyResource *)l->data)->name, resource)) {
-				jbr = l->data;
+		} else if(tmp->name) {
+			if(!strcmp(tmp->name, resource)) {
+				jbr = tmp;
 				break;
 			}
 		}
