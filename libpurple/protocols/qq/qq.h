897cb98afcf5502985019f6bd47edb20f9c17b20
Hu Yong <ccpaging@gmail.com>
2008-11-19 08:36:18 +0000

    Add a new variable to produce IM sequence number
    --HG--
    branch : openq
    extra : convert_revision : 9bcf64a321bdcc8c0cf3759bb75b74a156888e84

--- libpurple/protocols/qq/qq.h
+++ libpurple/protocols/qq/qq.h
@@ -183,7 +183,7 @@ struct _qq_data {
 	gboolean is_show_notice;
 	gboolean is_show_news;
 
-	guint16 send_im_seq;		/* send IM sequence number */
+	guint16 send_im_id;		/* send IM sequence number */
 };
 
 #endif
