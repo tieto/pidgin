1fa2d156bb9713c9240f9a316a4c62351f83814e
Paul Aurich <darkrain42@pidgin.im>
2008-11-23 03:52:33 +0000

    Fix a null dereference on non-connected accounts
    --HG--
    branch : cpw.darkrain42.xmpp.bosh
    extra : convert_revision : 0da3644551b2442b2db4cf9d828865d167df8072

--- libpurple/protocols/jabber/caps.c
+++ libpurple/protocols/jabber/caps.c
@@ -874,7 +874,7 @@ void jabber_caps_broadcast_change()
 	for (node = accounts; node; node = node->next) {
 		PurpleAccount *account = node->data;
 		const char *prpl_id = purple_account_get_protocol_id(account);
-		if (!strcmp("prpl-jabber", prpl_id)) {
+		if (!strcmp("prpl-jabber", prpl_id) && purple_account_is_connected(account)) {
 			PurpleConnection *gc = purple_account_get_connection(account);
 			jabber_presence_send(gc->proto_data, TRUE);
 		}
