aeaef051fd94bf7be524569c1ab6fa55b69a2d2b
Marcus Lundblad <malu@pidgin.im>
2009-01-01 21:01:42 +0000

    Use purple_prpl_got_attention
    --HG--
    branch : cpw.attention_ui
    extra : convert_revision : 4f5eec798a17d8dc80d236b4639d0eebd186e96c

--- libpurple/protocols/jabber/message.c
+++ libpurple/protocols/jabber/message.c
@@ -302,16 +302,16 @@ static void handle_buzz(JabberMessage *jm) {
 	
 	account = purple_connection_get_account(jm->js->gc);
 	
-	if ((buddy = purple_find_buddy(account, jm->from)) != NULL)
-		username = g_markup_escape_text(purple_buddy_get_alias(buddy), -1);
-	else
+	if ((buddy = purple_find_buddy(account, jm->from)) == NULL)
 		return; /* Do not accept buzzes from unknown people */
 
-	c = purple_conversation_new(PURPLE_CONV_TYPE_IM, account, jm->from);
+	c = purple_find_conversation_with_account(PURPLE_CONV_TYPE_IM, jm->from, account);
+	if (c == NULL)
+		c = purple_conversation_new(PURPLE_CONV_TYPE_IM, account, jm->from);
 
-	str = g_strdup_printf(_("%s has buzzed you!"), username);
-	
-	purple_conversation_write(c, NULL, str, PURPLE_MESSAGE_SYSTEM|PURPLE_MESSAGE_NOTIFY, time(NULL));
+	username = g_markup_escape_text(purple_buddy_get_alias(buddy), -1);
+	/* xmpp only has 1 attention type, so index is 0 */
+	purple_prpl_got_attention(jm->js->gc, username, 0);
 	purple_conversation_attention(c, username, 0, PURPLE_MESSAGE_RECV, time(NULL));
 	g_free(username);
 	g_free(str);
