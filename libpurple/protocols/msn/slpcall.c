3a007e2b4d086ad42a0654593fe69acc8d1b9e78
Elliott Sales de Andrade <qulogic@pidgin.im>
2009-01-19 00:58:19 +0000

    A patch from Thomas Gibson-Robinson to fix receiving buddy icons, custom emoticons and file transfers from WLM 9. I have a feeling this should really be a bitwise test (ie, flags | 0x20 == 0x20) but I can't really find many supportive docs for FT things and I'm no expert on these MSN SLP bits.
    Fixes #7180.
    Fixes #7294.
    
    --HG--
    extra : convert_revision : 8577210d86a9a6cd448208a4ef92c42cd39200f7

--- libpurple/protocols/msn/slpcall.c
+++ libpurple/protocols/msn/slpcall.c
@@ -206,7 +206,7 @@ msn_slp_process_msg(MsnSlpLink *slplink, MsnSlpMessage *slpmsg)
 	body = slpmsg->buffer;
 	body_len = slpmsg->size;
 
-	if (slpmsg->flags == 0x0)
+	if (slpmsg->flags == 0x0 || slpmsg->flags == 0x1000000)
 	{
 		char *body_str;
 
@@ -214,7 +214,9 @@ msn_slp_process_msg(MsnSlpLink *slplink, MsnSlpMessage *slpmsg)
 		slpcall = msn_slp_sip_recv(slplink, body_str);
 		g_free(body_str);
 	}
-	else if (slpmsg->flags == 0x20 || slpmsg->flags == 0x1000030)
+	else if (slpmsg->flags == 0x20 ||
+	         slpmsg->flags == 0x1000020 ||
+	         slpmsg->flags == 0x1000030)
 	{
 		slpcall = msn_slplink_find_slp_call_with_session_id(slplink, slpmsg->session_id);
 
