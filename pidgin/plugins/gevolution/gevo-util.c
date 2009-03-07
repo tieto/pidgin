fc766d7736d315adee3bd7cfc1be250ebce2f0d6
Paul Aurich <darkrain42@pidgin.im>
2009-03-07 22:40:30 +0000

    Fixing busted merge
    --HG--
    branch : cpw.darkrain42.buddy-add
    extra : convert_revision : fa0c982c0252e44f0310912b9dc35ca8d6334b51

--- pidgin/plugins/gevolution/gevo-util.c
+++ pidgin/plugins/gevolution/gevo-util.c
@@ -41,7 +41,7 @@ gevo_add_buddy(PurpleAccount *account, const char *group_name,
 		purple_blist_add_group(group, NULL);
 	}
 
-	if ((buddy = purple_find_buddy_in_group(account, buddy_name, group))) {
+	if ((buddy = purple_find_buddy_in_group(account, buddy_name, group)))
 	{	
 		buddy = purple_buddy_new(account, buddy_name, alias);
 		purple_blist_add_buddy(buddy, NULL, group, NULL);
