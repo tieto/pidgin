3fdef4323681354b4a22a17b683c32af8ccbdd36
Paul Aurich <darkrain42@pidgin.im>
2009-03-25 06:07:44 +0000

    Fix displaying of status messages in the conversation infopane.
    Rekkanoryo thinks this is okay (and it doesn't seem to have broken anything
    else that I've noticed).
    
    Fixes #8670.
    
    --HG--
    extra : convert_revision : 402af0151e0ef3f2ad2bd757aadbbe514321e48c

--- pidgin/gtkblist.c
+++ pidgin/gtkblist.c
@@ -3898,7 +3898,7 @@ pidgin_blist_get_name_markup(PurpleBuddy *b, gboolean selected, gboolean aliased
 	presence = purple_buddy_get_presence(b);
 
 	/* Name is all that is needed */
-	if (aliased && biglist) {
+	if (!aliased || biglist) {
 
 		/* Status Info */
 		prpl = purple_find_prpl(purple_account_get_protocol_id(b->account));
@@ -4038,7 +4038,7 @@ pidgin_blist_get_name_markup(PurpleBuddy *b, gboolean selected, gboolean aliased
 	}
 
 	/* Put it all together */
-	if (aliased && biglist && (statustext || idletime)) {
+	if ((!aliased || biglist) && (statustext || idletime)) {
 		/* using <span size='smaller'> breaks the status, so it must be seperated into <small><span>*/
 		if (name_color) {
 			text = g_strdup_printf("<span font_desc='%s' foreground='%s'>%s</span>\n"
