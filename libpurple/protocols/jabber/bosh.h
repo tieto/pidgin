577a04c86bb2580c3909e6cad5b7cd240b251848
Paul Aurich <darkrain42@pidgin.im>
2009-04-18 17:54:24 +0000

    Header file cleanup
    --HG--
    branch : cpw.darkrain42.xmpp.bosh
    extra : convert_revision : a205fd5eb99225943698ad84d1017091ad48a9a2

--- libpurple/protocols/jabber/bosh.h
+++ libpurple/protocols/jabber/bosh.h
@@ -1,5 +1,5 @@
 /**
- * @file bosh.h Buddy handlers
+ * @file bosh.h Bidirectional-streams over Synchronous HTTP (BOSH) (XEP-0124 and XEP-0206)
  *
  * purple
  *
@@ -19,8 +19,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111-1301  USA
  */
-#ifndef _PURPLE_JABBER_BOSH_H_
-#define _PURPLE_JABBER_BOSH_H_
+#ifndef PURPLE_JABBER_BOSH_H_
+#define PURPLE_JABBER_BOSH_H_
 
 typedef struct _PurpleBOSHConnection PurpleBOSHConnection;
 
@@ -38,4 +38,4 @@ void jabber_bosh_connection_connect(PurpleBOSHConnection *conn);
 void jabber_bosh_connection_close(PurpleBOSHConnection *conn);
 void jabber_bosh_connection_send_raw(PurpleBOSHConnection *conn, const char *data);
 void jabber_bosh_connection_refresh(PurpleBOSHConnection *conn);
-#endif /* _PURPLE_JABBER_BOSH_H_ */
+#endif /* PURPLE_JABBER_BOSH_H_ */
