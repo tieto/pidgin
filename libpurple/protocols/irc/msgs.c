9704b052cf42be2fddca8d1e49bf103340369603
Sadrul Habib Chowdhury <sadrul@pidgin.im>
2009-04-25 22:18:51 +0000

    Don't corrupt memory if nick can not be changed. You can be upset if you can't change your nick, I guess, but that's no reason to corrupt your memory.
    --HG--
    extra : convert_revision : 68cb5581c551eabb57bcdf437642ee9e6786bc64

--- libpurple/protocols/irc/msgs.c
+++ libpurple/protocols/irc/msgs.c
@@ -1019,6 +1019,7 @@ void irc_msg_nickused(struct irc_conn *irc, const char *name, const char *from, 
 				    _("Nickname in use"), buf);
 		g_free(buf);
 		g_free(irc->reqnick);
+		return;
 	}
 
 	if (strlen(args[1]) < strlen(irc->reqnick) || irc->nickused)
