d8331ef4a66473d563321e2fa8e2c9e84a714986
Ethan Blanton <elb@pidgin.im>
2009-04-28 20:40:46 +0000

    Hand-pluck of 68cb5581c551eabb57bcdf437642ee9e6786bc64
    --HG--
    branch : release-2.5.6
    extra : convert_revision : 2148612a951b799182f85ab778378f88113cdf01

--- libpurple/protocols/irc/msgs.c
+++ libpurple/protocols/irc/msgs.c
@@ -1009,6 +1009,7 @@ void irc_msg_nickused(struct irc_conn *irc, const char *name, const char *from, 
 		g_free(buf);
 		g_free(irc->reqnick);
 		irc->reqnick = NULL;
+		return;
 	}
 
 	if (strlen(args[1]) < strlen(irc->reqnick) || irc->nickused)
