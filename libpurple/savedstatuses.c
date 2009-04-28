eeda988e5f356f6b2089dd76b0dc0c22e24dbef5
Paul Aurich <darkrain42@pidgin.im>
2009-04-28 15:47:36 +0000

    Adjust some idle handling code to fix "wedging" idle reporting into never reporting idle properly until restarting Pidgin.  Fixes #2002.
    committer: John Bailey <rekkanoryo@rekkanoryo.org>
    
    --HG--
    extra : convert_revision : 6b5084da4501a491dab88006847c4e99fd4ce044

--- libpurple/savedstatuses.c
+++ libpurple/savedstatuses.c
@@ -870,14 +870,14 @@ purple_savedstatus_set_idleaway(gboolean idleaway)
 		/* Don't need to do anything */
 		return;
 
-	/* Changing our status makes us un-idle */
-	if (!idleaway)
-		purple_idle_touch();
-
 	old = purple_savedstatus_get_current();
 	saved_status = idleaway ? purple_savedstatus_get_idleaway()
 			: purple_savedstatus_get_default();
 	purple_prefs_set_bool("/purple/savedstatus/isidleaway", idleaway);
+
+	/* Changing our status makes us un-idle */
+	if (!idleaway)
+		purple_idle_touch();
 
 	if (idleaway && (purple_savedstatus_get_type(old) != PURPLE_STATUS_AVAILABLE))
 		/* Our global status is already "away," so don't change anything */
