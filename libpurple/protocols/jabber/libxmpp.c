346229ffc6a36db32e765e90849aa707e8a4d212
Paul Aurich <darkrain42@pidgin.im>
2009-05-06 21:58:50 +0000

    disapproval of revision '4f1de8106fb9a269bfd96f28b83b52a144cd9a72'
    --HG--
    extra : convert_revision : f54a277603759665b5cdb574b3a9f3288ba28782

--- libpurple/protocols/jabber/libxmpp.c
+++ libpurple/protocols/jabber/libxmpp.c
@@ -252,7 +252,9 @@ init_plugin(PurplePlugin *plugin)
 						  option);
 
 	option = purple_account_option_string_new(_("File transfer proxies"),
-						  "ft_proxies", NULL);
+						  "ft_proxies",
+						/* TODO: Is this an acceptable default? */
+						  "proxy.jabber.org");
 	prpl_info.protocol_options = g_list_append(prpl_info.protocol_options,
 						  option);
 
