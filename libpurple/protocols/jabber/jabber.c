359d398b3b1528970629cb78badd8ce9857e7c6f
Paul Aurich <darkrain42@pidgin.im>
2009-05-30 00:21:37 +0000

    disapproval of revision '9dbb983c97d60cfd2c1a88f99deace901b5d3554'
    xmlParseChunk is not reentrant. This /just happened/ to work for '<proceed/>' but not '<proceed></proceed>'. Closes #9271.
    
    --HG--
    extra : convert_revision : 7c614b09b72037ebd86c03fbbeac7794a155547b

--- libpurple/protocols/jabber/jabber.c
+++ libpurple/protocols/jabber/jabber.c
@@ -79,10 +79,6 @@ static void jabber_stream_init(JabberStream *js)
 						  "xmlns:stream='http://etherx.jabber.org/streams' "
 						  "version='1.0'>",
 						  js->user->domain);
-	if (js->reinit)
-		/* Close down the current stream to keep the XML parser happy */
-		jabber_parser_close_stream(js);
-
 	/* setup the parser fresh for each stream */
 	jabber_parser_setup(js);
 	jabber_send_raw(js, open_stream, -1);
@@ -632,9 +628,6 @@ jabber_ssl_connect_failure(PurpleSslConnection *gsc, PurpleSslErrorType error,
 
 static void tls_init(JabberStream *js)
 {
-	/* Close down the current stream to keep the XML parser happy */
-	jabber_parser_close_stream(js);
-
 	purple_input_remove(js->gc->inpa);
 	js->gc->inpa = 0;
 	js->gsc = purple_ssl_connect_with_host_fd(js->gc->account, js->fd,
@@ -1341,16 +1334,6 @@ void jabber_close(PurpleConnection *gc)
 	 */
 	if (!gc->disconnect_timeout)
 		jabber_send_raw(js, "</stream:stream>", -1);
-
-	if (!purple_account_get_current_error(purple_connection_get_account(gc))) {
-		/*
-		 * The common case is user-triggered, so we never receive a
-		 * </stream:stream> from the server when disconnecting, so silence the
-		 * parser's warnings. On errors, though, the server terminated the
-		 * connection, so we should have received a real </stream:stream>.
-		 */
-		jabber_parser_close_stream(js);
-	}
 
 	if (js->srv_query_data)
 		purple_srv_cancel(js->srv_query_data);
