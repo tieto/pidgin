2bdb444b8aa45e71559bcfb576e53bcc0be3d15a
Mark Doliner <markdoliner@pidgin.im>
2009-07-14 17:40:04 +0000

    This brace was a little goofy
    --HG--
    extra : convert_revision : 25fba04ec8d380ed765938363611931cc89b6097

--- libpurple/status.c
+++ libpurple/status.c
@@ -1533,7 +1533,8 @@ purple_presence_is_status_primitive_active(const PurplePresence *presence,
 	g_return_val_if_fail(primitive != PURPLE_STATUS_UNSET, FALSE);
 
 	for (l = purple_presence_get_statuses(presence);
-	     l != NULL; l = l->next)	{
+	     l != NULL; l = l->next)
+	{
 		PurpleStatus *temp_status = l->data;
 		PurpleStatusType *type = purple_status_get_type(temp_status);
 
