8aaae171001740d5608f8ddd0532486202d531f1
Mark Doliner <markdoliner@pidgin.im>
2009-07-19 16:28:32 +0000

    Correct the indentation of this macro by removing one level of indentation here
    --HG--
    extra : convert_revision : 567c064768c5476d5561dc04df996fb60c1aaec2

--- libpurple/util.c
+++ libpurple/util.c
@@ -1460,26 +1460,26 @@ struct purple_parse_tag {
 						g_string_free(innards, TRUE); \
 						continue; \
 					} \
-						if(!g_ascii_strncasecmp(c, "<" x, strlen("<" x)) && \
-								(*(c+strlen("<" x)) == '>' || \
-								 !g_ascii_strncasecmp(c+strlen("<" x), "/>", 2))) { \
+					if(!g_ascii_strncasecmp(c, "<" x, strlen("<" x)) && \
+							(*(c+strlen("<" x)) == '>' || \
+							 !g_ascii_strncasecmp(c+strlen("<" x), "/>", 2))) { \
+						if(xhtml) \
+							xhtml = g_string_append(xhtml, "<" y); \
+						c += strlen("<" x); \
+						if(*c != '/') { \
+							struct purple_parse_tag *pt = g_new0(struct purple_parse_tag, 1); \
+							pt->src_tag = x; \
+							pt->dest_tag = y; \
+							tags = g_list_prepend(tags, pt); \
 							if(xhtml) \
-								xhtml = g_string_append(xhtml, "<" y); \
-							c += strlen("<" x); \
-							if(*c != '/') { \
-								struct purple_parse_tag *pt = g_new0(struct purple_parse_tag, 1); \
-								pt->src_tag = x; \
-								pt->dest_tag = y; \
-								tags = g_list_prepend(tags, pt); \
-								if(xhtml) \
-									xhtml = g_string_append_c(xhtml, '>'); \
-							} else { \
-								if(xhtml) \
-									xhtml = g_string_append(xhtml, "/>");\
-							} \
-							c = strchr(c, '>') + 1; \
-							continue; \
-						}
+								xhtml = g_string_append_c(xhtml, '>'); \
+						} else { \
+							if(xhtml) \
+								xhtml = g_string_append(xhtml, "/>");\
+						} \
+						c = strchr(c, '>') + 1; \
+						continue; \
+					} \
 #define ALLOW_TAG(x) ALLOW_TAG_ALT(x, x)
 void
 purple_markup_html_to_xhtml(const char *html, char **xhtml_out,
