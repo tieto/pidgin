2d1b807f30d06ab0d8b8fdbb46187a29188a2fc1
Arnold Noronha <tdrhq@soc.pidgin.im>
2009-08-07 18:38:31 +0000

    cleaning up code.
    --HG--
    branch : soc.2009.webkitmessageview
    extra : convert_revision : c4a9d3c1001b23a9ac68c0a22a1a8355b984a946

--- pidgin/gtkwebview.c
+++ pidgin/gtkwebview.c
@@ -1,3 +1,33 @@
+/*
+ * @file gtkwebview.c GTK+ WebKitWebView wrapper class.
+ * @ingroup pidgin
+ */
+
+/* pidgin
+ *
+ * Pidgin is the legal property of its developers, whose names are too numerous
+ * to list here.  Please refer to the COPYRIGHT file distributed with this
+ * source distribution.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111-1301  USA
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 
 #include <ctype.h>
 #include <string.h>
@@ -7,6 +37,7 @@
 
 #include "util.h"
 #include "gtkwebview.h"
+#include "imgstore.h"
 
 static WebKitWebViewClass *parent_class = NULL;
 
@@ -16,7 +47,7 @@ GtkWidget* gtk_webview_new ()
 }
 
 static char*
-get_img_filename_by_id (GtkWebView* view, int id)
+get_image_filename_from_id (GtkWebView* view, int id)
 {
 	char *filename = NULL;
 	FILE *file;
@@ -101,7 +132,7 @@ replace_img_id_with_src (GtkWebView *view, const char* html)
 		/* let's dump this, tag and then dump the src information */
 		g_string_append_len (buffer, img, cur - img);
 
-		g_string_append_printf (buffer, " src='file://%s' ", get_img_filename_by_id (view, nid));
+		g_string_append_printf (buffer, " src='file://%s' ", get_image_filename_from_id (view, nid));
 	}
 	return g_string_free (buffer, FALSE);
 }
