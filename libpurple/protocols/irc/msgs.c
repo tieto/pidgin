8b6e3e48f7aaefc4c30db528014b4081f2b1a8a1
Mark Doliner <markdoliner@pidgin.im>
2009-09-04 06:33:46 +0000

    Oh boy, fix a missing semi-colon
    --HG--
    extra : convert_revision : a10f18c7ebd252d1627bda97ac796e21bd81a6c1

--- libpurple/protocols/irc/msgs.c
+++ libpurple/protocols/irc/msgs.c
@@ -451,7 +451,7 @@ void irc_msg_topic(struct irc_conn *irc, const char *name, const char *from, cha
 		topic = irc_mirc2txt (args[1]);
 	} else {
 		if (!args[0] || !args[1] || !args[2])
-			return
+			return;
 		chan = args[1];
 		topic = irc_mirc2txt (args[2]);
 	}
