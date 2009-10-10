cda335bfcc6070dd626ce98dcc089b7a63a21486
Paul Aurich <darkrain42@pidgin.im>
2009-10-10 03:27:31 +0000

    Check the GnuTLS version before using a recent-ish flag.  Fixes #10412.
    This was reported by Kev (of the XMPP world), who is using either Debian
    Lenny.
    
    --HG--
    extra : convert_revision : 765fe4913b177ce6912325cd1593df00d18aef42

--- libpurple/plugins/ssl/ssl-gnutls.c
+++ libpurple/plugins/ssl/ssl-gnutls.c
@@ -730,6 +730,7 @@ x509_certificate_signed_by(PurpleCertificate * crt,
 		return FALSE;
 	}
 
+#ifdef HAVE_GNUTLS_CERT_INSECURE_ALGORITHM
 	if (verify & GNUTLS_CERT_INSECURE_ALGORITHM) {
 		/*
 		 * A certificate in the chain is signed with an insecure
@@ -743,6 +744,7 @@ x509_certificate_signed_by(PurpleCertificate * crt,
 				"Insecure hash algorithm used by %s to sign %s\n",
 				issuer_id, crt_id);
 	}
+#endif
 
 	if (verify & GNUTLS_CERT_INVALID) {
 		/* Signature didn't check out, but at least
