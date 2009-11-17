271090ad914cf1e0d3da0737805358f3cb6e27ac
John Bailey <rekkanoryo@rekkanoryo.org>
2009-11-17 06:45:20 +0000

    I *think* this properly adds back the font preference stuff I axed earlier, but only for Windows.
    --HG--
    branch : cpw.rekkanoryo.prefs_window
    extra : convert_revision : 7bf03624c836a9d7e0b8b87e2528ef4195702631

--- pidgin/gtkconv.c
+++ pidgin/gtkconv.c
@@ -7931,6 +7931,11 @@ pidgin_conversations_init(void)
 	purple_prefs_add_int(PIDGIN_PREFS_ROOT "/conversations/tab_side", GTK_POS_TOP);
 	purple_prefs_add_int(PIDGIN_PREFS_ROOT "/conversations/scrollback_lines", 4000);
 
+#ifdef _WIN32
+	purple_prefs_add_bool(PIDGIN_PREFS_ROOT "/conversations/use_theme_font", TRUE);
+	purple_prefs_add_string(PIDGIN_PREFS_ROOT "/conversations/custom_font", "");
+#endif
+
 	/* Conversations -> Chat */
 	purple_prefs_add_none(PIDGIN_PREFS_ROOT "/conversations/chat");
 	purple_prefs_add_int(PIDGIN_PREFS_ROOT "/conversations/chat/entry_height", 54);
