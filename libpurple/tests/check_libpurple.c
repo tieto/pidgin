a8a23ee5c0b9f55f9a6dffb592ca4e74f9fb00fe
Elliott Sales de Andrade <qulogic@pidgin.im>
2009-12-13 00:36:36 +0000

    Fix the call to g_build_path in the test suite. Before it made some really strange set of directories for the settings dir. Now it's just 'home'.
    --HG--
    extra : convert_revision : 024b0f33f4ce79af59a2779ed32aa68da6a62e05

--- libpurple/tests/check_libpurple.c
+++ libpurple/tests/check_libpurple.c
@@ -44,7 +44,7 @@ purple_check_init(void) {
 	purple_eventloop_set_ui_ops(&eventloop_ui_ops);
 
 	/* build our fake home directory */
-	home_dir = g_build_path(BUILDDIR, "libpurple", "tests", "home", NULL);
+	home_dir = g_build_path(G_DIR_SEPARATOR_S, BUILDDIR, "libpurple", "tests", "home", NULL);
 	purple_util_set_user_dir(home_dir);
 	g_free(home_dir);
 
