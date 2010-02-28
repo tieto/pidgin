e9c0f09bc1d9d7bf0904f4cedcf7cf57bb475382
Elliott Sales de Andrade <qulogic@pidgin.im>
2010-02-28 04:38:11 +0000

    Fix some warnings.
    --HG--
    branch : cpw.malu.ft_thumbnails
    extra : convert_revision : 8e2b7f928b977c3a3e7555d39108bc3a47911f38

--- pidgin/gtkft.c
+++ pidgin/gtkft.c
@@ -1171,7 +1171,7 @@ pidgin_xfer_add_thumbnail(PurpleXfer *xfer)
 				purple_xfer_get_local_filename(xfer), 128, 128, NULL);
 
 		if (thumbnail) {
-			gpointer *buffer = NULL;
+			gchar *buffer = NULL;
 			gsize size;
 			char *option_keys[2] = {"quality", NULL};
 			char *option_values[2] = {"75", NULL};
@@ -1179,7 +1179,8 @@ pidgin_xfer_add_thumbnail(PurpleXfer *xfer)
 				option_keys, option_values, NULL);
 
 			if (buffer) {
-				purple_debug_info("pidgin", "created thumbnail of %d bytes\n",
+				purple_debug_info("pidgin",
+				                  "created thumbnail of %" G_GSIZE_FORMAT " bytes\n",
 					size);
 				purple_xfer_set_thumbnail(xfer, buffer, size);
 				g_free(buffer);
