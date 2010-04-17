7f5f7c1505d1d007f34ea6a5a70b84880e7411b6
Paul Aurich <darkrain42@pidgin.im>
2010-04-17 21:37:52 +0000

    Clarify
    --HG--
    extra : convert_revision : e8d0f65afa1697a579f40a67aa98f8a8fa4523f0

--- pidgin/gtkimhtml.c
+++ pidgin/gtkimhtml.c
@@ -1189,8 +1189,8 @@ static void paste_received_cb (GtkClipboard *clipboard, GtkSelectionData *select
 		text = g_malloc(selection_data->length + 1);
 		memcpy(text, selection_data->data, selection_data->length);
 		/* Make sure the paste data is null-terminated.  Given that
-		 * we're passed length (but assume later that it is), this
-		 * seems sensible to me.
+		 * we're passed length (but assume later that it is
+		 * null-terminated), this seems sensible to me.
 		 */
 		text[selection_data->length] = '\0';
 	}
