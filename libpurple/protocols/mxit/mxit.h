fec0733969a5037a6882fbcfdf70c59a4c5fefa3
Andrew Victor <andrew.victor@mxit.com>
2010-05-20 09:03:37 +0000

    Increment plugin version to 2.4.0.
    --HG--
    branch : mxit
    extra : convert_revision : 339168b713a11eedc7a4f6ffd6cb9ee960254448

--- libpurple/protocols/mxit/mxit.h
+++ libpurple/protocols/mxit/mxit.h
@@ -63,7 +63,7 @@
 /* Plugin details */
 #define		MXIT_PLUGIN_ID				"prpl-loubserp-mxit"
 #define		MXIT_PLUGIN_NAME			"MXit"
-#define		MXIT_PLUGIN_VERSION			"2.3.0"
+#define		MXIT_PLUGIN_VERSION			"2.4.0"
 #define		MXIT_PLUGIN_EMAIL			"Pieter Loubser <libpurple@mxit.com>"
 #define		MXIT_PLUGIN_WWW				"http://www.mxit.com"
 #define		MXIT_PLUGIN_SUMMARY			"MXit Protocol Plugin"
