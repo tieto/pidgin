8dbdcfc8aec41548b6e474394fdd352d23ab89e5
Jorge Villaseñor <masca@cpw.pidgin.im>
2010-06-03 23:27:23 +0000

    Fix a warning.
    --HG--
    branch : soc.2010.msn-tlc
    extra : convert_revision : 3fbfcbb72a2c73dd467d70c13157a4c0aadea69a

--- libpurple/protocols/msn/slpcall.c
+++ libpurple/protocols/msn/slpcall.c
@@ -267,7 +267,7 @@ msn_slp_process_msg(MsnSlpLink *slplink, MsnSlpMessage *slpmsg)
 		g_free(body_str);
 	}
 	else if (slpmsg->flags == P2P_MSN_OBJ_DATA ||
-	         slpmsg->flags == P2P_WML2009_COMP & P2P_MSN_OBJ_DATA ||
+	         slpmsg->flags == (P2P_WML2009_COMP & P2P_MSN_OBJ_DATA) ||
 	         slpmsg->flags == P2P_FILE_DATA)
 	{
 		slpcall = msn_slplink_find_slp_call_with_session_id(slplink, slpmsg->session_id);
