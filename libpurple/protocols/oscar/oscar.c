c9ad1bc7e97e3976b2796e7f38cbf99e002de97d
Mark Doliner <markdoliner@pidgin.im>
2010-07-21 02:58:11 +0000

    *** Plucked rev a8b26bdf35f2685c221a62e65eeb041aa1f41143 (markdoliner@pidgin.im): Fix a crash bug in oscar related to trying to allocate too much memory. This was reported to our security mailing list by Jan Kaluza The Great. I honestly couldn't figure out how to repro this crash, so I've been considering it as not a remote-crash security problem, so I chose to skip the CVE process for this.
    --HG--
    branch : release-2.7.2
    extra : convert_revision : 33f2cf39ab5eb0a4a63972bd843f6afa4072eebc

--- libpurple/protocols/oscar/oscar.c
+++ libpurple/protocols/oscar/oscar.c
@@ -3028,12 +3028,23 @@ incomingim_chan4(OscarData *od, FlapConnection *conn, aim_userinfo_t *userinfo, 
 			if (smstype != 0)
 				break;
 			taglen = byte_stream_getle32(&qbs);
+			if (taglen > 2000) {
+				/* Avoid trying to allocate large amounts of memory, in
+				   case we get something unexpected. */
+				break;
+			}
 			tagstr = byte_stream_getstr(&qbs, taglen);
 			if (tagstr == NULL)
 				break;
 			byte_stream_advance(&qbs, 3);
 			byte_stream_advance(&qbs, 4);
 			smslen = byte_stream_getle32(&qbs);
+			if (smslen > 2000) {
+				/* Avoid trying to allocate large amounts of memory, in
+				   case we get something unexpected. */
+				g_free(tagstr);
+				break;
+			}
 			smsmsg = byte_stream_getstr(&qbs, smslen);
 
 			/* Check if this is an SMS being sent from server */
