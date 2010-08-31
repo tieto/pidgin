04f6d4bc5bc09e077b98db45e953e1d6ee758636
Marcus Lundblad <malu@pidgin.im>
2010-08-31 20:48:59 +0000

    media: Add a close button to the stand-alone video configuration window. Adjusted title
    --HG--
    extra : convert_revision : d7c7bb3f6eb13f17750ceb5aa22cd2bfe891d2f4

--- pidgin/plugins/vvconfig.c
+++ pidgin/plugins/vvconfig.c
@@ -513,17 +513,35 @@ config_destroy(GtkObject *w, gpointer nul)
 }
 
 static void
+config_close(GtkObject *w, gpointer nul)
+{
+	gtk_widget_destroy(GTK_WIDGET(window));
+}
+
+static void
 show_config(PurplePluginAction *action)
 {
 	if (!window) {
+		GtkWidget *vbox = gtk_vbox_new(FALSE, PIDGIN_HIG_BORDER);
+		GtkWidget *hbox = gtk_hbox_new(FALSE, PIDGIN_HIG_BORDER);
 		GtkWidget *config_frame = get_plugin_config_frame(NULL);
-		window = pidgin_create_window(_("Voice and Video Settings"),
+		GtkWidget *close = gtk_button_new_from_stock(GTK_STOCK_CLOSE);
+		
+		gtk_container_add(GTK_CONTAINER(vbox), config_frame);
+		gtk_container_add(GTK_CONTAINER(vbox), hbox);
+		window = pidgin_create_window(_("Voice/Video Settings"),
 			PIDGIN_HIG_BORDER, NULL, TRUE);
 		g_signal_connect(G_OBJECT(window), "destroy", 
 			G_CALLBACK(config_destroy), NULL);
-		gtk_container_add(GTK_CONTAINER(window), config_frame);
+		g_signal_connect(G_OBJECT(close), "clicked",
+		    G_CALLBACK(config_close), NULL);
+		gtk_box_pack_end(GTK_BOX(hbox), close, FALSE, FALSE, PIDGIN_HIG_BORDER);
+		gtk_container_add(GTK_CONTAINER(window), vbox);
+		gtk_widget_show(GTK_WIDGET(close));
+		gtk_widget_show(GTK_WIDGET(vbox));
+		gtk_widget_show(GTK_WIDGET(hbox));
 	}
-	gtk_window_present(window);
+	gtk_window_present(GTK_WINDOW(window));
 }
 		
 		
