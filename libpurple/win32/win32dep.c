815712a5b39aa65ccd6fc5eaa6ba539827aa81aa
Daniel Atallah <datallah@pidgin.im>
2010-08-31 00:51:41 +0000

    Make wpurple_find_and_loadproc() work for non-ASCII full paths
    --HG--
    extra : convert_revision : 5125b363023fd23274b20c695fa3ff7e7bcb89c3

--- libpurple/win32/win32dep.c
+++ libpurple/win32/win32dep.c
@@ -77,15 +77,21 @@ FARPROC wpurple_find_and_loadproc(const char *dllname, const char *procedure) {
 	BOOL did_load = FALSE;
 	FARPROC proc = 0;
 
-	if(!(hmod = GetModuleHandle(dllname))) {
+	wchar_t *wc_dllname = g_utf8_to_utf16(dllname, -1, NULL, NULL, NULL);
+
+	if(!(hmod = GetModuleHandleW(wc_dllname))) {
 		purple_debug_warning("wpurple", "%s not already loaded; loading it...\n", dllname);
-		if(!(hmod = LoadLibrary(dllname))) {
+		if(!(hmod = LoadLibraryW(wc_dllname))) {
 			purple_debug_error("wpurple", "Could not load: %s\n", dllname);
+			g_free(wc_dllname);
 			return NULL;
 		}
 		else
 			did_load = TRUE;
 	}
+
+	g_free(wc_dllname);
+	wc_dllname = NULL;
 
 	if((proc = GetProcAddress(hmod, procedure))) {
 		purple_debug_info("wpurple", "This version of %s contains %s\n",
