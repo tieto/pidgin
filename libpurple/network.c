bac3d4ef0da519ccfbfef69102d097e13e2d3acb
Elliott Sales de Andrade <qulogic@pidgin.im>
2010-09-03 07:24:29 +0000

    protocol is the *value*, not the *key*. Thus, we really shouldn't be attempting to remove it from the hash table. Especially because we just removed the corresponding key, thus invalidating this pointer.
    Fixes #12387.
    
    --HG--
    extra : convert_revision : c7f2cce48dfe465889b7803de44156e7e89e41e0

--- libpurple/network.c
+++ libpurple/network.c
@@ -1077,12 +1077,10 @@ purple_network_remove_port_mapping(gint fd)
 
 	if (protocol) {
 		purple_network_upnp_mapping_remove(&port, protocol, NULL);
-		g_hash_table_remove(upnp_port_mappings, protocol);
 	} else {
 		protocol = g_hash_table_lookup(nat_pmp_port_mappings, &port);
 		if (protocol) {
 			purple_network_nat_pmp_mapping_remove(&port, protocol, NULL);
-			g_hash_table_remove(nat_pmp_port_mappings, protocol);
 		}
 	}
 }
