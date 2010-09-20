734b48453f37920eacf1b4c9bffd6982a10c70fd
Paul Aurich <darkrain42@pidgin.im>
2010-09-20 03:35:16 +0000

    jabber/jingle: Fix 2 warnings: jingle/transport.c: In function 'jingle_transport_set_property': jingle/transport.c:111: warning: unused variable 'transport' jingle/transport.c: In function 'jingle_transport_get_property': jingle/transport.c:124: warning: unused variable 'transport'
    --HG--
    extra : convert_revision : d5ed660cac7c28849ae1f5213bff7df85fe8a3c9

--- libpurple/protocols/jabber/jingle/transport.c
+++ libpurple/protocols/jabber/jingle/transport.c
@@ -108,7 +108,6 @@ jingle_transport_finalize (GObject *transport)
 static void
 jingle_transport_set_property (GObject *object, guint prop_id, const GValue *value, GParamSpec *pspec)
 {
-	JingleTransport *transport;
 	g_return_if_fail(JINGLE_IS_TRANSPORT(object));
 
 	switch (prop_id) {
@@ -121,7 +120,6 @@ jingle_transport_set_property (GObject *object, guint prop_id, const GValue *val
 static void
 jingle_transport_get_property (GObject *object, guint prop_id, GValue *value, GParamSpec *pspec)
 {
-	JingleTransport *transport;
 	g_return_if_fail(JINGLE_IS_TRANSPORT(object));
 
 	switch (prop_id) {
