bfe767880ab0d2b41b154faf4564c09cbd0cf367
John Bailey <rekkanoryo@rekkanoryo.org>
2010-11-23 04:33:29 +0000

    Properly handle an AIM login server of login.messaging.aol.com.  Fixes #12948.
    --HG--
    extra : convert_revision : b21beb71827799eec3525c256f12e609d91e6e4f

--- libpurple/protocols/oscar/oscar.c
+++ libpurple/protocols/oscar/oscar.c
@@ -792,7 +792,7 @@ oscar_login(PurpleAccount *account)
 			 * do what we know is best for them and change the setting out
 			 * from under them to the SSL login server.
 			 */
-			if (!strcmp(server, get_login_server(od->icq, FALSE))) {
+			if (!strcmp(server, get_login_server(od->icq, FALSE)) || !strcmp(server, AIM_ALT_LOGIN_SERVER)) {
 				purple_debug_info("oscar", "Account uses SSL, so changing server to default SSL server\n");
 				purple_account_set_string(account, "server", get_login_server(od->icq, TRUE));
 				server = get_login_server(od->icq, TRUE);
