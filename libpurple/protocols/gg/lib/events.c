795da58b3ba381c5d2e45fef0111a3d8c4446874
Elliott Sales de Andrade <qulogic@pidgin.im>
2010-11-28 00:18:31 +0000

    Update internal/external libgadu to 1.9.0, except for some superfluous changes, such as the extra format_ variable in gg_convert_to_html.
    Fixes #12789.
    
    --HG--
    extra : convert_revision : cb89d88037865aa2ec058f28dc480e92825cd9f5

--- libpurple/protocols/gg/lib/events.c
+++ libpurple/protocols/gg/lib/events.c
@@ -1149,6 +1149,7 @@ static int gg_watch_fd_connected(struct gg_session *sess, struct gg_event *e)
 
 			e->type = GG_EVENT_NOTIFY60;
 			e->event.notify60 = malloc(sizeof(*e->event.notify60));
+
 			if (!e->event.notify60) {
 				gg_debug_session(sess, GG_DEBUG_MISC, "// gg_watch_fd_connected() not enough memory for notify data\n");
 				goto fail;
@@ -1159,7 +1160,7 @@ static int gg_watch_fd_connected(struct gg_session *sess, struct gg_event *e)
 			while (length >= sizeof(struct gg_notify_reply80)) {
 				uint32_t descr_len;
 				char *tmp;
-				
+
 				e->event.notify60[i].uin	= gg_fix32(n->uin);
 				e->event.notify60[i].status	= gg_fix32(n->status);
 				e->event.notify60[i].remote_ip	= n->remote_ip;
@@ -1169,7 +1170,6 @@ static int gg_watch_fd_connected(struct gg_session *sess, struct gg_event *e)
 				e->event.notify60[i].version	= 0x00;	/* not-supported */
 				e->event.notify60[i].time	= 0;	/* not-supported */
 
-				
 				descr_len = gg_fix32(n->descr_len);
 
 				length -= sizeof(struct gg_notify_reply80);
