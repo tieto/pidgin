df4cdd172a3732f524d3115fe9dabad2e89dd3f5
Mark Doliner <markdoliner@pidgin.im>
2011-02-28 06:53:53 +0000

    Rename "userName" to "friendly_name" to be consistent with the term we use for this everywhere, and to get rid of the camel case var name. Also remove a stray space
    --HG--
    extra : convert_revision : d3c73d69688687fb6bc62852836a8e0bd1b51382

--- libpurple/protocols/msn/userlist.c
+++ libpurple/protocols/msn/userlist.c
@@ -236,17 +236,17 @@ msn_userlist_destroy(MsnUserList *userlist)
 }
 
 MsnUser *
-msn_userlist_find_add_user(MsnUserList *userlist,const char *passport,const char *userName)
+msn_userlist_find_add_user(MsnUserList *userlist, const char *passport, const char *friendly_name)
 {
 	MsnUser *user;
 
 	user = msn_userlist_find_user(userlist, passport);
 	if (user == NULL)
 	{
-		user = msn_user_new(userlist, passport, userName);
+		user = msn_user_new(userlist, passport, friendly_name);
 		msn_userlist_add_user(userlist, user);
 	} else {
-		msn_user_set_friendly_name(user, userName);
+		msn_user_set_friendly_name(user, friendly_name);
 	}
 	return user;
 }
@@ -287,7 +287,7 @@ msn_userlist_find_user(MsnUserList *userlist, const char *passport)
 MsnUser *
 msn_userlist_find_user_with_id(MsnUserList *userlist, const char *uid)
 {
- 	GList *l;
+	GList *l;
 
 	g_return_val_if_fail(uid != NULL, NULL);
 
