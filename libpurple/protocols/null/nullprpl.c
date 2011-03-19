3189f194b9d1251b75d7223fd0416c04380c163b
Elliott Sales de Andrade <qulogic@pidgin.im>
2011-03-19 23:47:36 +0000

    Fix comment order.
    --HG--
    extra : convert_revision : 624acee6261639cfd78ac1bfb5a8aaf75715728c

--- libpurple/protocols/null/nullprpl.c
+++ libpurple/protocols/null/nullprpl.c
@@ -1120,9 +1120,9 @@ static PurplePluginProtocolInfo prpl_info =
   NULL,                                /* get_account_text_table */
   NULL,                                /* initiate_media */
   NULL,                                /* get_media_caps */
+  NULL,                                /* get_moods */
   NULL,                                /* set_public_alias */
-  NULL,                                /* get_public_alias */
-  NULL                                 /* get_moods */
+  NULL                                 /* get_public_alias */
 };
 
 static void nullprpl_init(PurplePlugin *plugin)
