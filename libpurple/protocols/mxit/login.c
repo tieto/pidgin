16bfcfe21c513b06093746a8c80e2092bed7c748
Andrew Victor <andrew.victor@mxit.com>
2011-08-23 08:33:52 +0000

    Fixed properly now.
    --HG--
    extra : convert_revision : da7bc1419142f97b2def127ef42c9c7c562e77e3

--- libpurple/protocols/mxit/login.c
+++ libpurple/protocols/mxit/login.c
@@ -57,7 +57,7 @@ static struct MXitSession* mxit_create_object( PurpleAccount* account )
 		const char* username	= purple_account_get_username( account );
 
 		if ( username[0] == '+' ) {
-			char* fixed	= g_strdup( &account->username[1] );
+			char* fixed	= g_strdup( &username[1] );
 			purple_account_set_username( account, fixed );
 			g_free( fixed );
 		}
