a6825771ce4cb035894881a0fe7c7827c0f8922b
Elliott Sales de Andrade <qulogic@pidgin.im>
2011-10-31 07:55:55 +0000

    These checks are unnecessary because we require a newer GLib.
    --HG--
    branch : soc.2008.masterpassword
    extra : convert_revision : 46052394c50008d62db7ff3fe28d2e01e480c05f

--- libpurple/plugins/keyrings/gnomekeyring.c
+++ libpurple/plugins/keyrings/gnomekeyring.c
@@ -41,14 +41,6 @@
 #include <glib.h>
 #include <gnome-keyring.h>
 
-#ifndef G_GNUC_NULL_TERMINATED
-# if __GNUC__ >= 4
-#  define G_GNUC_NULL_TERMINATED __attribute__((__sentinel__))
-# else
-#  define G_GNUC_NULL_TERMINATED
-# endif
-#endif
-
 #include "account.h"
 #include "version.h"
 #include "keyring.h"
--- libpurple/plugins/keyrings/kwallet.cpp
+++ libpurple/plugins/keyrings/kwallet.cpp
@@ -38,14 +38,6 @@
 
 #include <glib.h>
 
-#ifndef G_GNUC_NULL_TERMINATED
- #if __GNUC__ >= 4
-  #define G_GNUC_NULL_TERMINATED __attribute__((__sentinel__))
- #else
-  #define G_GNUC_NULL_TERMINATED
- #endif
-#endif
-
 #include "account.h"
 #include "version.h"
 #include "keyring.h"
