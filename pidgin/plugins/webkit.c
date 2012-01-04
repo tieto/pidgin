99550b64c95f0cfedbea323dad2acafc9bbb214f
Elliott Sales de Andrade <qulogic@pidgin.im>
2012-01-04 03:37:59 +0000

    This #include is unnecessary when internal.h is used.
    --HG--
    extra : convert_revision : fdfd87aa38b93b9e2ad6d5bd108ec4f8716dcd34

--- pidgin/plugins/webkit.c
+++ pidgin/plugins/webkit.c
@@ -17,10 +17,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1301, USA.
  */
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
 #include "internal.h"
 
 #include "version.h"
