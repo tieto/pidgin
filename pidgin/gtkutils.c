28d47269eea2dd3f1d1c725ecdd13c7da54a0009
Elliott Sales de Andrade <qulogic@pidgin.im>
2012-01-04 09:45:10 +0000

    Use a GtkWebViewToolbar when creating an editable GtkWebView.
    --HG--
    extra : convert_revision : 073c067fa0d2b0dc8378ade5b6398691ad7d371b

--- pidgin/gtkutils.c
+++ pidgin/gtkutils.c
@@ -68,6 +68,7 @@
 #include "gtkthemes.h"
 #include "gtkutils.h"
 #include "gtkwebview.h"
+#include "gtkwebviewtoolbar.h"
 #include "pidgin/minidialog.h"
 
 typedef struct {
@@ -295,7 +296,7 @@ pidgin_create_webview(gboolean editable, GtkWidget **webview_ret, GtkWidget **to
 	gtk_widget_show(vbox);
 
 	if (editable) {
-		toolbar = gtk_imhtmltoolbar_new();
+		toolbar = gtk_webviewtoolbar_new();
 		gtk_box_pack_start(GTK_BOX(vbox), toolbar, FALSE, FALSE, 0);
 		gtk_widget_show(toolbar);
 
@@ -307,10 +308,7 @@ pidgin_create_webview(gboolean editable, GtkWidget **webview_ret, GtkWidget **to
 	}
 
 	webview = gtk_webview_new();
-#if 0
-	/* TODO WEBKIT: Don't have editable webview yet. */
 	gtk_webview_set_editable(GTK_WEBVIEW(webview), editable);
-#endif /* if 0 */
 #ifdef USE_GTKSPELL
 	if (editable && purple_prefs_get_bool(PIDGIN_PREFS_ROOT "/conversations/spellcheck"))
 		pidgin_setup_gtkspell(GTK_TEXT_VIEW(webview));
@@ -318,8 +316,8 @@ pidgin_create_webview(gboolean editable, GtkWidget **webview_ret, GtkWidget **to
 	gtk_widget_show(webview);
 
 	if (editable) {
-		gtk_imhtmltoolbar_attach(GTK_IMHTMLTOOLBAR(toolbar), webview);
-		gtk_imhtmltoolbar_associate_smileys(GTK_IMHTMLTOOLBAR(toolbar), "default");
+		gtk_webviewtoolbar_attach(GTK_WEBVIEWTOOLBAR(toolbar), webview);
+		gtk_webviewtoolbar_associate_smileys(GTK_WEBVIEWTOOLBAR(toolbar), "default");
 	}
 
 	sw = pidgin_make_scrollable(webview, GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC, GTK_SHADOW_NONE, -1, -1);
