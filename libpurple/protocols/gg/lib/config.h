70c54c89e04f4be241361661d2c6a7aefa2cedd1
Tomasz Wasilczyk <tomkiewicz@cpw.pidgin.im>
2012-05-25 21:35:32 +0000

    Libgadu configuration file cleanup and update
    --HG--
    branch : soc.2012.gg
    extra : convert_revision : 86ff13bbe87f89af715123867fd41d4798f58272

--- libpurple/protocols/gg/lib/config.h
+++ libpurple/protocols/gg/lib/config.h
@@ -1,22 +1,19 @@
 /* Local libgadu configuration. */
 
-/* libpurple's config */
-#include <config.h>
-
 #ifndef __GG_LIBGADU_CONFIG_H
 #define __GG_LIBGADU_CONFIG_H
 
+/* libpurple's config */
+#include <config.h>
+
 /* Defined if libgadu was compiled for bigendian machine. */
-#undef __GG_LIBGADU_BIGENDIAN
+#undef GG_CONFIG_BIGENDIAN
 #ifdef WORDS_BIGENDIAN
-#  define __GG_LIBGADU_BIGENDIAN
+#  define GG_CONFIG_BIGENDIAN
 #endif
 
 /* Defined if this machine has gethostbyname_r(). */
 #undef GG_CONFIG_HAVE_GETHOSTBYNAME_R
-
-/* Defined if this machine has _exit(). */
-#define GG_CONFIG_HAVE__EXIT
 
 /* Defined if libgadu was compiled and linked with fork support. */
 #undef GG_CONFIG_HAVE_FORK
@@ -25,25 +22,25 @@
 #endif
 
 /* Defined if libgadu was compiled and linked with pthread support. */
-/* We don't like pthreads. */
-#undef __GG_LIBGADU_HAVE_PTHREAD
+/* We don't use pthreads due to stability issues. */
+#undef GG_CONFIG_HAVE_PTHREAD
 
 /* Defined if this machine has C99-compiliant vsnprintf(). */
-#undef __GG_LIBGADU_HAVE_C99_VSNPRINTF
+#undef HAVE_C99_VSNPRINTF
 #ifndef _WIN32
-#  define __GG_LIBGADU_HAVE_C99_VSNPRINTF
+#  define HAVE_C99_VSNPRINTF
 #endif
 
 /* Defined if this machine has va_copy(). */
-#define __GG_LIBGADU_HAVE_VA_COPY
+#define GG_CONFIG_HAVE_VA_COPY
 
 /* Defined if this machine has __va_copy(). */
-#define __GG_LIBGADU_HAVE___VA_COPY
+#define GG_CONFIG_HAVE___VA_COPY
 
 /* Defined if this machine supports long long. */
-#undef __GG_LIBGADU_HAVE_LONG_LONG
+#undef GG_CONFIG_HAVE_LONG_LONG
 #ifdef HAVE_LONG_LONG
-#  define __GG_LIBGADU_HAVE_LONG_LONG
+#  define GG_CONFIG_HAVE_LONG_LONG
 #endif
 
 /* Defined if libgadu was compiled and linked with GnuTLS support. */
@@ -53,16 +50,28 @@
 #endif
 
 /* Defined if libgadu was compiled and linked with OpenSSL support. */
-/* Always undefined in Purple. */
-#undef __GG_LIBGADU_HAVE_OPENSSL
+/* OpenSSL cannot be used with libpurple due to licence type. */
+#undef GG_CONFIG_HAVE_OPENSSL
 
 /* Defined if libgadu was compiled and linked with zlib support. */
 #undef GG_CONFIG_HAVE_ZLIB
 
 /* Defined if uintX_t types are defined in <stdint.h>. */
 #undef GG_CONFIG_HAVE_STDINT_H
-#if HAVE_STDINT_H
+#ifdef HAVE_STDINT_H
 #  define GG_CONFIG_HAVE_STDINT_H
+#endif
+
+/* Defined if uintX_t types are defined in <inttypes.h>. */
+#undef GG_CONFIG_HAVE_INTTYPES_H
+#ifdef HAVE_INTTYPES_H
+#  define GG_CONFIG_HAVE_INTTYPES_H
+#endif
+
+/* Defined if uintX_t types are defined in <sys/types.h>. */
+#undef GG_CONFIG_HAVE_SYS_TYPES_H
+#ifdef HAVE_SYS_TYPES_H
+#  define GG_CONFIG_HAVE_SYS_TYPES_H
 #endif
 
 
