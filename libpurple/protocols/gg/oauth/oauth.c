fe35b7165b229fd3affbbbcd0e9fb6d2a2bf6b36
Tomasz Wasilczyk <tomkiewicz@cpw.pidgin.im>
2012-08-20 21:02:55 +0200

    Gadu-Gadu: missing copyright notes
    --HG--
    branch : soc.2012.gg

--- libpurple/protocols/gg/oauth/oauth.c
+++ libpurple/protocols/gg/oauth/oauth.c
@@ -1,22 +1,27 @@
-/*
- *  (C) Copyright 2008 Wojtek Kaniewski <wojtekka@irc.pl>
+/* purple
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License Version
- *  2.1 as published by the Free Software Foundation.
+ * Purple is the legal property of its developers, whose names are too numerous
+ * to list here.  Please refer to the COPYRIGHT file distributed with this
+ * source distribution.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
+ * Code adapted from libgadu (C) 2008 Wojtek Kaniewski <wojtekka@irc.pl>
+ * (http://toxygen.net/libgadu/) during Google Summer of Code 2012
+ * by Tomek Wasilczyk (http://www.wasilczyk.pl).
  *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
- *  USA.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111-1301  USA
  */
-
-// source: http://toxygen.net/libgadu/
 
 #include "oauth.h"
 
--- libpurple/protocols/gg/oauth/oauth.h
+++ libpurple/protocols/gg/oauth/oauth.h
@@ -1,4 +1,27 @@
-// source: http://toxygen.net/libgadu/
+/* purple
+ *
+ * Purple is the legal property of its developers, whose names are too numerous
+ * to list here.  Please refer to the COPYRIGHT file distributed with this
+ * source distribution.
+ *
+ * Code adapted from libgadu (C) 2008 Wojtek Kaniewski <wojtekka@irc.pl>
+ * (http://toxygen.net/libgadu/) during Google Summer of Code 2012
+ * by Tomek Wasilczyk (http://www.wasilczyk.pl).
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111-1301  USA
+ */
 
 #ifndef _GGP_OAUTH_H
 #define _GGP_OAUTH_H
