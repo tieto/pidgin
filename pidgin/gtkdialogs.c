f44e669abc2a3e1f4f09de7eebbb55515c3d36da
Mark Doliner <mark@kingant.net>
2013-02-20 00:46:03 -0800

    Add Paulo's email address.
    --HG--
    branch : release-2.x.y

--- pidgin/gtkdialogs.c
+++ pidgin/gtkdialogs.c
@@ -227,7 +227,7 @@ static const struct translator translators[] = {
 	{N_("Punjabi"),             "pa", "Amanpreet Singh Alam", "aalam@users.sf.net"},
 	{N_("Polish"),              "pl", "Piotr Drąg", "piotrdrag@gmail.com"},
 	{N_("Polish"),              "pl", "Piotr Makowski", "pmakowski@aviary.pl"},
-	{N_("Portuguese"),          "pt", "Paulo Ribeiro", NULL},
+	{N_("Portuguese"),          "pt", "Paulo Ribeiro", "paulo@diffraction.pt"},
 	{N_("Portuguese-Brazil"),   "pt_BR", "Renato Silva", "br.renatosilva@gmail.com"},
 	{N_("Pashto"),              "ps", "Kashif Masood", "masudmails@yahoo.com"},
 	{N_("Romanian"),            "ro", "Mișu Moldovan", "dumol@gnome.org"},
