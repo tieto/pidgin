9bb068da173796e3927ca09ad212925efc700a58
Ankit Vani <a@nevitus.org>
2013-06-30 16:01:09 +0530

    set ui_data to NULL after setting ui_ops in gtkconv
    --HG--
    branch : soc.2013.gobjectification

--- pidgin/gtkconv.c
+++ pidgin/gtkconv.c
@@ -8423,8 +8423,8 @@ pidgin_conv_attach(PurpleConversation *conv)
 	g_object_set_data(G_OBJECT(conv), "unseen-count", NULL);
 	g_object_set_data(G_OBJECT(conv), "unseen-state", NULL);
 	purple_conversation_set_ui_ops(conv, pidgin_conversations_get_conv_ui_ops());
-	if (!PIDGIN_CONVERSATION(conv))
-		private_gtkconv_new(conv, FALSE);
+	purple_conversation_set_ui_data(conv, NULL);
+	private_gtkconv_new(conv, FALSE);
 	timer = GPOINTER_TO_INT(g_object_get_data(G_OBJECT(conv), "close-timer"));
 	if (timer) {
 		purple_timeout_remove(timer);
