6e83523f84bafcf12436af99ea41b3bf4cf3b60f
Ankit Vani <a@nevitus.org>
2013-09-02 00:58:05 +0530

    Since some pref paths have been changed anyways, changed the pidgin UI identifier
    --HG--
    branch : soc.2013.gobjectification.plugins

--- pidgin/pidgin.h
+++ pidgin/pidgin.h
@@ -36,8 +36,7 @@
 /**
  * Our UI's identifier.
  */
-/* leave this as gtk-gaim until we have a decent way to migrate UI-prefs */
-#define PIDGIN_UI "gtk-gaim"
+#define PIDGIN_UI "gtk-pidgin"
 
 /* change this only when we have a sane upgrade path for old prefs */
 #define PIDGIN_PREFS_ROOT "/pidgin"
