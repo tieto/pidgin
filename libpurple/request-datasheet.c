321d9435f4710907af18622a6961fd075f039391
Tomasz Wasilczyk <twasilczyk@pidgin.im>
2013-11-05 13:39:29 +0100

    Request API: fix a bug when checking datasheet action sensitivity

--- libpurple/request-datasheet.c
+++ libpurple/request-datasheet.c
@@ -244,8 +244,16 @@ purple_request_datasheet_action_call(PurpleRequestDatasheetAction *act,
 {
 	g_return_if_fail(act != NULL);
 
-	if (act->cb)
-		act->cb(rec, act->cb_data);
+	if (!act->cb)
+		return;
+
+	if (!purple_request_datasheet_action_is_sensitive(act, rec) {
+		purple_debug_warning("request-datasheet",
+			"Action is disabled for this record");
+		return;
+	}
+
+	act->cb(rec, act->cb_data);
 }
 
 void
@@ -268,7 +276,7 @@ purple_request_datasheet_action_is_sensitive(PurpleRequestDatasheetAction *act,
 	if (!act->sens_cb)
 		return (rec != NULL);
 
-	return act->sens_cb(rec, act->cb_data);
+	return act->sens_cb(rec, act->sens_data);
 }
 
 /***** Datasheet record API ***************************************************/
