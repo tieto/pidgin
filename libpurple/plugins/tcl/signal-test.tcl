aa14f3d7c936dfcb26a533cffb59cea40a8245e6
Mark Doliner <mark@kingant.net>
2014-01-10 21:33:36 -0800

    Prefer https pidgin.im rather than http

--- libpurple/plugins/tcl/signal-test.tcl
+++ libpurple/plugins/tcl/signal-test.tcl
@@ -119,5 +119,5 @@ proc plugin_init { } {
 	     "Tests Tcl signal handlers" \
              "Debugs a ridiculous amount of signal information." \
              "Ethan Blanton <elb@pidgin.im>" \
-             "https://www.pidgin.im/"
+             "https://pidgin.im/"
 }
