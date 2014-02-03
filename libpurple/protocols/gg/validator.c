f98a3c6b5c9e37fd0a9f2c0f01cad846846757d0
Tomasz Wasilczyk <twasilczyk@pidgin.im>
2014-02-03 21:49:38 +0100

    Style: fix gg (partially)

--- libpurple/protocols/gg/validator.c
+++ libpurple/protocols/gg/validator.c
@@ -38,16 +38,16 @@ gboolean ggp_validator_token(PurpleRequestField *field, gchar **errmsg,
 	void *token)
 {
 	const char *value;
-	
+
 	g_return_val_if_fail(field != NULL, FALSE);
 	g_return_val_if_fail(purple_request_field_get_type(field) ==
 		PURPLE_REQUEST_FIELD_STRING, FALSE);
-	
+
 	value = purple_request_field_string_get_value(field);
-	
+
 	if (value != NULL && ggp_account_token_validate(token, value))
 		return TRUE;
-	
+
 	if (errmsg)
 		*errmsg = g_strdup(_("Captcha validation failed"));
 	return FALSE;
@@ -58,16 +58,16 @@ gboolean ggp_validator_password(PurpleRequestField *field, gchar **errmsg,
 	void *user_data)
 {
 	const char *value;
-	
+
 	g_return_val_if_fail(field != NULL, FALSE);
 	g_return_val_if_fail(purple_request_field_get_type(field) ==
 		PURPLE_REQUEST_FIELD_STRING, FALSE);
-	
+
 	value = purple_request_field_string_get_value(field);
-	
+
 	if (value != NULL && ggp_password_validate(value))
 		return TRUE;
-	
+
 	if (errmsg)
 		*errmsg = g_strdup(_("Password can contain 6-15 alphanumeric characters"));
 	return FALSE;
@@ -78,20 +78,20 @@ gboolean ggp_validator_password_equal(PurpleRequestField *field, gchar **errmsg,
 {
 	const char *value1, *value2;
 	PurpleRequestField *field2 = field2_p;
-	
+
 	g_return_val_if_fail(field != NULL, FALSE);
 	g_return_val_if_fail(field2 != NULL, FALSE);
 	g_return_val_if_fail(purple_request_field_get_type(field) ==
 		PURPLE_REQUEST_FIELD_STRING, FALSE);
 	g_return_val_if_fail(purple_request_field_get_type(field2) ==
 		PURPLE_REQUEST_FIELD_STRING, FALSE);
-	
+
 	value1 = purple_request_field_string_get_value(field);
 	value2 = purple_request_field_string_get_value(field2);
-	
+
 	if (g_strcmp0(value1, value2) == 0)
 		return TRUE;
-	
+
 	if (errmsg)
 		*errmsg = g_strdup(_("Passwords do not match"));
 	return FALSE;
