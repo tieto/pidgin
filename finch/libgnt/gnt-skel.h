bf911351bcc78a518bbbe215cb8f0e3336d8766c
Ankit Vani <a@nevitus.org>
2014-02-06 22:53:45 +0530

    Remove struct nesting -- gtk-doc and gir don't like it
    --HG--
    branch : gtkdoc-conversion

--- finch/libgnt/gnt-skel.h
+++ finch/libgnt/gnt-skel.h
@@ -19,7 +19,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111-1301  USA
  */
-/**
+/*
  * SECTION:gnt-skel
  * @section_id: libgnt-gnt-skel
  * @short_description: <filename>gnt-skel.h</filename>
