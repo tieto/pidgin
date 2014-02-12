cfafbcfc61bf4db75d46de00c2100e82ff74fe5d
Tomasz Wasilczyk <twasilczyk@pidgin.im>
2014-02-12 15:49:30 +0100

    Fix some GTK 3.8 deprecation warnings

--- pidgin/plugins/gestures/stroke-draw.c
+++ pidgin/plugins/gestures/stroke-draw.c
@@ -53,47 +53,53 @@ static guint timer_id;
 static void gstroke_execute (GtkWidget *widget, const gchar *name);
 
 static void
-record_stroke_segment (GtkWidget *widget)
+record_stroke_segment(GtkWidget *widget)
 {
-  gint x, y;
-  struct gstroke_metrics *metrics;
-
-  g_return_if_fail( widget != NULL );
-
-  gtk_widget_get_pointer (widget, &x, &y);
-
-  if (last_mouse_position.invalid)
-    last_mouse_position.invalid = FALSE;
-  else if (gstroke_draw_strokes())
-    {
-#if 1
-      XDrawLine (gstroke_disp, gstroke_window, gstroke_gc,
-                 last_mouse_position.last_point.x,
-                 last_mouse_position.last_point.y,
-                 x, y);
-      /* XFlush (gstroke_disp); */
-#else
-      /* FIXME: this does not work. It will only work if we create a
-         corresponding GDK window for stroke_window and draw on
-         that... */
-      gdk_draw_line (gtk_widget_get_window(widget),
-                     widget->style->fg_gc[GTK_STATE_NORMAL],
-                     last_mouse_position.last_point.x,
-                     last_mouse_position.last_point.y,
-                     x,
-                     y);
+	gint x, y;
+	struct gstroke_metrics *metrics;
+#if GTK_CHECK_VERSION(3,0,0)
+	GdkDeviceManager *devmgr;
+	GdkDevice *dev;
 #endif
-    }
 
-  if (last_mouse_position.last_point.x != x
-      || last_mouse_position.last_point.y != y)
-    {
-      last_mouse_position.last_point.x = x;
-      last_mouse_position.last_point.y = y;
-      metrics = (struct gstroke_metrics *)g_object_get_data(G_OBJECT(widget),
-															GSTROKE_METRICS);
-      _gstroke_record (x, y, metrics);
-    }
+	g_return_if_fail(widget != NULL);
+
+#if GTK_CHECK_VERSION(3,0,0)
+	devmgr = gdk_display_get_device_manager(gtk_widget_get_display(widget));
+	dev = gdk_device_manager_get_client_pointer(devmgr);
+	gdk_window_get_device_position(gtk_widget_get_window(widget),
+		dev, &x, &y, NULL);
+#else
+	gtk_widget_get_pointer(widget, &x, &y);
+#endif
+
+	if (last_mouse_position.invalid)
+		last_mouse_position.invalid = FALSE;
+	else if (gstroke_draw_strokes()) {
+#if 1
+		XDrawLine(gstroke_disp, gstroke_window, gstroke_gc,
+			last_mouse_position.last_point.x,
+			last_mouse_position.last_point.y, x, y);
+		/* XFlush (gstroke_disp); */
+#else
+		/* FIXME: this does not work. It will only work if we create
+		 * a corresponding GDK window for stroke_window and draw on
+		 * that... */
+		gdk_draw_line(gtk_widget_get_window(widget),
+			widget->style->fg_gc[GTK_STATE_NORMAL],
+			last_mouse_position.last_point.x,
+			last_mouse_position.last_point.y, x, y);
+#endif
+	}
+
+	if (last_mouse_position.last_point.x != x ||
+		last_mouse_position.last_point.y != y)
+	{
+		last_mouse_position.last_point.x = x;
+		last_mouse_position.last_point.y = y;
+		metrics = g_object_get_data(G_OBJECT(widget), GSTROKE_METRICS);
+		_gstroke_record (x, y, metrics);
+	}
 }
 
 static gint
