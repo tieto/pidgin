ddc6fa3c3232cbeefd9fb852427574621bedd391
Tomasz Wasilczyk <twasilczyk@pidgin.im>
2014-03-17 10:41:24 +0100

    Fix a glib warning about bad gtk_widget_set_size_request call

--- pidgin/gtkblist.c
+++ pidgin/gtkblist.c
@@ -5921,6 +5921,7 @@ static void pidgin_blist_show(PurpleBuddyList *list)
 	GError *error;
 	GtkAccelGroup *accel_group;
 	GtkTreeSelection *selection;
+	int blist_width;
 	GtkTargetEntry dte[] = {{"PURPLE_BLIST_NODE", GTK_TARGET_SAME_APP, DRAG_ROW},
 				{"application/x-im-contact", 0, DRAG_BUDDY},
 				{"text/x-vcard", 0, DRAG_VCARD },
@@ -6087,8 +6088,10 @@ static void pidgin_blist_show(PurpleBuddyList *list)
 	gtkblist->headline_image = gtk_image_new_from_pixbuf(NULL);
 	gtk_misc_set_alignment(GTK_MISC(gtkblist->headline_image), 0.0, 0);
 	gtkblist->headline_label = gtk_label_new(NULL);
-	gtk_widget_set_size_request(gtkblist->headline_label,
-				    purple_prefs_get_int(PIDGIN_PREFS_ROOT "/blist/width")-25,-1);
+	blist_width = purple_prefs_get_int(PIDGIN_PREFS_ROOT "/blist/width") - 25;
+	if (blist_width < -1)
+		blist_width = -1;
+	gtk_widget_set_size_request(gtkblist->headline_label, blist_width, -1);
 	gtk_label_set_line_wrap(GTK_LABEL(gtkblist->headline_label), TRUE);
 	gtk_box_pack_start(GTK_BOX(gtkblist->headline), gtkblist->headline_image, FALSE, FALSE, 0);
 	gtk_box_pack_start(GTK_BOX(gtkblist->headline), gtkblist->headline_label, TRUE, TRUE, 0);
