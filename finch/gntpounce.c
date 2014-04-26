5da4e3065f3d4f05684953c51c3296dc87eae2d5
Tomasz Wasilczyk <twasilczyk@pidgin.im>
2014-04-26 21:08:57 +0200

    Finch: fix glib warnings

--- finch/gntpounce.c
+++ finch/gntpounce.c
@@ -977,11 +977,11 @@ finch_pounces_init(void)
 						PURPLE_CALLBACK(signed_on_off_cb), NULL);
 }
 
-/* XXX: There's no such thing in pidgin. Perhaps there should be? */
+/* XXX: There's no such thing in pidgin. Perhaps there should be?
+ * For sure, we don't need purple_pounces_unregister_handler -
+ * it's wiped in purple_pounces_uninit.
+ */
 void finch_pounces_uninit()
 {
-	purple_pounces_unregister_handler(FINCH_UI);
-
 	purple_signals_disconnect_by_handle(finch_pounces_get_handle());
 }
-
