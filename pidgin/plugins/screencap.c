a91782f0f11073cc7db1169074f392904fc10a39
Tomasz Wasilczyk <twasilczyk@pidgin.im>
2014-04-30 21:28:35 +0200

    Fix a warning (lack of copyright word)

--- pidgin/plugins/screencap.c
+++ pidgin/plugins/screencap.c
@@ -2,6 +2,8 @@
  * Screen Capture - a plugin that allows taking screenshots and sending them
  * to your buddies as inline images.
  *
+ * Copyright (C) 2014, Tomasz Wasilczyk <twasilczyk@pidgin.im>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
