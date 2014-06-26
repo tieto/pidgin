3c0ae6290a1a94b8b9357c6c001877a73aa431f1
Mark Doliner <mark@kingant.net>
2014-06-25 22:55:53 -0700

    s/reason/reasonable/ (thanks, rlaager) And improve the wording slightly.

--- libpurple/plugins/keyrings/secretservice.c
+++ libpurple/plugins/keyrings/secretservice.c
@@ -46,8 +46,8 @@
 /* Translators: Secret Service is a service that runs on the user's computer.
    It is one option for where the user's passwords can be stored. It is a
    project name. It may not be appropriate to translate this string, but
-   transliterating seems reason. More info about the project at
-   https://wiki.gnome.org/Projects/Libsecret */
+   transliterating to your alphabet is reasonable. More info about the
+   project can be found at https://wiki.gnome.org/Projects/Libsecret */
 #define SECRETSERVICE_NAME        N_("Secret Service")
 #define SECRETSERVICE_ID          "keyring-libsecret"
 
