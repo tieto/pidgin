1ba837772e3f743e7849a369238f69e7749ded81
Elliott Sales de Andrade <qulogic@pidgin.im>
2014-11-15 19:43:02 -0500

    Fix minor assertion.
    The body of a SOAP callback may not exist yet if it's prematurely freed.
    
    --HG--
    branch : release-2.x.y
    extra : rebase_source : 2296a38822e2eceec1cecfe5db3c6604b2cd1df6

--- libpurple/protocols/msn/contact.c
+++ libpurple/protocols/msn/contact.c
@@ -96,7 +96,8 @@ msn_callback_state_free(MsnCallbackState *state)
 	g_free(state->old_group_name);
 	g_free(state->new_group_name);
 	g_free(state->guid);
-	xmlnode_free(state->body);
+	if (state->body)
+		xmlnode_free(state->body);
 
 	g_free(state);
 }
