e295f757b71ffd188dc5e6dbab3ba1fe611acc1f
Richard Laager <rlaager@pidgin.im>
2016-01-04 12:01:24 -0600

    Remove extraneous periods in Add Chat dialog
    Closes #15128
    See also BitBucket PR #6
    
    --HG--
    branch : release-2.x.y

--- pidgin/gtkblist.c
+++ pidgin/gtkblist.c
@@ -7294,8 +7294,8 @@ pidgin_blist_request_add_chat(PurpleAccount *account, PurpleGroup *group,
 	                          data->chat_data.rq_data.sg, data->group_combo,
 	                          TRUE, NULL);
 
-	data->autojoin = gtk_check_button_new_with_mnemonic(_("Auto_join when account connects."));
-	data->persistent = gtk_check_button_new_with_mnemonic(_("_Remain in chat after window is closed."));
+	data->autojoin = gtk_check_button_new_with_mnemonic(_("Automatically _join when account connects"));
+	data->persistent = gtk_check_button_new_with_mnemonic(_("_Remain in chat after window is closed"));
 	gtk_box_pack_start(GTK_BOX(vbox), data->autojoin, FALSE, FALSE, 0);
 	gtk_box_pack_start(GTK_BOX(vbox), data->persistent, FALSE, FALSE, 0);
 
