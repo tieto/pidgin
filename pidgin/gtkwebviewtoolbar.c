08fa5a8afa49d79a0fe42417d81bd0dffd099c0e
Jakub Adam <jakub.adam@ktknet.cz>
2016-09-14 20:56:41 +0200

    gtkutils: remove usage of deprecated xthickness and ythickness
    GTK+ doesn't support xthickness and ythickness anymore and ignores
    the values in its calculations. GTK+ since 3.20 even crash when
    gtk_style_context_get() queries the thicknesses.
    
    I removed the properties without providing any replacement and have not
    seen any issues with placement of popup menus after that on Ubuntu 14.04
    (GTK+ 3.10.8) and newer.

--- pidgin/gtkwebviewtoolbar.c
+++ pidgin/gtkwebviewtoolbar.c
@@ -1198,13 +1198,8 @@ menu_position_func(GtkMenu  *menu,
 {
 	GtkWidget *widget = GTK_WIDGET(data);
 	GtkAllocation allocation;
-	GtkStyleContext *context;
-	gint ythickness;
 	int savy;
 
-	context = gtk_widget_get_style_context(widget);
-	gtk_style_context_get(context, gtk_style_context_get_state(context),
-	                      "ythickness", &ythickness, NULL);
 	gtk_widget_get_allocation(widget, &allocation);
 	gdk_window_get_origin(gtk_widget_get_window(widget), x, y);
 	*x += allocation.x;
@@ -1213,7 +1208,7 @@ menu_position_func(GtkMenu  *menu,
 
 	pidgin_menu_position_func_helper(menu, x, y, push_in, data);
 
-	if (savy > *y + ythickness + 1)
+	if (savy > *y + 1)
 		*y -= allocation.height;
 }
 
