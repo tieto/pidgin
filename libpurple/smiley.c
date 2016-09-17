0ff593adcd1d2fcc19abb6f5e1fb15c4900ab1c3
Gary Kramlich <grim@reaperworld.com>
2016-09-17 12:40:32 -0500

    A bunch of cleanups for smileys
    --HG--
    extra : rebase_source : a1a472462502288dd7202072cd22beadfc4396c0

--- libpurple/smiley.c
+++ libpurple/smiley.c
@@ -19,20 +19,14 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111-1301  USA
  */
 
-#include "internal.h"
-#include "glibcompat.h"
-#include "dbus-maybe.h"
-#include "debug.h"
 #include "smiley.h"
-#include "util.h"
-#include "xmlnode.h"
+#include "dbus-maybe.h"
 
 #define PURPLE_SMILEY_GET_PRIVATE(obj) \
 	(G_TYPE_INSTANCE_GET_PRIVATE((obj), PURPLE_TYPE_SMILEY, PurpleSmileyPrivate))
 
 typedef struct {
 	gchar *shortcut;
-	PurpleImage *image;
 } PurpleSmileyPrivate;
 
 enum
@@ -42,98 +36,36 @@ enum
 	PROP_LAST
 };
 
-static GObjectClass *parent_class;
 static GParamSpec *properties[PROP_LAST];
 
 /*******************************************************************************
- * API implementation
+ * Helpers
  ******************************************************************************/
-
-PurpleSmiley *
-purple_smiley_new(const gchar *shortcut, const gchar *path)
-{
-	PurpleSmiley *smiley;
-	PurpleSmileyPrivate *priv;
-
-	g_return_val_if_fail(shortcut != NULL, NULL);
-	g_return_val_if_fail(path != NULL, NULL);
-
-	smiley = g_object_new(PURPLE_TYPE_SMILEY,
-		"shortcut", shortcut,
-		NULL);
-	priv = PURPLE_SMILEY_GET_PRIVATE(smiley);
-
-	priv->image = purple_image_new_from_file(path);
-	if (!priv->image) {
-		purple_debug_error("smiley", "Couldn't load smiley data ");
-		g_object_unref(smiley);
-		return NULL;
-	}
-
-	return smiley;
-}
-
-PurpleSmiley *
-purple_smiley_new_remote(const gchar *shortcut)
-{
-	PurpleSmiley *smiley;
-	PurpleSmileyPrivate *priv;
-
-	g_return_val_if_fail(shortcut != NULL, NULL);
-
-	smiley = g_object_new(PURPLE_TYPE_SMILEY,
-		"shortcut", shortcut,
-		NULL);
-	priv = PURPLE_SMILEY_GET_PRIVATE(smiley);
-
-	priv->image = purple_image_transfer_new();
-
-	return smiley;
-}
-
-const gchar *
-purple_smiley_get_shortcut(const PurpleSmiley *smiley)
-{
+static void
+_purple_smiley_set_shortcut(PurpleSmiley *smiley, const ghcar *shortcut) {
 	PurpleSmileyPrivate *priv = PURPLE_SMILEY_GET_PRIVATE(smiley);
 
-	g_return_val_if_fail(priv != NULL, NULL);
+	g_free(priv->smiley);
 
-	return priv->shortcut;
+	priv->smiley = (shortcut) ? g_strdup(shortcut) : NULL;
+
+	g_object_notify(G_OBJECT(smiley), "shortcut");
 }
-
-PurpleImage *
-purple_smiley_get_image(PurpleSmiley *smiley)
-{
-	PurpleSmileyPrivate *priv = PURPLE_SMILEY_GET_PRIVATE(smiley);
-
-	g_return_val_if_fail(priv != NULL, NULL);
-	g_return_val_if_fail(priv->image != NULL, NULL);
-
-	return priv->image;
-}
-
 
 /*******************************************************************************
  * Object stuff
  ******************************************************************************/
-
 static void
-purple_smiley_init(GTypeInstance *instance, gpointer klass)
-{
+purple_smiley_init(GTypeInstance *instance, gpointer klass) {
 	PurpleSmiley *smiley = PURPLE_SMILEY(instance);
 	PURPLE_DBUS_REGISTER_POINTER(smiley, PurpleSmiley);
 }
 
 static void
-purple_smiley_finalize(GObject *obj)
-{
-	PurpleSmiley *smiley = PURPLE_SMILEY(obj);
-	PurpleSmileyPrivate *priv = PURPLE_SMILEY_GET_PRIVATE(smiley);
+purple_smiley_finalize(GObject *obj) {
+	PurpleSmileyPrivate *priv = PURPLE_SMILEY_GET_PRIVATE(obj);
 
 	g_free(priv->shortcut);
-
-	if (priv->image)
-		g_object_unref(priv->image);
 
 	PURPLE_DBUS_UNREGISTER_POINTER(smiley);
 
@@ -141,15 +73,14 @@ purple_smiley_finalize(GObject *obj)
 }
 
 static void
-purple_smiley_get_property(GObject *object, guint par_id, GValue *value,
-	GParamSpec *pspec)
+purple_smiley_get_property(GObject *obj, guint param_id, GValue *value,
+                           GParamSpec *pspec)
 {
 	PurpleSmiley *smiley = PURPLE_SMILEY(object);
-	PurpleSmileyPrivate *priv = PURPLE_SMILEY_GET_PRIVATE(smiley);
 
 	switch (par_id) {
 		case PROP_SHORTCUT:
-			g_value_set_string(value, priv->shortcut);
+			g_value_set_string(value, purple_smiley_get_shortcut(smiley));
 			break;
 		default:
 			G_OBJECT_WARN_INVALID_PROPERTY_ID(object, par_id, pspec);
@@ -158,16 +89,14 @@ purple_smiley_get_property(GObject *object, guint par_id, GValue *value,
 }
 
 static void
-purple_smiley_set_property(GObject *object, guint par_id, const GValue *value,
-	GParamSpec *pspec)
+purple_smiley_set_property(GObject *obj, guint param_id, const GValue *value,
+                           GParamSpec *pspec)
 {
-	PurpleSmiley *smiley = PURPLE_SMILEY(object);
-	PurpleSmileyPrivate *priv = PURPLE_SMILEY_GET_PRIVATE(smiley);
+	PurpleSmiley *smiley = PURPLE_SMILEY(obj);
 
-	switch (par_id) {
+	switch (param_id) {
 		case PROP_SHORTCUT:
-			g_free(priv->shortcut);
-			priv->shortcut = g_strdup(g_value_get_string(value));
+			_purple_smiley_set_shortcut(smiley, g_value_get_string(value));
 			break;
 		default:
 			G_OBJECT_WARN_INVALID_PROPERTY_ID(object, par_id, pspec);
@@ -176,41 +105,60 @@ purple_smiley_set_property(GObject *object, guint par_id, const GValue *value,
 }
 
 static void
-purple_smiley_class_init(PurpleSmileyClass *klass)
-{
-	GObjectClass *gobj_class = G_OBJECT_CLASS(klass);
+purple_smiley_class_init(PurpleSmileyClass *klass) {
+	GObjectClass *obj_class = G_OBJECT_CLASS(klass);
 
-	parent_class = g_type_class_peek_parent(klass);
+	obj_class->get_property = purple_smiley_get_property;
+	obj_class->set_property = purple_smiley_set_property;
+	obj_class->finalize = purple_smiley_finalize;
 
-	g_type_class_add_private(klass, sizeof(PurpleSmileyPrivate));
+	properties[PROP_SHORTCUT] = g_param_spec_string(
+		"shortcut",
+		"Shortcut",
+		"A non-escaped textual representation of a smiley.",
+		NULL,
+		G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY | G_PARAM_STATIC_STRINGS
+	);
 
-	gobj_class->get_property = purple_smiley_get_property;
-	gobj_class->set_property = purple_smiley_set_property;
-	gobj_class->finalize = purple_smiley_finalize;
-
-	properties[PROP_SHORTCUT] = g_param_spec_string("shortcut", "Shortcut",
-		"A non-escaped textual representation of a smiley.", NULL,
-		G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
-
-	g_object_class_install_properties(gobj_class, PROP_LAST, properties);
+	g_object_class_install_properties(obj_class, PROP_LAST, properties);
 }
 
-GType
-purple_smiley_get_type(void)
+G_DEFINE_TYPE_WITH_PRIVATE(PurpleSmiley, purple_smiley, PURPLE_TYPE_IMAGE);
+
+/*******************************************************************************
+ * API
+ ******************************************************************************/
+PurpleSmiley *
+purple_smiley_new(const gchar *shortcut, const gchar *path)
 {
-	static GType type = 0;
+	g_return_val_if_fail(shortcut != NULL, NULL);
+	g_return_val_if_fail(path != NULL, NULL);
 
-	if (G_UNLIKELY(type == 0)) {
-		static const GTypeInfo info = {
-			.class_size = sizeof(PurpleSmileyClass),
-			.class_init = (GClassInitFunc)purple_smiley_class_init,
-			.instance_size = sizeof(PurpleSmiley),
-			.instance_init = purple_smiley_init,
-		};
+	return g_object_new(
+		PURPLE_TYPE_SMILEY,
+		"shortcut", shortcut,
+		NULL
+	);
+}
 
-		type = g_type_register_static(G_TYPE_OBJECT,
-			"PurpleSmiley", &info, 0);
-	}
+PurpleSmiley *
+purple_smiley_new_remote(const gchar *shortcut) {
+	g_return_val_if_fail(shortcut != NULL, NULL);
 
-	return type;
+	return g_object_new(
+		PURPLE_TYPE_SMILEY,
+		"shortcut", shortcut,
+		NULL
+	);
+}
+
+const gchar *
+purple_smiley_get_shortcut(const PurpleSmiley *smiley) {
+	PurpleSmileyPrivate *priv = NULL;
+
+	g_return_val_if_fail(PURPLE_IS_SMILEY(smiley), NULL);
+
+	priv = PURPLE_SMILEY_GET_PRIVATE(smiley);
+
+	return priv->shortcut;
 }
