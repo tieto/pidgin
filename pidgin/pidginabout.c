4a01c632454a10762d194211a16ab24850fb3d8b
Gary Kramlich <grim@reaperworld.com>
2017-08-10 23:00:22 -0500

    Get the meson arguments working

--- pidgin/pidginabout.c
+++ pidgin/pidginabout.c
@@ -4,6 +4,15 @@
 #include "pidginabout.h"
 #include "pidginresources.h"
 
+#include <stdio.h>
+
+#include "config.h"
+#ifdef HAVE_MESON_CONFIG
+#include "meson-config.h"
+#else
+#error HAVE_MESON_CONFIG is not defined
+#endif
+
 struct _PidginAboutDialogPrivate {
 	GtkWidget *stack;
 
@@ -11,6 +20,11 @@ struct _PidginAboutDialogPrivate {
 	GtkWidget *credits_page;
 	GtkWidget *credits_treeview;
 	GtkTreeStore *credits_store;
+
+	GtkWidget *build_info_button;
+	GtkWidget *build_info_page;
+	GtkWidget *build_info_treeview;
+	GtkWidget *build_info_store;
 
 	gboolean switching_pages;
 };
@@ -28,6 +42,12 @@ _pidgin_about_dialog_switch_page(PidginAboutDialog *about, const gchar *name) {
 	gtk_toggle_button_set_active(
 		GTK_TOGGLE_BUTTON(about->priv->credits_button),
 		g_str_equal("credits", name)
+	);
+
+	/* is the build info button active? */
+	gtk_toggle_button_set_active(
+		GTK_TOGGLE_BUTTON(about->priv->build_info_button),
+		g_str_equal("build-info", name)
 	);
 
 	about->priv->switching_pages = FALSE;
@@ -55,7 +75,7 @@ _pidgin_about_dialog_load_contributors(PidginAboutDialog *about) {
 	parser = json_parser_new();
 
 	if(!json_parser_load_from_stream(parser, istream, NULL, &error)) {
-		g_critical(error->message);
+		g_critical("%s", error->message);
 	}
 
 	root_node = json_parser_get_root(parser);
@@ -117,6 +137,55 @@ _pidgin_about_dialog_load_contributors(PidginAboutDialog *about) {
 	g_input_stream_close(istream, NULL, NULL);
 }
 
+static void
+_pidgin_about_dialog_add_config_args(PidginAboutDialog *about) {
+#ifdef CONFIG_ARGS
+#endif /* CONFIG_ARGS */
+}
+
+static void
+_pidgin_about_dialog_add_meson_args(PidginAboutDialog *about) {
+#ifdef MESON_ARGS
+	GtkTreeIter meson, value;
+	gchar **splits = NULL;
+	gchar *split = NULL;
+	gint idx = 0;
+
+	gtk_tree_store_append(about->priv->build_info_store, &meson, NULL);
+	gtk_tree_store_set(
+		about->priv->build_info_store,
+		&meson,
+		0, "<span font-weight=\"bold\">Meson Arguments</span>",
+		-1
+	);
+
+	/* now walk through the arguments and add them */
+	splits = g_strsplit(MESON_ARGS, " ", -1);
+	for(idx = 0; splits[idx]; idx++) {
+		gchar **value_split = g_strsplit(splits[idx], "=", 2);
+
+		gtk_tree_store_append(about->priv->build_info_store, &value, &meson);
+		gtk_tree_store_set(
+			about->priv->build_info_store,
+			&value,
+			0, value_split[0],
+			1, value_split[1],
+			-1
+		);
+
+		g_strfreev(value_split);
+	}
+
+	g_strfreev(splits);
+#endif /* MESON_ARGS */
+}
+
+static void
+_pidgin_about_dialog_load_build_info(PidginAboutDialog *about) {
+	_pidgin_about_dialog_add_config_args(about);
+	_pidgin_about_dialog_add_meson_args(about);
+}
+
 /******************************************************************************
  * Callbacks
  *****************************************************************************/
@@ -131,6 +200,19 @@ _pidgin_about_dialog_toggle_credits(GtkToggleButton *b, gpointer d) {
 	show = gtk_toggle_button_get_active(b);
 
 	_pidgin_about_dialog_switch_page(d, show ? "credits" : "main");
+}
+
+static void
+_pidgin_about_dialog_toggle_build_info(GtkToggleButton *b, gpointer d) {
+	PidginAboutDialog *about = d;
+	gboolean show = FALSE;
+
+	if(about->priv->switching_pages)
+		return;
+
+	show = gtk_toggle_button_get_active(b);
+
+	_pidgin_about_dialog_switch_page(d, show ? "build-info" : "main");
 }
 
 /******************************************************************************
@@ -153,6 +235,11 @@ pidgin_about_dialog_class_init(PidginAboutDialogClass *klass) {
 	gtk_widget_class_bind_template_child_private(widget_class, PidginAboutDialog, credits_page);
 	gtk_widget_class_bind_template_child_private(widget_class, PidginAboutDialog, credits_store);
 	gtk_widget_class_bind_template_child_private(widget_class, PidginAboutDialog, credits_treeview);
+
+	gtk_widget_class_bind_template_child_private(widget_class, PidginAboutDialog, build_info_button);
+	gtk_widget_class_bind_template_child_private(widget_class, PidginAboutDialog, build_info_page);
+	gtk_widget_class_bind_template_child_private(widget_class, PidginAboutDialog, build_info_store);
+	gtk_widget_class_bind_template_child_private(widget_class, PidginAboutDialog, build_info_treeview);
 }
 
 static void
@@ -163,6 +250,7 @@ pidgin_about_dialog_init(PidginAboutDialog *about) {
 
 	gtk_widget_init_template(GTK_WIDGET(about));
 
+	/* setup the credits stuff */
 	g_signal_connect(
 		about->priv->credits_button,
 		"toggled",
@@ -171,11 +259,18 @@ pidgin_about_dialog_init(PidginAboutDialog *about) {
 	);
 
 	_pidgin_about_dialog_load_contributors(about);
-
-	/* expand all the nodes in the credits treeview and tweak some other
-	 * properties.
-	 */
 	gtk_tree_view_expand_all(GTK_TREE_VIEW(about->priv->credits_treeview));
+
+	/* setup the build info page */
+	g_signal_connect(
+		about->priv->build_info_button,
+		"toggled",
+		G_CALLBACK(_pidgin_about_dialog_toggle_build_info),
+		about
+	);
+
+	_pidgin_about_dialog_load_build_info(about);
+	gtk_tree_view_expand_all(GTK_TREE_VIEW(about->priv->build_info_treeview));
 }
 
 GtkWidget *
